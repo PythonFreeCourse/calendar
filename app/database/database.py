4b7e491d68978f962f901eb0dd41261f7fb5ed0f
leddest <46251307+leddest@users.noreply.github.com>
2021-01-10 21:13:46 +0200

    Add environment variable

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./dev.db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
