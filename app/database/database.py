877149a6a5e1a1121dd33c3e9a5fc7099caf19d5
leddest <46251307+leddest@users.noreply.github.com>
2021-01-14 10:47:45 +0200

    Add python-dotenv

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_CONNECTION_STRING")
 
