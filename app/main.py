f6c619bd23f29df322aae27c058971ed8890350c
Aviad Amar <aviadamar@gmail.com>
2021-01-15 22:09:28 +0100

    Hello Wrold

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", {
+async def home(request: Request):
+    return templates.TemplateResponse("home.j2", {
         "request": request,
         "message": "Hello, World!"
     })
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
