5c875966ff9073b1ec99cf7b95a84ae3905c83bf
Michael BD <mbdi007@gamil.com>
2021-01-15 12:28:39 +0200

    delete the unrelevant table for this tiket

--- app/database/models.py
+++ app/database/models.py
@@ -28,17 +28,17 @@ class Event(Base):
     content = Column(String)
     location = Column(String)
     owner_id = Column(Integer, ForeignKey("users.id"))
-    repeated_event_id = Column(Integer, ForeignKey("repeated_events.id"))
+    # repeated_event_id = Column(Integer, ForeignKey("repeated_events.id"))
 
     owner = relationship("User", back_populates="events")
-    repeated_id = relationship("Repeated_event", back_populates="repeated_event")
+    # repeated_id = relationship("Repeated_event", back_populates="repeated_event")
 
 
-class Repeated_event(Base):
-    """have an ID to every sequence of repeated events so you can select all the sequence in one"""
-    __tablename__ = "repeated_events"
+# class Repeated_event(Base):
+#     """have an ID to every sequence of repeated events so you can select all the sequence in one"""
+#     __tablename__ = "repeated_events"
 
-    id = Column(Integer, primary_key=True, index=True)
+#     id = Column(Integer, primary_key=True, index=True)
     
-    repeated_event = relationship(
-        "Event", cascade="all, delete", back_populates="repeated_id")
+#     repeated_event = relationship(
+#         "Event", cascade="all, delete", back_populates="repeated_")
