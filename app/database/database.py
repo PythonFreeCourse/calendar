56877c62923919f74e823725faf1e528c457814c
Idan Pelled <pelled.idan@gmail.com>
2021-01-16 18:01:19 +0200

    fix bug

--- app/database/database.py
+++ app/database/database.py
@@ -7,8 +7,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_CONNECTION_STRING")
 
 engine = create_engine(
-    'sqlite:///test.db', connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
