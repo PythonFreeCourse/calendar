4cc2447ecec9933ca3af7db34f4a3df344617634
Hadas Kedar <hadasit@gmail.com>
2021-01-18 17:43:02 +0200

    feat: get weather forecast - fixes according to requested changes.

--- tests/test_weather_forecast.py
+++ tests/test_weather_forecast.py
@@ -1,30 +1,27 @@
-from datetime import datetime, timedelta
+import datetime
 import pytest
 
-from app.weather_forecast import get_weather_data
+from app.routers.weather_forecast import get_weather_data
 
 
 DATA_GET_WEATHER = [
-    pytest.param(4, "d", 2020, "tel aviv", 0, marks=pytest.mark.xfail, id="ivalid input type"),
-    pytest.param(4, 4, 2020, "tel aviv", 0, id="basic historical test"),
-    pytest.param(4, 4, 2070, "tel aviv", 0, marks=pytest.mark.xfail, id="year out of range"),
-    pytest.param(1, 1, 2030, "tel aviv", 0, id="basic historical forecast test - prior in current year"),
-    pytest.param(31, 12, 2030, "tel aviv", 0, id="basic historical forecast test - future"),
-    pytest.param(15, 1, 2020, "neo", 0, marks=pytest.mark.xfail, id="location not found test"),
-    pytest.param(32, 1, 2020, "tel aviv", 0, marks=pytest.mark.xfail, id="invalid date"),
-    pytest.param(29, 2, 2024, "tel aviv", 0, id="basic historical forecast test"),
+    pytest.param(2020, "tel aviv", 0, marks=pytest.mark.xfail, id="invalid input type"),
+    pytest.param(datetime.datetime(day=4, month=4, year=2070), "tel aviv", 0, marks=pytest.mark.xfail, id="year out of range"),
+    pytest.param(datetime.datetime(day=4, month=4, year=2020), "tel aviv", 0, id="basic historical test"),
+    pytest.param(datetime.datetime(day=1, month=1, year=2030), "tel aviv", 0, id="basic historical forecast test - prior in current year"),
+    pytest.param(datetime.datetime(day=31, month=12, year=2030), "tel aviv", 0, id="basic historical forecast test - future"),
+    pytest.param(datetime.datetime(day=29, month=2, year=2024), "tel aviv", 0, id="basic historical forecast test"),
+    pytest.param(datetime.datetime(day=15, month=1, year=2020), "neo", 0, marks=pytest.mark.xfail, id="location not found test"),
 ]
 
 
-@pytest.mark.parametrize('day, month, year, location, expected', DATA_GET_WEATHER)
-def test_get_weather_data(day, month, year, location, expected):
-    output = get_weather_data(day, month, year, location)
+@pytest.mark.parametrize('requested_date, location, expected', DATA_GET_WEATHER)
+def test_get_weather_data(requested_date, location, expected):
+    output = get_weather_data(requested_date, location)
     assert output['Status'] == expected
 
 
 def test_get_forecast_weather_data():
-    temp_date = datetime.now() + timedelta(days=1)
-    output = get_weather_data(temp_date.day, temp_date.month, temp_date.year, "tel aviv")
+    temp_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    output = get_weather_data(temp_date, "tel aviv")
     assert output['Status'] == 0
-
-# pytest.param(15, 1, 2021, "tel aviv", 0, id="basic forecast test"),
