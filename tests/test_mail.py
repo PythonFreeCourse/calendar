0e167cdeb2bd4f9e91100ce6d0b118510ddd1366
Anna Shtirberg <anna.shtirberg@gmail.com>
2021-01-19 14:57:34 +0200

    changed fixture for smtpd to include configuration

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -13,13 +13,19 @@ client = TestClient(app)
 
 
 @pytest.fixture
-def override_smtp_config(smtpd):
+def configured_smtpd(smtpd):
+    """
+    Overrides the SMTP related configuration to use a mock SMTP server
+    :param smtpd: the smtpdfix fixture that represents an SMTP server
+    :return: smtpd
+    """
+
     def override_settings():
         return config.Settings(smtp_server=smtpd.hostname,
                                smtp_port=smtpd.port)
 
     app.dependency_overrides[config.get_settings] = override_settings
-    yield
+    yield smtpd
     app.dependency_overrides.pop(config.get_settings)
 
 
@@ -28,14 +34,14 @@ def test_read_main():
     assert response.status_code == 200
 
 
-def test_send_mail_no_body(smtpd, override_smtp_config):
+def test_send_mail_no_body(configured_smtpd):
     response = client.post("/mail/invitation/")
     assert response.status_code == 422
     assert response.json() == {'detail': [{
         'loc': ['body'],
         'msg': 'field required',
         'type': 'value_error.missing'}]}
-    assert len(smtpd.messages) == 0
+    assert len(configured_smtpd.messages) == 0
 
 
 @pytest.mark.parametrize("body,expected_json", [
@@ -96,14 +102,14 @@ def test_send_mail_no_body(smtpd, override_smtp_config):
     ),
 ])
 def test_send_mail_partial_body(body, expected_json,
-                                smtpd, override_smtp_config):
+                                configured_smtpd):
     response = client.post("/mail/invitation/", json=body)
     assert response.status_code == 422
     assert response.json() == expected_json
-    assert len(smtpd.messages) == 0
+    assert len(configured_smtpd.messages) == 0
 
 
-def test_send_mail_invalid_email(smtpd, override_smtp_config):
+def test_send_mail_invalid_email(configured_smtpd):
     response = client.post("/mail/invitation/", json={
         "sender_name": "string",
         "recipient_name": "string",
@@ -112,10 +118,10 @@ def test_send_mail_invalid_email(smtpd, override_smtp_config):
 
     assert response.status_code == 200
     assert response.json() == {'message': "Please enter valid email address"}
-    assert len(smtpd.messages) == 0
+    assert len(configured_smtpd.messages) == 0
 
 
-def test_send_mail_valid_email(smtpd, override_smtp_config):
+def test_send_mail_valid_email(configured_smtpd):
     response = client.post("/mail/invitation/", json={
         "sender_name": "string",
         "recipient_name": "string",
@@ -125,7 +131,7 @@ def test_send_mail_valid_email(smtpd, override_smtp_config):
     assert response.status_code == 200
     assert response.json() == {
         'message': 'Your message was sent successfully to string'}
-    assert len(smtpd.messages) == 1
+    assert len(configured_smtpd.messages) == 1
 
 
 # internal mail checks
