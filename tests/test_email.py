09b0ebee4dc91d0fe70c1aef51adb0417c98bd04
PureDreamer <70023598+PureDreamer@users.noreply.github.com>
2021-01-19 21:38:46 +0200

    Update test_email.py

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,19 +1,8 @@
-import os
 
-import pytest
 from app.internal.email import mail
 from fastapi import BackgroundTasks
 
-NO_CONFIG = pytest.mark.skipif(
-    (
-        os.getenv("MAIL_USERNAME") == "a",
-        os.getenv("MAIL_PASSWORD") == "a",
-        os.getenv("MAIL_FROM") == "a"
-    ), reason="Config is not set!"
-)
 
-
-@NO_CONFIG
 def test_email_send(client, user, event):
     mail.config.SUPPRESS_SEND = 1
     with mail.record_messages() as outbox:
@@ -26,7 +15,6 @@ def test_email_send(client, user, event):
         assert response.status_code == 303
 
 
-@NO_CONFIG
 def test_failed_email_send(client, user, event):
     mail.config.SUPPRESS_SEND = 1
     with mail.record_messages() as outbox:
