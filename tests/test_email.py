14168bb53d23afcd32bf17f6acc819de96ebbc75
PureDreamer <70023598+PureDreamer@users.noreply.github.com>
2021-01-20 00:19:19 +0200

    Update test_email.py

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,21 +1,21 @@
 
+from fastapi_mail.errors import ConnectionErrors
 from app.internal.email import mail
 from fastapi import BackgroundTasks
-from fastapi_mail.errors import ErrorConnection
-
-
-class ErrorConnection(Exception):
-    pass
 
 
 def test_email_send(client, user, event):
     mail.config.SUPPRESS_SEND = 1
+    response = None
     with mail.record_messages() as outbox:
-        response = client.post(
-            "/email_send/", data={
-                "event_used": event.id, "user_to_send": user.id,
-                "title": "Testing",
-                "background_tasks": BackgroundTasks})
+        try:
+            response = client.post(
+                "/email_send/", data={
+                    "event_used": event.id, "user_to_send": user.id,
+                    "title": "Testing",
+                    "background_tasks": BackgroundTasks})
+        except ConnectionErrors:
+            pass
         assert len(outbox) == 1
         assert response.status_code == 303
 
