b9d44a979be974c2b86bc42a35e642b7ca7f56c7
Liad Noam <LiadN@Taldor.corp>
2021-01-20 01:26:57 +0200

    first logging commit

--- app/main.py
+++ app/main.py
@@ -2,12 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from app import app
 
-app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+app.logger.debug("shimishinmiyeah")
+app.logger.info("shimishinmiyeah")
+app.logger.error("shimishinmiyeah")
 
 
 @app.get("/")
@@ -22,11 +28,21 @@ def home(request: Request):
 def profile(request: Request):
 
     # Get relevant data from database
-    upcouming_events = range(5)
+    upcoming_events = range(5)
     current_username = "Chuck Norris"
 
     return templates.TemplateResponse("profile.html", {
         "request": request,
         "username": current_username,
-        "events": upcouming_events
+        "events": upcoming_events
     })
+
+
+@app.get('/custom-logger')
+def customize_logger(request: Request):
+    request.app.logger.info("Here Is Your Info Log")
+    try:
+        a = 1 / 0
+    except ZeroDivisionError:
+        request.app.logger.error("Here Is Your Error Log")
+    return {'data': "Successfully Implemented Custom Log"}
