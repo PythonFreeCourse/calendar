7325a036d6d49496fdad141d6da275c024f13d03
Efrat <efratush.by@gmail.com>
2021-01-20 07:16:07 +0200

    func update event + testing

--- app/routers/event.py
+++ app/routers/event.py
@@ -1,11 +1,10 @@
-import os
 from datetime import datetime
-from typing import Optional, Set, Tuple, Union
+from typing import Dict, Optional, Set
 
-from app.database.database import get_db
-from app.database.models import Event, User
+from app.database.models import Event
 from app.dependencies import templates
-from fastapi import APIRouter, Depends, Form, Request
+from fastapi import APIRouter, Request
+from sqlalchemy import exc
 from sqlalchemy.orm import Session
 
 router = APIRouter(
@@ -19,99 +18,69 @@ router = APIRouter(
 async def eventedit(request: Request):
     return templates.TemplateResponse("eventedit.html", {"request": request})
 
-def check_validation(start_time, end_time):
+
+def get_event_by_id(db: Session, event_id: int) -> Event:
+    """Select event by id"""
+    return db.query(Event).filter(Event.id == event_id).first()
+
+
+def check_validation(start_time: datetime, end_time: datetime) -> bool:
     """Check if the start date is smaller then the end time"""
     if start_time < end_time:
         return True
     return False
 
 
-def get_event_by_id(db: Session, event_id: int) -> Event:
-    """Select event by id"""
-    event = db.query(Event).filter(Event.id == event_id).first()
-    return event
+def get_column_names_event(db: Session) -> Set:
+    result = db.execute("select * from events")
+    return {col for col in result.keys()}
 
 
-def prepre_mail_for_update_event(old_event: Event, new_event: Event, link_to_event: str) -> Tuple[str]:
-    """prepre text for the email thet will send on update event"""
-    title = old_event.title + ' updated'
-    body = ''
-    if old_event.title != new_event.title:
-        body += 'title changed - from: ' + old_event.title + ' to: ' + new_event.title
-        body += '\n'
-    if old_event.location != new_event.location:
-        if old_event.location:
-            body += 'location changed - from: ' + \
-                old_event.location + ' to: ' + new_event.location
-        else:
-            body += 'location added ' + new_event.location
-        body += '\n'
-    if old_event.start != new_event.start or old_event.end != new_event.end:
-        body += 'the evant time has changed - from: ' + str(old_event.start) + ' - ' + str(
-            old_event.end) + '/nto: ' + str(new_event.start) + ' - ' + str(new_event.end)
-        body += '\n'
-    if old_event.VC_link != new_event.VC_link:
-        if old_event.VC_link:
-            body += 'link changed - from: ' + old_event.VC_link + ' to: ' + new_event.VC_link
-        else:
-            body += 'link added ' + new_event.VC_link
-        body += '\n'
-    if old_event.content != new_event.content:
-        if old_event.content:
-            body += 'content changed - from: ' + \
-                old_event.content + ' to: ' + new_event.content
-        else:
-            body += 'content added ' + new_event.content
-        body += '\n'
-    body += 'Link to updated event: ' + link_to_event
-    # להוסיף שורה ולבקש לאשר מחדש
-    return (title, body)
-
-
-@router.post('/{event_id}/edit')
-def update_event(request: Request,
-                 event_id: int,
-                 event_title: str = Form(...),
-                 location: Optional[str] = Form(None),
-                 from_date: Optional[datetime] = Form(...),
-                 to_date: Optional[datetime] = Form(...),
-                 content: str = Form(None),
-                 link_vc: str = Form(None),
-                 db: Session = Depends(get_db)
-                 ) -> Set[Union[str, int]]:
-    # To DO: לבדוק שהמשתמש הוא יוצר האירוע
-            #  אם לא זהה, להחזיר שגיאה
-    success = False
-    error_msg = ""
-    if not check_validation(from_date, to_date):
-        error_msg = "Error, Your date is invalid"
-        return {success, event_id, error_msg}
-    old_event = get_event_by_id(db=db, event_id=event_id)
-    
-
-    if old_event is None:
-        error_msg = "Error, An event does not exist"
-        return {success, event_id, error_msg}
-
-    props = {'title': event_title, 'location': location,
-             'start': from_date, 'end': to_date, 'content': content,
-             'VC_link': link_vc}
-    update_event = Event(id=event_id, owner_id=old_event.owner_id, **props)
-    # לבדוק מי הנמענים ואם ישנם
-    link_to_event = os.path.dirname(str(request.url))
-    # יצירת מייל לעדכונים:
-    title, body = prepre_mail_for_update_event(
-        old_event, update_event, link_to_event)
-
-    if 'from' not in body:
-        error_msg = "Error, No changes"
-        return {success, event_id, error_msg}
+def check_date_valitation(old_event: Event, event_update: Event):
+    """ Checks if the time change is possible, otherwise returns False"""
     try:
-        db.query(Event).filter(Event.id).update(
-            props, synchronize_session=False)
+        if not check_validation(event_update.start, event_update.end):
+            return False
+    except TypeError:
+        try:
+            if event_update.start and not event_update.end:
+                if not check_validation(event_update.start, old_event.end):
+                    return False
+            elif not check_validation(old_event.start, event_update.end):
+                return False
+        except TypeError:
+            return False
+    return True
+
+
+def update_event(event_id: int, event_items: Dict, db: Session) -> Optional[Event]:
+    # To do: Check if the user is the owner of the event.
+    if not bool(event_items):
+        return None
+    event_items.pop('id', None)
+    event_items.pop('owner_id', None)
+
+    try:
+        old_event = get_event_by_id(db=db, event_id=event_id)
+    except AttributeError:  # Problem connecting to db
+        return None
+
+    if old_event is None:   # No such event number.
+        return None
+
+    column_names = get_column_names_event(db)
+    for key in event_items.keys():
+        if key not in column_names:
+            return None
+
+    update = Event(**event_items)
+    if not check_date_valitation(old_event, update):
+        return None
+    try:
+        db.query(Event).filter(Event.id == event_id).update(
+            event_items, synchronize_session=False)
         db.commit()
-        success = True
-        # שליחת המיילים ואיפוס אישורי הגעה
-    except AttributeError:
-        error_msg = "Error occurred"
-    return {success, event_id, error_msg}
+        # To do: Sending emails and reset.
+    except (AttributeError, exc.SQLAlchemyError):
+        return None
+    return get_event_by_id(db=db, event_id=event_id)
--- tests/test_event.py
+++ tests/test_event.py
@@ -1,54 +1,48 @@
-from fastapi.testclient import TestClient
 from datetime import datetime
-from app.main import app
-from app.routers.event import prepre_mail_for_update_event
+
+import pytest
 from app.database.models import Event
+from app.main import app
+from app.routers.event import update_event
 
-client = TestClient(app)
+DATA_UPDATE_OPTIONS = [
+    {}, {"test": "test"}, {"start": "20.01.2020"},
+    {"start": datetime(2020, 2, 2), "end": datetime(2020, 1, 1)},
+    {"start": datetime(2030, 2, 2)}, {"end": datetime(1990, 1, 1)},
+    {"start": "2020-02-02", "end": "2021-12-21"},
+]
 
 
-def test_eventedit():
+def test_eventedit(client):
     response = client.get("/event/edit")
     assert response.status_code == 200
     assert b"Edit Event" in response.content
 
-def test_update_event(session, event):
-    data = {'event_id': event.id,
-            'event_title':event.title,
-            'from_date': event.start,
-            'to_date': datetime.now(),
-            'content': event.content,
-            'db': session,
-    }
-    assert client.post('event/1/edit', data=data).status_code == 200
-    assert b'Error' in client.post('event/1/edit', data=data).content
-def test_update_event2(session, event):
-    data = {'event_id': event.id,
-            'event_title':event.title,
-            'from_date': event.end,
-            'to_date': event.start,
-            'content': event.content,
-            'db': session,
-    }
-    assert client.post('event/1/edit', data=data).status_code == 200
-    assert b'date' in client.post('event/1/edit', data=data).content
-def test_update_event3(session, event):
-    data = {'event_id': event.id,
-            'event_title':event.title,
-            'from_date': event.start,
-            'to_date': datetime.now(),
-            'content': event.content,
-            'db': None,
-    }
-    assert client.post('event/1/edit', data=data).status_code == 200
-    assert b'Error' in client.post('event/1/edit', data=data).content
 
-def test_prepre_email(event):
-    new_event = Event(
-            title=event.title + '1',
-            start=event.end,
-            end=event.start,
-            content=event.content+'1')
-    link = 'test'
-    assert 'changed' in prepre_mail_for_update_event(event, new_event, link)[1]
-    assert 'updated' in prepre_mail_for_update_event(event, new_event, link)
+@pytest.mark.parametrize("data", DATA_UPDATE_OPTIONS)
+def test_invalid_update(data, event, session):
+    assert update_event(1, data, session) == None
+
+
+def test_successful_update(event, session):
+    data = {
+        "title": "successful",
+        "start": datetime(2021, 1, 20),
+        "end": datetime(2021, 1, 21),
+    }
+    assert type(update_event(1, data, session)) == Event
+    assert "successful" in update_event(1, data, session).title
+
+
+def test_update_db_close(event):
+    data = {
+        "title": "Problem connecting to db",
+    }
+    assert update_event(1, data, db=None) == None
+
+
+def test_update_event_does_not_exist(event, session):
+    data = {
+        "content": "An update test for an event does not exist"
+    }
+    assert update_event(5, data, db=session) == None
