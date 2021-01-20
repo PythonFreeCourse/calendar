12b28818fef76650905a244a6a294f6b60a0a161
Hadas Kedar <hadasit@gmail.com>
2021-01-20 17:48:37 +0200

    feat: get weather forecast - fix changes & add cache support

--- app/routers/weather_forecast.py
+++ app/routers/weather_forecast.py
@@ -6,7 +6,8 @@ import requests
 from app import config
 
 
-# This feature requires an API KEY - get yours free @ visual-crossing-weather.p.rapidapi.com
+# This feature requires an API KEY
+#  get yours free @ visual-crossing-weather.p.rapidapi.com
 
 SUCCESS_STATUS = 0
 ERROR_STATUS = -1
@@ -20,8 +21,9 @@ INVALID_YEAR = "Year is out of supported range"
 HISTORY_URL = "https://visual-crossing-weather.p.rapidapi.com/history"
 FORECAST_URL = "https://visual-crossing-weather.p.rapidapi.com/forecast"
 HEADERS = {'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com"}
-BASE_QUERY_STRING = {"aggregateHours": "24", "unitGroup": "metric", "dayStartTime": "00:00:01",
-                     "contentType": "json", "dayEndTime": "23:59:59", "shortColumnNames": "True"}
+BASE_QUERY_STRING = {"aggregateHours": "24", "unitGroup": "metric",
+                     "dayStartTime": "00:00:01", "contentType": "json",
+                     "dayEndTime": "23:59:59", "shortColumnNames": "True"}
 HISTORICAL_AVERAGE_NUM_OF_YEARS = 3
 NO_API_RESPONSE = "No response from server"
 
@@ -48,8 +50,10 @@ def freezeargs(func):
     """
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
-        args = tuple([frozendict.frozendict(arg) if isinstance(arg, dict) else arg for arg in args])
-        kwargs = {k: frozendict.frozendict(v) if isinstance(v, dict) else v for k, v in kwargs.items()}
+        args = tuple([frozendict.frozendict(arg)
+                      if isinstance(arg, dict) else arg for arg in args])
+        kwargs = {k: frozendict.frozendict(v) if isinstance(v, dict)
+                        else v for k, v in kwargs.items()}
         return func(*args, **kwargs)
     return wrapped
 
@@ -57,7 +61,7 @@ def freezeargs(func):
 @freezeargs
 @functools.lru_cache(maxsize=128, typed=False)
 def get_data_from_weather_api(url, input_query_string):
-    """ get the relevant weather data by calling the "Visual Crossing Weather" API.
+    """ get relevant weather data by calling "Visual Crossing Weather" API.
     Args:
         url (str) - API url.
         input_query_string (dict) - input for the API.
@@ -67,7 +71,8 @@ def get_data_from_weather_api(url, input_query_string):
     """
     HEADERS['x-rapidapi-key'] = config.WEATHER_API_KEY
     try:
-        response = requests.request("GET", url, headers=HEADERS, params=input_query_string)
+        response = requests.request("GET", url,
+                                    headers=HEADERS, params=input_query_string)
     except requests.exceptions.RequestException:
         return None, NO_API_RESPONSE
     if response.ok:
@@ -90,15 +95,18 @@ def get_historical_weather(input_date, location):
     """
     input_query_string = BASE_QUERY_STRING
     input_query_string["startDateTime"] = input_date.isoformat()
-    input_query_string["endDateTime"] = (input_date + datetime.timedelta(days=1)).isoformat()
+    input_query_string["endDateTime"] =\
+        (input_date + datetime.timedelta(days=1)).isoformat()
     input_query_string["location"] = location
-    api_json, error_text = get_data_from_weather_api(HISTORY_URL, input_query_string)
+    api_json, error_text =\
+        get_data_from_weather_api(HISTORY_URL, input_query_string)
     location_found = list(api_json.keys())[0]
     if api_json:
-        weather_data = {'MinTempCel': api_json[location_found]['values'][0]['mint'],
-                        'MaxTempCel': api_json[location_found]['values'][0]['maxt'],
-                        'Conditions': api_json[location_found]['values'][0]['conditions'],
-                        'Address': location_found}
+        weather_data = {
+            'MinTempCel': api_json[location_found]['values'][0]['mint'],
+            'MaxTempCel': api_json[location_found]['values'][0]['maxt'],
+            'Conditions': api_json[location_found]['values'][0]['conditions'],
+            'Address': location_found}
         return weather_data, None
     return None, error_text
 
@@ -114,22 +122,26 @@ def get_forecast_weather(input_date, location):
     """
     input_query_string = BASE_QUERY_STRING
     input_query_string["location"] = location
-    api_json, error_text = get_data_from_weather_api(FORECAST_URL, input_query_string)
+    api_json, error_text = get_data_from_weather_api(FORECAST_URL,
+                                                     input_query_string)
     location_found = list(api_json.keys())[0]
     if not api_json:
         return None, error_text
     for i in range(len(api_json[location_found]['values'])):
         # find relevant date from API output
-        if str(input_date) == api_json[location_found]['values'][i]['datetimeStr'][:10]:
-            weather_data = {'MinTempCel': api_json[location_found]['values'][i]['mint'],
-                            'MaxTempCel': api_json[location_found]['values'][i]['maxt'],
-                            'Conditions': api_json[location_found]['values'][i]['conditions'],
-                            'Address': location_found}
+        if str(input_date) ==\
+                api_json[location_found]['values'][i]['datetimeStr'][:10]:
+            weather_data = {
+                'MinTempCel': api_json[location_found]['values'][i]['mint'],
+                'MaxTempCel': api_json[location_found]['values'][i]['maxt'],
+                'Conditions': api_json[location_found]['values'][i]['conditions'],
+                'Address': location_found}
             return weather_data, None
 
 
 def get_history_relevant_year(day, month):
-    """ return the relevant year in order to call the get_historical_weather function with.
+    """ return the relevant year in order to call the
+        get_historical_weather function with.
         decided according to if date occurred this year or not.
     Args:
         day (int) - day part of date.
@@ -138,9 +150,12 @@ def get_history_relevant_year(day, month):
         last_year (int) - relevant year.
     """
     try:
-        relevant_date = datetime.datetime(year=datetime.datetime.now().year, month=month, day=day)
-    except ValueError:  # only if the day & month are 29.02 and there is no such date this year
-        relevant_date = datetime.datetime(year=datetime.datetime.now().year, month=month, day=day - 1)
+        relevant_date = datetime.datetime(year=datetime.datetime.now().year,
+                                          month=month, day=day)
+    except ValueError:
+        # only if day & month are 29.02 and there is no such date this year
+        relevant_date = datetime.datetime(year=datetime.datetime.now().year,
+                                          month=month, day=day - 1)
     if datetime.datetime.now() > relevant_date:
         last_year = datetime.datetime.now().year
     else:
@@ -149,7 +164,8 @@ def get_history_relevant_year(day, month):
 
 
 def get_forecast_by_historical_data(day, month, location):
-    """ get historical average weather by calling the get_historical_weather function.
+    """ get historical average weather by calling the
+        get_historical_weather function.
     Args:
         day (int) - day part of date.
         month (int) - month part of date.
@@ -160,10 +176,13 @@ def get_forecast_by_historical_data(day, month, location):
     """
     relevant_year = get_history_relevant_year(day, month)
     try:
-        input_date = datetime.datetime(year=relevant_year, month=month, day=day)
+        input_date = datetime.datetime(year=relevant_year, month=month,
+                                       day=day)
     except ValueError:
-        # only if the day & month are 29.02 and there is no such date on the relevant year
-        input_date = datetime.datetime(year=relevant_year, month=month, day=day - 1)
+        # only if day & month are 29.02 and there is no such date
+        # on the relevant year
+        input_date = datetime.datetime(year=relevant_year, month=month,
+                                       day=day - 1)
     return get_historical_weather(input_date, location)
 
 
@@ -195,9 +214,11 @@ def get_forecast(requested_date, location):
     """
     forecast_type = get_forecast_type(requested_date)
     if forecast_type == HISTORY_TYPE:
-        weather_json, error_text = get_historical_weather(requested_date, location)
+        weather_json, error_text = get_historical_weather(requested_date,
+                                                          location)
     if forecast_type == FORECAST_TYPE:
-        weather_json, error_text = get_forecast_weather(requested_date, location)
+        weather_json, error_text = get_forecast_weather(requested_date,
+                                                        location)
     if forecast_type == HISTORICAL_FORECAST_TYPE:
         weather_json, error_text = get_forecast_by_historical_data(
             requested_date.day, requested_date.month, location)
@@ -222,11 +243,12 @@ def get_weather_data(requested_date, location):
             "forecast" - relevant for the upcoming 15 days.
             "history" - historical data.
             "historical average" - average of the last 3 years on that date.
-                                    relevant for future dates (more then forecast).
+                            relevant for future dates (more then forecast).
         Address - The location found by the service.
     """
     output = {}
-    requested_date = datetime.date(requested_date.year, requested_date.month, requested_date.day)
+    requested_date = datetime.date(requested_date.year, requested_date.month,
+                                   requested_date.day)
     valid_input, error_text = validate_date_input(requested_date)
     if valid_input:
         weather_json, error_text = get_forecast(requested_date, location)
@@ -236,8 +258,10 @@ def get_weather_data(requested_date, location):
         else:
             output["Status"] = SUCCESS_STATUS
             output["ErrorDescription"] = None
-            output["MinTempFar"] = round((weather_json['MinTempCel'] * 9/5) + 32)
-            output["MaxTempFar"] = round((weather_json['MaxTempCel'] * 9/5) + 32)
+            output["MinTempFar"] = round((weather_json['MinTempCel'] * 9/5)
+                                         + 32)
+            output["MaxTempFar"] = round((weather_json['MaxTempCel'] * 9/5)
+                                         + 32)
             output.update(weather_json)
     else:
         output["Status"] = ERROR_STATUS
--- tests/test_weather_forecast.py
+++ tests/test_weather_forecast.py
@@ -5,17 +5,25 @@ from app.routers.weather_forecast import get_weather_data
 
 
 DATA_GET_WEATHER = [
-    pytest.param(2020, "tel aviv", 0, marks=pytest.mark.xfail, id="invalid input type"),
-    pytest.param(datetime.datetime(day=4, month=4, year=2070), "tel aviv", 0, marks=pytest.mark.xfail, id="year out of range"),
-    pytest.param(datetime.datetime(day=4, month=4, year=2020), "tel aviv", 0, id="basic historical test"),
-    pytest.param(datetime.datetime(day=1, month=1, year=2030), "tel aviv", 0, id="basic historical forecast test - prior in current year"),
-    pytest.param(datetime.datetime(day=31, month=12, year=2030), "tel aviv", 0, id="basic historical forecast test - future"),
-    pytest.param(datetime.datetime(day=29, month=2, year=2024), "tel aviv", 0, id="basic historical forecast test"),
-    pytest.param(datetime.datetime(day=15, month=1, year=2020), "neo", 0, marks=pytest.mark.xfail, id="location not found test"),
+    pytest.param(2020, "tel aviv", 0, marks=pytest.mark.xfail,
+                 id="invalid input type"),
+    pytest.param(datetime.datetime(day=4, month=4, year=2070), "tel aviv", 0,
+                 marks=pytest.mark.xfail, id="year out of range"),
+    pytest.param(datetime.datetime(day=4, month=4, year=2020),
+                 "tel aviv", 0, id="basic historical test"),
+    pytest.param(datetime.datetime(day=1, month=1, year=2030), "tel aviv", 0,
+                 id="basic historical forecast test - prior in current year"),
+    pytest.param(datetime.datetime(day=31, month=12, year=2030),
+                 "tel aviv", 0, id="basic historical forecast test - future"),
+    pytest.param(datetime.datetime(day=29, month=2, year=2024), "tel aviv",
+                 0, id="basic historical forecast test"),
+    pytest.param(datetime.datetime(day=15, month=1, year=2020), "neo", 0,
+                 marks=pytest.mark.xfail, id="location not found test"),
 ]
 
 
-@pytest.mark.parametrize('requested_date, location, expected', DATA_GET_WEATHER)
+@pytest.mark.parametrize('requested_date, location, expected',
+                         DATA_GET_WEATHER)
 def test_get_weather_data(requested_date, location, expected):
     output = get_weather_data(requested_date, location)
     assert output['Status'] == expected
