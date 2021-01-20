9919f2e0b1ededfe19e49ed02e79649289a40ec7
Adva Alkalay <adva.alkalay@gmail.com>
2021-01-20 12:46:26 +0200

    help

--- app/database/database.py
+++ app/database/database.py
@@ -5,11 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_CONNECTION_STRING")
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:h1h2h3h4@localhost/postgres"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
