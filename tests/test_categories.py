6700546db09000bd08c4b543aa1c26418312eceb
i <inbvar@gmail.com>
2021-01-22 00:10:42 +0200

    Linter

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -7,47 +7,58 @@ from app.routers.categories import get_user_categories, validate_request_params
 
 
 class TestCategories:
-    CATEGORY_ALREADY_EXISTS_MESSAGE = "category is already exists for {0} user."
+    CATEGORY_ALREADY_EXISTS_MSG = "category is already exists for"
     UNALLOWED_PARAMS = "contains unallowed params"
 
     @staticmethod
     def test_creating_new_category(client, user):
-        response = client.post("/categories/", json={"user_id": user.id, "name": "Foo", "color": "eecc11"})
+        response = client.post("/categories/", json={"user_id": user.id,
+                                                     "name": "Foo", "color": "eecc11"})
         assert response.status_code == status.HTTP_200_OK
-        assert {"user_id": user.id, "name": "Foo", "color": "eecc11"}.items() <= response.json()['category'].items()
+        assert {"user_id": user.id, "name": "Foo", "color": "eecc11"}.items() \
+               <= response.json()['category'].items()
 
     @staticmethod
     def test_creating_not_unique_category_failed(client, user, category):
-        response = client.post("/categories/", json={"user_id": user.id, "name": "Guitar Lesson", "color": "121212"})
+        response = client.post("/categories/", json={"user_id": user.id,
+                                                     "name": "Guitar Lesson", "color": "121212"})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert response.json()["detail"] == TestCategories.CATEGORY_ALREADY_EXISTS_MESSAGE.format(user.id)
+        assert TestCategories.CATEGORY_ALREADY_EXISTS_MSG in \
+               response.json()["detail"]
 
     @staticmethod
     def test_create_event_with_category(category):
-        event = Event(title="OOO", content="Guitar rocks!!", owner_id=category.user_id, category_id=category.id)
+        event = Event(title="OOO", content="Guitar rocks!!",
+                      owner_id=category.user_id, category_id=category.id)
         assert event.category_id is not None
         assert event.category_id == category.id
 
     @staticmethod
     def test_get_user_categories(client, category):
-        response = client.get(f"/categories/?user_id={category.user_id}&name={category.name}&color={category.color}")
+        response = client.get(f"/categories/?user_id={category.user_id}"
+                              f"&name={category.name}&color={category.color}")
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == [
-            {"user_id": category.user_id, "color": "121212", "name": "Guitar Lesson", "id": category.id}]
+            {"user_id": category.user_id, "color": "121212",
+             "name": "Guitar Lesson", "id": category.id}]
 
     @staticmethod
     def test_get_category_by_name(client, user, category):
-        response = client.get(f"/categories/?user_id={category.user_id}&name={category.name}")
+        response = client.get(f"/categories/?user_id={category.user_id}"
+                              f"&name={category.name}")
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == [
-            {"user_id": category.user_id, "color": "121212", "name": "Guitar Lesson", "id": category.id}]
+            {"user_id": category.user_id, "color": "121212",
+             "name": "Guitar Lesson", "id": category.id}]
 
     @staticmethod
     def test_get_category_by_color(client, user, category):
-        response = client.get(f"/categories/?user_id={category.user_id}&color={category.color}")
+        response = client.get(f"/categories/?user_id={category.user_id}&"
+                              f"color={category.color}")
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == [
-            {"user_id": category.user_id, "color": "121212", "name": "Guitar Lesson", "id": category.id}]
+            {"user_id": category.user_id, "color": "121212",
+             "name": "Guitar Lesson", "id": category.id}]
 
     @staticmethod
     def test_get_category_bad_request(client):
@@ -57,11 +68,13 @@ class TestCategories:
 
     @staticmethod
     def test_repr(category):
-        assert category.__repr__() == f'<Category {category.id} {category.name} {category.color}>'
+        assert category.__repr__() == \
+               f'<Category {category.id} {category.name} {category.color}>'
 
     @staticmethod
     def test_to_dict(category):
-        assert {c.name: getattr(category, c.name) for c in category.__table__.columns} == category.to_dict()
+        assert {c.name: getattr(category, c.name) for c in
+                category.__table__.columns} == category.to_dict()
 
     @staticmethod
     def test_get_categories_logic_succeeded(session, user, category):
@@ -69,13 +82,15 @@ class TestCategories:
 
     @staticmethod
     @pytest.mark.parametrize('params, expected_result', [
-        (ImmutableMultiDict([('user_id', ''), ('name', ''), ('color', '')]), True),
+        (ImmutableMultiDict([('user_id', ''), ('name', ''),
+                             ('color', '')]), True),
         (ImmutableMultiDict([('user_id', ''), ('name', '')]), True),
         (ImmutableMultiDict([('user_id', ''), ('color', '')]), True),
         (ImmutableMultiDict([('user_id', '')]), True),
         (ImmutableMultiDict([('name', ''), ('color', '')]), False),
         (ImmutableMultiDict([]), False),
-        (ImmutableMultiDict([('user_id', ''), ('name', ''), ('color', ''), ('bad_param', '')]), False),
+        (ImmutableMultiDict([('user_id', ''), ('name', ''), ('color', ''),
+                             ('bad_param', '')]), False),
     ])
     def test_validate_request_params(params, expected_result):
         assert validate_request_params(params) == expected_result
