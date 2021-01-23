e2cd3387cde2159cd68c52ef83f2285b7325f4a0
Amir Pollak <pollakamir34@gmail.com>
2021-01-23 17:28:59 +0200

    Added checking for zoom link and saving the event

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,6 +1,35 @@
 from fastapi.testclient import TestClient
+from starlette.status import HTTP_303_SEE_OTHER
 
 from app.main import app
+
+CORRECT_EVENT_FORM_DATA = {
+    'title': 'test title',
+    'start_date': '2021-01-28',
+    'start_time': '15:59',
+    'end_date': '2021-01-27',
+    'end_time': '15:01',
+    'location_type': 'vc_url',
+    'location': 'https://us02web.zoom.us/j/875384596',
+    'description': 'content',
+    'color': 'red',
+    'availability': 'busy',
+    'privacy': 'public'
+}
+
+WRONG_EVENT_FORM_DATA = {
+    'title': 'test title',
+    'start_date': '2021-01-28',
+    'start_time': '15:59',
+    'end_date': '2021-01-27',
+    'end_time': '15:01',
+    'location_type': 'vc_url',
+    'location': 'not a zoom link',
+    'description': 'content',
+    'color': 'red',
+    'availability': 'busy',
+    'privacy': 'public'
+}
 
 client = TestClient(app)
 
@@ -9,6 +38,17 @@ def test_eventedit():
     response = client.get("/event/edit")
     assert response.status_code == 200
     assert b"Edit Event" in response.content
+
+
+def test_eventedit_post_correct(user):
+    response = client.post("/event/edit", data=CORRECT_EVENT_FORM_DATA)
+    assert response.status_code == HTTP_303_SEE_OTHER
+    assert '/event/view/' in response.headers['location']
+
+
+def test_eventedit_post_wrong(user):
+    response = client.post("/event/edit", data=WRONG_EVENT_FORM_DATA)
+    assert response.json()['detail'] == 'VC type with no valid zoom link'
 
 
 def test_eventview_with_id():
