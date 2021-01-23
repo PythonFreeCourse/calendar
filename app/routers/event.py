525120dc27e3497f576be04354b97024dfcca49f
Amir Pollak <pollakamir34@gmail.com>
2021-01-23 17:57:58 +0200

    Flake8 fixes

--- app/routers/event.py
+++ app/routers/event.py
@@ -8,7 +8,7 @@ from app.database.database import get_db
 from app.database.models import Event, User
 from app.dependencies import templates
 
-ZOOM_REGEX = re.compile('https://.*?\.zoom.us/[a-z]/.[^.,\b\t\n]+')
+ZOOM_REGEX = re.compile(r'https://.*?\.zoom.us/[a-z]/.[^.,\b\t\n]+')
 
 router = APIRouter(
     prefix="/event",
@@ -29,8 +29,10 @@ async def create_event(request: Request, session=Depends(get_db)):
     print(data)
     title = data['title']
     content = data['description']
-    start = dt.strptime(data['start_date'] + ' ' + data['start_time'], '%Y-%m-%d %H:%M')
-    end = dt.strptime(data['end_date'] + ' ' + data['end_time'], '%Y-%m-%d %H:%M')
+    start = dt.strptime(data['start_date'] + ' ' + data['start_time'],
+                        '%Y-%m-%d %H:%M')
+    end = dt.strptime(data['end_date'] + ' ' + data['end_time'],
+                      '%Y-%m-%d %H:%M')
     user = session.query(User).filter_by(id=1).first()
     owner_id = user.id
     location_type = data['location_type']
@@ -38,11 +40,14 @@ async def create_event(request: Request, session=Depends(get_db)):
     location = data['location']
 
     if is_zoom and not ZOOM_REGEX.findall(location):
-        raise HTTPException(status_code=400, detail="VC type with no valid zoom link")
-    event = Event(title=title, content=content, start=start, end=end, owner_id=owner_id)
+        raise HTTPException(status_code=400,
+                            detail="VC type with no valid zoom link")
+    event = Event(title=title, content=content, start=start, end=end,
+                  owner_id=owner_id)
     session.add(event)
     session.commit()
-    return RedirectResponse(f'/event/view/{event.id}', status_code=HTTP_303_SEE_OTHER)
+    return RedirectResponse(f'/event/view/{event.id}',
+                            status_code=HTTP_303_SEE_OTHER)
 
 
 @router.get("/view/{id}")
