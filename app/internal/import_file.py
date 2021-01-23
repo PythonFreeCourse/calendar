ed5704deea028b10b76394c3a1b4d44fbb9f92ef
Nir Perelshtein <nirpn1@gmail.com>
2021-01-23 23:13:39 +0200

    fix: change functions to support start and end dates plus code readability

--- app/internal/import_file.py
+++ app/internal/import_file.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from pathlib import Path
 import re
 from typing import Any, Dict, List, Tuple, Union
 
@@ -9,7 +10,7 @@ from app.database.models import Event
 from app.database.database import SessionLocal
 
 
-NUM_OF_VALUES = 3  # Event contains head, content and date.
+NUM_OF_VALUES = 4  # Event contains head, content, start_date and end_date.
 MAX_FILE_SIZE_MB = 5  # 5MB
 VALID_FILE_EXTENSION = (".txt", ".csv", ".ics")  # Can import only these files.
 VALID_YEARS = 20  # Events must be within 20 years range from the current year.
@@ -19,87 +20,85 @@ EVENT_CONTENT_LIMIT = 500  # Max characters for event characters.
 MAX_EVENTS_START_DATE = 10  # Max Events with the same start date.
 
 
-def check_file_size(file: str, max_size: int = MAX_FILE_SIZE_MB) -> bool:
+def is_file_size_valid(file: str, max_size: int = MAX_FILE_SIZE_MB) -> bool:
     file_size = os.stat(file).st_size / 1048576  # convert bytes to MB.
     return file_size <= max_size
 
 
-def check_file_extension(file: str,
-                         extension: Union[str, Tuple[str, ...]]
-                         = VALID_FILE_EXTENSION) -> bool:
+def is_file_extension_valid(file: str,
+                            extension: Union[str, Tuple[str, ...]]
+                            = VALID_FILE_EXTENSION) -> bool:
     return file.lower().endswith(extension)
 
 
 def is_file_exist(file: str) -> bool:
-    try:
-        with open(file, "r"):
-            pass
+    if Path(file).is_file():
         return True
-    except (FileNotFoundError, OSError):
-        return False
+    return False
 
 
-def check_date_in_range(date1: Union[str, datetime.datetime],
-                        valid_dates: int = VALID_YEARS) -> bool:
+def is_date_in_range(date: Union[str, datetime.datetime],
+                     valid_dates: int = VALID_YEARS) -> bool:
     """
     check if date is valid and in the range according to the rule we have set
     """
     now_year = datetime.datetime.now().year
-    if isinstance(date1, str):
+    if isinstance(date, str):
         try:
-            check_date = datetime.datetime.strptime(date1, "%m-%d-%Y")
+            check_date = datetime.datetime.strptime(date, "%m-%d-%Y")
         except ValueError:
             return False
     else:
-        check_date = date1
+        check_date = date
     if check_date.year > now_year + valid_dates or \
        check_date.year < now_year - valid_dates:
         return False
     return True
 
 
-def check_validity_of_txt(row: str) -> bool:
+def is_event_text_valid(row: str) -> bool:
     """Check if the row contains valid data"""
     get_values = re.findall(r"^(\w{" + str(EVENT_HEADER_NOT_EMPTY) + "," +
-                            str(EVENT_HEADER_LIMIT) + r"})\,\s(\w{0," +
+                            str(EVENT_HEADER_LIMIT) + r"}),\s(\w{0," +
                             str(EVENT_CONTENT_LIMIT) +
-                            r"})\,\s(\d{2}\-\d{2}\-\d{4})$", row)
+                            r"}),\s(\d{2}-\d{2}-\d{4})," +
+                            r"\s(\d{2}-\d{2}-\d{4})$", row)
     if get_values:
         if len(get_values[0]) == NUM_OF_VALUES:
             return True
     return False
 
 
-def before_import_checking(file: str) -> bool:
+def is_file_valid_to_import(file: str) -> bool:
     """
     checking before importing that the file exist, the file extension and
     the size meet the rules we have set.
     """
     if not is_file_exist(file):
         return False
-    if not check_file_extension(file):
+    if not is_file_extension_valid(file):
         return False
-    if not check_file_size(file):
+    if not is_file_size_valid(file):
         return False
     return True
 
 
-def after_import_checking(calendar1: List[Dict[str, Union[str, Any]]],
-                          max_event_start_date: int
-                          = MAX_EVENTS_START_DATE) -> bool:
+def is_file_valid_to_save_to_database(events: List[Dict[str, Union[str, Any]]],
+                                      max_event_start_date: int
+                                      = MAX_EVENTS_START_DATE) -> bool:
     """
     checking after importing that there is no larger quantity of events
     with the same date according to the rule we have set.
     """
     same_date_counter = 1
     date_n_count = {}
-    for event in calendar1:
-        if event["Date"] in date_n_count:
-            date_n_count[event["Date"]] += 1
-            if date_n_count[event["Date"]] > same_date_counter:
-                same_date_counter = date_n_count[event["Date"]]
+    for event in events:
+        if event["S_Date"] in date_n_count:
+            date_n_count[event["S_Date"]] += 1
+            if date_n_count[event["S_Date"]] > same_date_counter:
+                same_date_counter = date_n_count[event["S_Date"]]
         else:
-            date_n_count[event["Date"]] = 1
+            date_n_count[event["S_Date"]] = 1
     return same_date_counter <= max_event_start_date
 
 
@@ -108,16 +107,19 @@ def import_txt_file(txt_file: str) -> List[Dict[str, Union[str, Any]]]:
     with open(txt_file, "r") as text:
         events = text.readlines()
     for event in events:
-        if not check_validity_of_txt(event):
+        if not is_event_text_valid(event):
             return list()
-        head, content, event_date = event.split(", ")
-        if not check_date_in_range(event_date.replace("\n", "")):
+        head, content, start_date, end_date = event.split(", ")
+        if not is_date_in_range(start_date) or \
+           not is_date_in_range(end_date.replace("\n", "")):
             return list()
-        event_date = datetime.datetime.strptime(event_date.replace("\n", ""),
-                                                "%m-%d-%Y")
+        start_date = datetime.datetime.strptime(start_date, "%m-%d-%Y")
+        end_date = datetime.datetime.strptime(end_date.replace("\n", ""),
+                                              "%m-%d-%Y")
         calendar_content.append({"Head": head,
                                  "Content": content,
-                                 "Date": event_date})
+                                 "S_Date": start_date,
+                                 "E_Date": end_date})
     return calendar_content
 
 
@@ -132,27 +134,32 @@ def import_ics_file(ics_file: str) -> List[Dict[str, Union[str, Any]]]:
             if component.name == "VEVENT":
                 if str(component.get('summary')) is None or \
                    component.get('dtstart') is None or \
-                   not check_date_in_range(component.get('dtstart').dt):
+                   component.get('dtend') is None or \
+                   not is_date_in_range(component.get('dtstart').dt) or \
+                   not is_date_in_range(component.get('dtend').dt):
                     return list()
                 else:
                     calendar_content.append({
                         "Head": str(component.get('summary')),
                         "Content": str(component.get('description')),
-                        "Date": component.get('dtstart').dt
+                        "S_Date": component.get('dtstart').dt
+                        .replace(tzinfo=None),
+                        "E_Date": component.get('dtend').dt
                         .replace(tzinfo=None)
                     })
     return calendar_content
 
 
-def move_events_to_db(events: List[Dict[str, Union[str, Any]]],
-                      user_id: int,
-                      session: SessionLocal = SessionLocal()) -> None:
+def save_events_to_database(events: List[Dict[str, Union[str, Any]]],
+                            user_id: int,
+                            session: SessionLocal = SessionLocal()) -> None:
     """insert the events into Event table"""
     for event in events:
         event = Event(
             title=event["Head"],
             content=event["Content"],
-            date=event["Date"],
+            start=event["S_Date"],
+            end=event["E_Date"],
             owner_id=user_id
         )
         session.add(event)
@@ -166,13 +173,13 @@ def user_click_import(file: str, user_id: int,
     when user choose a file and click import, we are checking the file
     and if everything is ok we will insert the data to DB
     """
-    if before_import_checking(file):
-        if file.lower().endswith(VALID_FILE_EXTENSION[-1]):
+    if is_file_valid_to_import(file):
+        if is_file_extension_valid(file, ".ics"):
             import_file = import_ics_file(file)
         else:
             import_file = import_txt_file(file)
         if import_file:
-            if after_import_checking(import_file):
-                move_events_to_db(import_file, user_id, session)
-                return "Import success"
-    return "Import failed"
+            if is_file_valid_to_save_to_database(import_file):
+                save_events_to_database(import_file, user_id, session)
+                return True
+    return False
--- tests/test_import_file.py
+++ tests/test_import_file.py
@@ -3,46 +3,72 @@ import datetime
 import pytest
 
 from app.internal.import_file import (
-    before_import_checking, check_date_in_range, check_file_extension,
-    check_file_size, check_validity_of_txt, import_ics_file, import_txt_file,
-    is_file_exist, user_click_import)
+    import_ics_file, import_txt_file, is_date_in_range, is_event_text_valid,
+    is_file_exist, is_file_extension_valid, is_file_size_valid,
+    is_file_valid_to_import, user_click_import)
+
+
+FILE_TXT_SAMPLE = r"tests/files_for_import_file_tests/sample_calendar_data.txt"
+FILE_CSV_SAMPLE = r"tests/files_for_import_file_tests/sample_calendar_data.csv"
+FILE_TXT_ABOVE_5MB = r"tests/files_for_import_file_tests/sample_above_5mb.txt"
+FILE_TXT_BELOW_1MB = r"tests/files_for_import_file_tests/sample_below_1mb.txt"
+FILE_CSV_BELOW_1MB = r"tests/files_for_import_file_tests/sample_below_1mb.csv"
+FILE_NOT_EXIST = r"tests/files_for_import_file_tests/not_exist.txt"
+FILE_ICS = r"tests/files_for_import_file_tests/sample.ics"
+FILE_ICS_INVALID_DATA = r"tests/files_for_import_file_tests/sample2.ics"
+NOT_EXIST_BLABLA_EXTENSION = r"tests/files_for_import_file_tests/sample.blabla"
+FILE_BLABLA_EXTENSION = r"tests/files_for_import_file_tests/sample2.blabla"
+
+
+IMPORT_TXT_FILE_RESULT_DATA = [
+    {'Head': 'Head1', 'Content': 'Content1',
+     'S_Date': datetime.datetime(2019, 5, 21, 0, 0),
+     'E_Date': datetime.datetime(2019, 5, 21, 0, 0)},
+    {'Head': 'Head2', 'Content': 'Content2',
+     'S_Date': datetime.datetime(2010, 1, 11, 0, 0),
+     'E_Date': datetime.datetime(2010, 1, 11, 0, 0)},
+    {'Head': 'Head3', 'Content': 'Content3',
+     'S_Date': datetime.datetime(2022, 2, 2, 0, 0),
+     'E_Date': datetime.datetime(2022, 2, 2, 0, 0)}
+]
 
 
 file_size_tests = [
     # File size above the constraint we set in the global variable (5 MB)
-    (r"tests/files_for_import_file_tests/sample_above_5mb.txt", False),
+    (FILE_TXT_ABOVE_5MB, None, False),
     # File size below the constraint we set in the global variable (5 MB)
-    (r"tests/files_for_import_file_tests/sample_below_1mb.txt", True),
+    (FILE_TXT_BELOW_1MB, None, True),
     # File size below the constraint we set to the function
-    (r"tests/files_for_import_file_tests/sample_above_5mb.txt", 10, True),
+    (FILE_TXT_ABOVE_5MB, 10, True),
     # File size above the constraint we set to the function
-    (r"tests/files_for_import_file_tests/sample_above_5mb.txt", 2, False)
+    (FILE_TXT_ABOVE_5MB, 2, False)
 ]
 
 file_exist_tests = [
     # File exist
-    (r"tests/files_for_import_file_tests/sample_below_1mb.txt", True),
+    (FILE_TXT_BELOW_1MB, True),
     # File not exist
-    (r"tests/files_for_import_file_tests/not_exist.txt", False)
+    (FILE_NOT_EXIST, False)
 ]
 
 file_extension_tests = [
-    (r"tests/files_for_import_file_tests/sample_below_1mb.txt", True),
-    (r"tests/files_for_import_file_tests/sample_below_1mb.csv", True),
-    (r"tests/files_for_import_file_tests/sample.ics", True),
-    (r"tests/files_for_import_file_tests/sample.blabla", False),
-    (r"tests/files_for_import_file_tests/sample.csv.blabla", False)
+    (FILE_TXT_BELOW_1MB, None, True),
+    (FILE_CSV_BELOW_1MB, None, True),
+    (FILE_ICS, None, True),
+    (NOT_EXIST_BLABLA_EXTENSION, ".blabla", True),
+    (NOT_EXIST_BLABLA_EXTENSION, None, False),
+    (r"tests/files_for_import_file_tests/sample.csv.blabla", None, False)
 ]
 
 date_in_range_tests = [
-    ("01-31-2022", True),
-    ("01-32-2022", False),
-    ("20-02-2022", False),
-    ("1-20-2011", True),
+    ("01-31-2022", None, True),
+    ("01-32-2022", None, False),
+    ("20-02-2022", None, False),
+    ("1-20-2011", None, True),
     # date above the constraint we set (20 years after today date)
-    ("01-01-2050", False),
+    ("01-01-2050", None, False),
     # date below the constraint we set (20 years before today date)
-    ("01-01-2000", False),
+    ("01-01-2000", None, False),
     # date in the range we set in the function
     ("01-01-2019", 5, True),
     # date below the range we set in the function
@@ -50,127 +76,109 @@ date_in_range_tests = [
 ]
 
 check_validity_of_text_tests = [
-    (r"Head1, Content1, 05-21-2019", True),
-    (r"Head1Content1, 05-21-2019", False),
+    (r"Head1, Content1, 05-21-2019, 05-21-2019", True),
+    (r"Head1Content1, 05-21-2019, 05-21-2019", False),
     # title can't be empty
-    (r"  , Content1, 05-21-2019", False),
+    (r"  , Content1, 05-21-2019, 05-21-2019", False),
     # content may be empty
-    (r"Head1, , 05-21-2019", True),
-    # date can't be empty
-    (r"Head1, Content1, ", False),
+    (r"Head1, , 05-21-2019, 05-21-2019", True),
+    # start_date can't be empty
+    (r"Head1, Content1, , 05-21-2019", False),
+    # end_date can't be empty
+    (r"Head1, Content1, 05-21-2019, ", False),
     # row cant have multiple events
-    (r"Head1, Content1, 05-21-2019, Head2, Content2, 05-21-2019", False)
+    (r"""Head1, Content1, 05-21-2019, 05-21-2019,
+     Head2, Content2, 05-21-2019, 05-21-2019""", False)
 ]
 
 import_txt_file_tests = [
     # txt file
-    (r"tests/files_for_import_file_tests/sample_calendar_data_to_import_1.txt",
-        [
-            {'Head': 'Head1', 'Content': 'Content1',
-             'Date': datetime.datetime(2019, 5, 21, 0, 0)},
-            {'Head': 'Head2', 'Content': 'Content2',
-             'Date': datetime.datetime(2010, 1, 11, 0, 0)},
-            {'Head': 'Head3', 'Content': 'Content3',
-             'Date': datetime.datetime(2022, 2, 2, 0, 0)}
-        ]),
+    (FILE_TXT_SAMPLE, IMPORT_TXT_FILE_RESULT_DATA),
     # csv file
-    (r"tests/files_for_import_file_tests/sample_calendar_data_to_import_1.csv",
-        [
-            {'Head': 'Head1', 'Content': 'Content1',
-             'Date': datetime.datetime(2019, 5, 21, 0, 0)},
-            {'Head': 'Head2', 'Content': 'Content2',
-             'Date': datetime.datetime(2010, 1, 11, 0, 0)},
-            {'Head': 'Head3', 'Content': 'Content3',
-             'Date': datetime.datetime(2022, 2, 2, 0, 0)}
-        ])
+    (FILE_CSV_SAMPLE, IMPORT_TXT_FILE_RESULT_DATA)
 ]
 
 import_ics_tests = [
     # ics file
-    (r"tests/files_for_import_file_tests/sample.ics",
+    (FILE_ICS,
         [
             {'Head': 'HeadA', 'Content': 'Content1',
-             'Date': datetime.datetime(2019, 8, 2, 10, 34)},
+             'S_Date': datetime.datetime(2019, 8, 2, 10, 34),
+             'E_Date': datetime.datetime(2019, 8, 2, 11, 4)},
             {'Head': 'HeadB', 'Content': 'Content2',
-             'Date': datetime.datetime(2019, 8, 2, 20, 0)}
+             'S_Date': datetime.datetime(2019, 8, 2, 20, 0),
+             'E_Date': datetime.datetime(2019, 8, 2, 20, 30)
+             }
         ]),
     # ics file
-    (r"tests/files_for_import_file_tests/sample2.ics", [])
+    (FILE_ICS_INVALID_DATA, list())
 ]
 
-before_import_checking_tests = [
-    (r"tests/files_for_import_file_tests/sample_calendar_data_to_import_1.txt",
-     True),
-    (r"tests/files_for_import_file_tests/sample_above_5mb.txt", False),
-    (r"tests/files_for_import_file_tests/sample.blabla", False),
-    (r"tests/files_for_import_file_tests/sample2.blabla", False)
+is_file_valid_to_import_tests = [
+    (FILE_TXT_SAMPLE, True),
+    (FILE_TXT_ABOVE_5MB, False),
+    (NOT_EXIST_BLABLA_EXTENSION, False),
+    (FILE_BLABLA_EXTENSION, False)
 ]
-
 
 user_click_import_tests = [
-    (r"tests/files_for_import_file_tests/sample_calendar_data_to_import_1.txt",
-     1, "Import success"),
-    (r"tests/files_for_import_file_tests/sample.ics", 1,
-     "Import success"),
-    (r"tests/files_for_import_file_tests/sample_above_5mb.txt", 1,
-     "Import failed"),
-    (r"tests/files_for_import_file_tests/sample.blabla", 1,
-     "Import failed"),
-    (r"tests/files_for_import_file_tests/sample_same_date_many_times.txt", 1,
-     "Import failed")
+    (FILE_TXT_SAMPLE, 1, True),
+    (FILE_ICS, 1, True),
+    (FILE_TXT_ABOVE_5MB, 1, False),
+    (NOT_EXIST_BLABLA_EXTENSION, 1, False),
+    (FILE_TXT_BELOW_1MB, 1, False)
 ]
 
 
-@pytest.mark.parametrize("file", file_size_tests)
-def test_check_file_size(file):
-    if len(file) == 2:
-        assert check_file_size(file[0]) == file[1]
+@pytest.mark.parametrize("file1, max_size, result", file_size_tests)
+def test_is_file_size_valid(file1, max_size, result):
+    if max_size is None:
+        assert is_file_size_valid(file1) == result
     else:
-        assert check_file_size(file[0], file[1]) == file[2]
+        assert is_file_size_valid(file1, max_size) == result
 
 
-@pytest.mark.parametrize("file", file_exist_tests)
-def test_is_file_exist(file):
-    assert is_file_exist(file[0]) == file[1]
+@pytest.mark.parametrize("file1, result", file_exist_tests)
+def test_is_file_exist(file1, result):
+    assert is_file_exist(file1) == result
 
 
-@pytest.mark.parametrize("file", file_extension_tests)
-def test_check_file_extension(file):
-    if len(file) == 2:
-        assert check_file_extension(file[0]) == file[1]
+@pytest.mark.parametrize("file1, extension, result", file_extension_tests)
+def test_is_file_extension_valid(file1, extension, result):
+    if extension is None:
+        assert is_file_extension_valid(file1) == result
     else:
-        assert check_file_extension(file[0], file[1]) == file[2]
+        assert is_file_extension_valid(file1, extension) == result
 
 
-@pytest.mark.parametrize("date_n_stat", date_in_range_tests)
-def test_check_date_in_range(date_n_stat):
-    if len(date_n_stat) == 2:
-        assert check_date_in_range(date_n_stat[0]) == date_n_stat[1]
+@pytest.mark.parametrize("date, valid_dates, result", date_in_range_tests)
+def test_is_date_in_range(date, valid_dates, result):
+    if valid_dates is None:
+        assert is_date_in_range(date) == result
     else:
-        assert check_date_in_range(date_n_stat[0],
-                                   date_n_stat[1]) == date_n_stat[2]
+        assert is_date_in_range(date, valid_dates) == result
 
 
-@pytest.mark.parametrize("row", check_validity_of_text_tests)
-def test_check_validity_of_text(row):
-    assert check_validity_of_txt(row[0]) == row[1]
+@pytest.mark.parametrize("row, result", check_validity_of_text_tests)
+def test_check_validity_of_text(row, result):
+    assert is_event_text_valid(row) == result
 
 
-@pytest.mark.parametrize("file1", import_txt_file_tests)
-def test_import_txt_file(file1):
-    assert import_txt_file(file1[0]) == file1[1]
+@pytest.mark.parametrize("file1, result", import_txt_file_tests)
+def test_import_txt_file(file1, result):
+    assert import_txt_file(file1) == result
 
 
-@pytest.mark.parametrize("file1", import_ics_tests)
-def test_import_ics_file(file1):
-    assert import_ics_file(file1[0]) == file1[1]
+@pytest.mark.parametrize("file1, result", import_ics_tests)
+def test_import_ics_file(file1, result):
+    assert import_ics_file(file1) == result
 
 
-@pytest.mark.parametrize("file1", before_import_checking_tests)
-def test_before_import_checking(file1):
-    assert before_import_checking(file1[0]) == file1[1]
+@pytest.mark.parametrize("file1, result", is_file_valid_to_import_tests)
+def test_is_file_valid_to_import(file1, result):
+    assert is_file_valid_to_import(file1) == result
 
 
-@pytest.mark.parametrize("file1", user_click_import_tests)
-def test_user_click_import(file1, test_session):
-    assert user_click_import(file1[0], file1[1], test_session) == file1[2]
+@pytest.mark.parametrize("file1, user_id, result", user_click_import_tests)
+def test_user_click_import(file1, user_id, result, test_session):
+    assert user_click_import(file1, user_id, test_session) == result
