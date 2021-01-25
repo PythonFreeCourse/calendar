3f81ab1f04d9e018d98f2059978cdcf51fc2b4d3
Ellen <ellenc345@gmail.com>
2021-01-25 08:44:08 +0200

    pr fixes

--- app/internal/on_this_day_events.py
+++ app/internal/on_this_day_events.py
@@ -6,8 +6,7 @@ import requests
 
 def get_on_this_day_events():
     now = datetime.now()
-    day = now.day
-    month = now.month
+    day, month = now.day, now.month
 
     res = requests.get(
         f'https://byabbe.se/on-this-day/{month}/{day}/events.json')
