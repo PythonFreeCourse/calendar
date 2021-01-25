81b377bf8b306964de6d88f1338470eb4665ccec
ZoharYamin <zohary89@gmail.com>
2021-01-26 01:34:42 +0200

    add status code

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,6 +1,9 @@
+from fastapi import status
+
+
 class TestHome:
     URL = "/"
 
     def test_get_page(self, home_test_client):
         resp = home_test_client.get(self.URL)
-        assert resp.status_code == 200
+        assert resp.status_code == status.HTTP_200_OK
