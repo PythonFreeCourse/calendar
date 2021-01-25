e1caaa4b3c3ca095010a9d703760039fe503e4bc
Gonny <1@1>
2021-01-25 15:03:45 +0200

    fix(i18n): add missing documentation

--- app/internal/utilities.py
+++ app/internal/utilities.py
@@ -9,6 +9,19 @@ from app.internal import languages
 
 def get_template_response(html_file_name: str, request: Request,
                           variables: Dict[str, Any] = None) -> Jinja2Templates:
+    """Creates and returns a Jinja2Templates object with a result dictionary
+    containing three parts: the request object, a variables dictionary,
+    and a translation dictionary.
+
+    Args:
+        html_file_name (str): the name of the html file.
+        request (Request): a FastApi Request object.
+        variables (Dict[str, Any]): an optional variables dictionary used
+        in the html file.
+
+    Returns:
+        Jinja2Templates: a Jinja2Templates response object.
+    """
     translations = languages.get_translation_words()
     result = {"request": request,
               "variables": variables,
