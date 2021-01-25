7525611363d0e162751560ac03b5b2ad87364383
Ellen <ellenc345@gmail.com>
2021-01-25 08:50:51 +0200

    fix: pr changes

--- app/internal/on_this_day_events.py
+++ app/internal/on_this_day_events.py
@@ -1,10 +1,11 @@
 import json
 from datetime import datetime
+from typing import Dict
 
 import requests
 
 
-def get_on_this_day_events():
+def get_on_this_day_events() -> Dict:
     now = datetime.now()
     day, month = now.day, now.month
 
