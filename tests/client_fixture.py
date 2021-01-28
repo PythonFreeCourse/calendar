bb388ce16967db8da257d12f51b404470b3d1c97
efratush <70986341+efratush@users.noreply.github.com>
2021-01-28 03:10:42 +0200

    Feature/delete event (#139)
    * basic delete event
    
    * Add an event delete function
    
    * Fix flake8
    
    * Deleting an unnecessary model
    
    * Extract lines from try

--- tests/client_fixture.py
+++ tests/client_fixture.py
@@ -1,12 +1,12 @@
-from fastapi.testclient import TestClient
 import pytest
-
 from app import main
+from app.database.database import Base
 from app.database.models import User
 from app.main import app
-from app.database.database import Base
-from app.routers import agenda, invitation, profile
-from tests.conftest import test_engine, get_test_db
+from app.routers import agenda, event, invitation, profile
+from fastapi.testclient import TestClient
+
+from tests.conftest import get_test_db, test_engine
 
 
 @pytest.fixture(scope="session")
@@ -62,3 +62,15 @@ def get_test_placeholder_user():
         full_name='FakeName',
         telegram_id='666666'
     )
+
+
+@pytest.fixture(scope="session")
+def event_test_client():
+    Base.metadata.create_all(bind=test_engine)
+    app.dependency_overrides[event.get_db] = get_test_db
+
+    with TestClient(app) as client:
+        yield client
+
+    app.dependency_overrides = {}
+    Base.metadata.drop_all(bind=test_engine)
