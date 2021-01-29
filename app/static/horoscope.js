7a6480acef19d4e31564f454cfb23b06cc1fa87a
nadav pesach <nadav-pesach@users.noreply.github.com>
2021-01-29 15:08:25 +0200

    fix: unnecessary lines removed

--- app/static/horoscope.js
+++ app/static/horoscope.js
@@ -1,10 +1,4 @@
 function daily_horoscope(sign) {
-    // sign = 'aries'#####################
-    //     // change to get sign and then stop
-    //     // change to get sign and then stop// change to get sign and then stop
-
-
-
     var x = 'https://aztro.sameerkumar.website/?sign=' + sign + '&day=today'
     const xhr = new XMLHttpRequest();
     xhr.open("POST", x, true);
