47c3a481353112b1f292e1340892a4f57b858a30
nadav pesach <nadav-pesach@users.noreply.github.com>
2021-01-29 16:03:39 +0200

    fix: Resolved merge conflict

--- app/static/horoscope.js
+++ app/static/horoscope.js
@@ -1,7 +1,7 @@
 function daily_horoscope(sign) {
-    var x = 'https://aztro.sameerkumar.website/?sign=' + sign + '&day=today'
+    var sign_data = 'https://aztro.sameerkumar.website/?sign=' + sign + '&day=today'
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", x, true);
+    xhr.open("POST", sign_data, true);
     xhr.onload = function() {
         obj = JSON.parse(this.responseText);
         let daily = document.getElementById("daily_horoscope");
