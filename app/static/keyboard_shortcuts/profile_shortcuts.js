9c29e960952ff41732d7f9eba8742083fb9ee7a1
Yair Engel <yair1engel@gmail.com>
2021-01-30 20:14:54 +0200

    delete profile_shortcuts.js file

--- app/static/keyboard_shortcuts/profile_shortcuts.js
+++
@@ -1,7 +0,0 @@
-import './mousetrap.js'
-
-Mousetrap.bind('alt+n', function () {
-    var nameModal = document.getElementById("updateNameModal");
-    nameModal.style = 'padding-right: 16px; display: block;';
-    console.log('hey')
-});
