488a5466e2b0680052fee89921e06cefa50da34e
Efrat <efratush.by@gmail.com>
2021-01-31 04:28:44 +0200

    flake8

--- tests/test_event.py
+++ tests/test_event.py
@@ -34,17 +34,18 @@ class TestEvent:
     def test_invalid_update(self, event, data, session):
         assert update_event(event_id=event.id,
                             event=data, db=session) is None
-    
+
     def test_fields_types_invalid(self, event, session):
         data = {"start": "20.01.2020"}
         with pytest.raises(HTTPException):
             response = update_event(event_id=event.id, event=data, db=session)
             assert response.status_code == status.HTTP_400_BAD_REQUEST
+
     def test_not_is_change_dates_allowed(self, event):
         data = {"start": "20.01.2020"}
         assert is_change_dates_allowed(event, data) is False
 
-    def test_successful_update(self,event, session):
+    def test_successful_update(self, event, session):
         data = {
             "title": "successful",
             "start": datetime(2021, 1, 20),
@@ -77,9 +78,8 @@ class TestEvent:
     def test_delete_failed(self, event_test_client, event):
         response = event_test_client.delete("/event/2")
         assert response.status_code == status.HTTP_404_NOT_FOUND
-    
+
     def test_no_connection_to_db(self, event):
         with pytest.raises(HTTPException):
             response = delete_event(event_id=1, db=None)
-            print(response)
             assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
