66d89721ed9d6af31a17cf134f929ac9fe2dd5cf
Aviad Amar <aviadamar@gmail.com>
2021-01-31 22:39:50 +0100

    Js fixing varibales and adding div element to request.

--- app/static/js/grid_scripts.js
+++ app/static/js/grid_scripts.js
@@ -3,10 +3,10 @@ let last = 0;
 let lastIndex = 0;
 
 function setToggle(elementClass, targetElement, classToAdd) {
-    let allDays = document.getElementsByClassName(elementClass);
+    const allDays = document.getElementsByClassName(elementClass);
     for (let i = lastIndex; i < allDays.length; ++i) {
         allDays[i].addEventListener("click", function () {
-            let target = document.getElementById(targetElement);
+            const target = document.getElementById(targetElement);
             target.classList.toggle(classToAdd);
         })
     }
@@ -23,26 +23,27 @@ function loadWeek(lastDay) {
     if (lastDay === last) {
         return false;
     }
-    let path = '/calendar/month/' + lastDay;
+    const path = '/calendar/month/' + lastDay;
+    const newDays = document.createElement('html');
     last = lastDay;
     fetch(path).then(function (response) {
         return response.text();
     }).then(function (html) {
-        let newDays = document.createElement('html');
-        newDays.innerHTML = html;
+        const newDiv = document.createElement("div");
+        newDays.innerHTML = html
+        newDiv.appendChild(newDays);
         document.getElementById("calender-grid").append(newDays);
         setToggle("day", "day-view", "day-view-visible");
     });
 }
-
 
 window.addEventListener(
     'scroll', function () {
         if (window.scrollY + window.innerHeight + tolerance < document.documentElement.scrollHeight) {
             return false;
         }
-        let allDays = document.querySelectorAll('.day');
-        let lastDay = allDays[allDays.length - 1].id;
+        const allDays = document.querySelectorAll('.day');
+        const lastDay = allDays[allDays.length - 1].id;
         loadWeek(lastDay);
     }
 )
