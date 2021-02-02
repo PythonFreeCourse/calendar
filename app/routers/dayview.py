2daa57402d0b932b6f3f9500a2033a597b08f8bd
Sagi Zaid Or <sagi@eretz.org.il>
2021-02-02 17:55:06 +0200

    changed branch name

--- app/routers/dayview.py
+++ app/routers/dayview.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Tuple, Union
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import and_, or_
 
@@ -16,6 +16,7 @@ templates = Jinja2Templates(directory=TEMPLATES_PATH)
 router = APIRouter()
 
 
+<<<<<<< Updated upstream
 class DivAttributes:
     GRID_BAR_QUARTER = 1
     FULL_GRID_BAR = 4
@@ -69,6 +70,34 @@ class DivAttributes:
         else:
             end = self._get_position(self.end_time)
         return f'{start} / {end}'
+=======
+#inner class of the router, for the jinja page to process the json
+class Event:
+    GRID_BAR_QUARTER = 1
+    FULL_GRID_BAR = 4
+    MIN_MINUTS = 0
+    MAX_MINUTS = 15
+    BASE_GRID_BAR = 5
+
+    def __init__(self, id: int, color: str, content: str, start_datetime: str, end_datetime: str) -> None:
+        self.id = id
+        self.color = color
+        self.content = content
+        self.start_time = datetime.strptime(start_datetime, "%d/%m/%Y %H:%M")
+        self.end_time = datetime.strptime(end_datetime, "%d/%m/%Y %H:%M")
+        self._set_total_time()
+        self._set_grid_position()
+
+    def _minutes_position(self, minutes: int) -> int:
+        for i in range(self.GRID_BAR_QUARTER, self.FULL_GRID_BAR + 1):
+            if self.MIN_MINUTS <= minutes < self.MAX_MINUTS:
+                return i
+            self.MIN_MINUTS = self.MAX_MINUTS
+            self.MAX_MINUTS += 15
+
+    def _get_position(self, time: datetime) -> int:
+        return time.hour * self.FULL_GRID_BAR + self._minutes_position(time.minute) + self.BASE_GRID_BAR
+>>>>>>> Stashed changes
 
     def _get_time_format(self) -> str:
         for multiday in [self.start_multiday, self.end_multiday]:
@@ -77,6 +106,7 @@ class DivAttributes:
     def _set_total_time(self) -> None:
         length = self.end_time - self.start_time
         self.length = length.seconds / 60
+<<<<<<< Updated upstream
         format_gen = self._get_time_format()
         start_time_str = self.start_time.strftime(next(format_gen))
         end_time_str = self.end_time.strftime(next(format_gen))
@@ -94,17 +124,35 @@ class DivAttributes:
 
 
 @router.get('/day/{date}')
-async def dayview(request: Request, date: str, db_session=Depends(get_db), view='day'):
+#async def dayview(request: Request, date: str, db_session=Depends(get_db), view='day'):
+async def dayview(request: Request, date: str, view='day'):
     # TODO: add a login session
-    user = db_session.query(User).filter_by(username='test1').first()
-    day = datetime.strptime(date, '%Y-%m-%d')
+    #user = db_session.query(User).filter_by(username='test1').first()
+    try:
+        day = datetime.strptime(date, '%Y-%m-%d')
+        print(day)
+    except ValueError as err:
+        raise HTTPException(status_code=404, detail=f"{err}")
     day_end = day + timedelta(hours=24)
-    events = db_session.query(Event).filter(
+    '''events = db_session.query(Event).filter(
         Event.owner_id == user.id).filter(
             or_(and_(Event.start >= day, Event.start < day_end),
                 and_(Event.end >= day, Event.end < day_end),
-                and_(Event.start < day_end, day_end < Event.end)))
+                and_(Event.start < day_end, day_end < Event.end)))'''
+    start = datetime(year=2021, month=2, day=1, hour=13, minute=13)
+    end = datetime(year=2021, month=2, day=1, hour=15, minute=46)
+    events = [Event(title='test2', content='test',
+                 start=start, end=end, owner_id=1, color='pink')]
     events_n_attrs = [(event, DivAttributes(event, day)) for event in events]
+=======
+        self.total_time = self.start_time.strftime("%H:%M") + ' - ' + self.end_time.strftime("%H:%M")
+
+
+@router.post("/dayview")
+async def dayview(request: Request):
+    form = await request.json()
+    events = [Event(**event) for event in form['events']]
+>>>>>>> Stashed changes
     return templates.TemplateResponse("dayview.html", {
         "request": request,
         "events": events_n_attrs,
--- app/routers/weekview.py
+++ app/routers/weekview.py
@@ -5,8 +5,9 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
 from app.database.database import get_db
+from app.database.models import Event
 from app.dependencies import TEMPLATES_PATH
-from app.routers.dayview import dayview
+from app.routers.dayview import dayview, DivAttributes
 
 
 templates = Jinja2Templates(directory=TEMPLATES_PATH)
@@ -27,14 +28,13 @@ def get_week_dates(sunday: datetime) -> List[datetime]:
 
 async def get_day_view_template(req: Request,
                                 day: datetime,
-                                session) -> templates.TemplateResponse:
+                                session='') -> templates.TemplateResponse:
     view = 'week'
-    if day.strftime('%A') == 'Sunday':
-        view = 'sunday'
     template = await dayview(request=req,
                              date=day.strftime('%Y-%m-%d'),
-                             db_session=session,
+                             #db_session=session,
                              view=view)
+    print(template)
     return template
 
 
@@ -42,10 +42,20 @@ async def get_day_view_template(req: Request,
 async def weekview(request: Request, sunday: str, db_session=Depends(get_db)):
     sunday = datetime.strptime(sunday, '%Y-%m-%d')
     week_days = get_week_dates(sunday)
-    week = [(day, await get_day_view_template(
+    '''week = [(day, await get_day_view_template(
         req=request, day=day, session=db_session)
+        ) for day in week_days]'''
+    start = datetime(year=2021, month=2, day=1, hour=13, minute=13)
+    end = datetime(year=2021, month=2, day=1, hour=15, minute=46)
+    events = [Event(title='test2', content='test',
+                 start=start, end=end, owner_id=1, color='pink')]
+    week = [(day, await get_day_view_template(
+        req=request, day=day)
         ) for day in week_days]
+    day = sunday
+    events_n_attrs = [(event, DivAttributes(event, day)) for event in events]
     return templates.TemplateResponse("weekview.html", {
         "request": request,
-        "week": week
+        "week": week,
+        "weekly_events": events_n_attrs
         })
