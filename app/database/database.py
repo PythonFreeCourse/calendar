4c0d3b8d7bbc982c9d1b40f810060b89e0fe1c69
Lisafiluz <58887224+Lisafiluz@users.noreply.github.com>
2021-02-02 22:12:24 +0200

    Update database.py

--- app/database/database.py
+++ app/database/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+
 
 from app import config
 
