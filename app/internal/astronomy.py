69a722be3212406c2c7c0f4414ac9a31e21a3cdf
Hadas Kedar <hadasit@gmail.com>
2021-02-02 17:53:37 +0200

    feat: astronomy - async support & fixes

--- app/internal/astronomy.py
+++ app/internal/astronomy.py
@@ -69,5 +69,6 @@ def get_astronomical_data(requested_date: datetime.datetime, location: str)\
     """
     formatted_date = requested_date.strftime('%Y-%m-%d')
     loop = asyncio.get_event_loop()
-    output = loop.run_until_complete(get_data_from_api(formatted_date, location))
+    output = loop.run_until_complete(get_data_from_api(formatted_date,
+                                                       location))
     return output
