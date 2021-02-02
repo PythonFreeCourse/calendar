e3e792b067ef793ea17b21c9c0b951948d7d5fa1
Adva Alkalay <adva.alkalay@gmail.com>
2021-02-02 12:59:33 +0200

    Fixes for Yam

--- app/static/currency.js
+++ app/static/currency.js
@@ -1,54 +1,71 @@
 function getCurrency(baseCurrency, dateToday) {
     const showCurrencyElement = document.getElementById("showCurrency")
     const currencyViewElement = document.getElementById("currencyView");
-    const currencyButtonElement = document.getElementById("currencyButton");
-    currencyViewElement.innerHTML = "";
+    const myUrl = 'https://api.exchangeratesapi.io/' + dateToday + '?base=' + baseCurrency
 
-    function getResponse(yourUrl) {
-        var httpRequest = new XMLHttpRequest();
-        httpRequest.open("GET", yourUrl, false);
-        httpRequest.send(null);
-        return httpRequest.responseText;
-    }
-
-    const data = JSON.parse(getResponse('https://api.exchangeratesapi.io/' + dateToday + '?base=' + baseCurrency));
-    if (!('rates' in data)) {
-        return false;
-    }
-
-    if (showCurrencyElement.style.display == 'none') {
-        showCurrencyElement.style.display = 'block';
-    }
-
-    var listItem = document.createElement("li");
-    listItem.className = "list-group-item title_size_small";
-    const baseItem = document.createElement("strong");
-    baseItem.innerHTML = "BASE: " + baseCurrency;
-    listItem.appendChild(baseItem);
-    currencyViewElement.appendChild(listItem);
-
-    for (key in data.rates) {
-        if (key !== baseCurrency) {
-            listItem = document.createElement("li");
-            listItem.className = "list-group-item title_size_small";
-            var linkItem = document.createElement("a");
-            linkItem.setAttribute("id", key);
-            linkItem.setAttribute("href", "javascript:void(0)");
-            linkItem.innerText = key;
-            listItem.appendChild(linkItem);
-            var ratesItem = document.createElement("div");
-            ratesItem.setAttribute("style", "display: inline;")
-            ratesItem.innerHTML = ": " + data.rates[key];
-            listItem.appendChild(ratesItem);
-            currencyViewElement.appendChild(listItem);
+    async function getResponse(myUrl) {
+        let response = await fetch(myUrl);
+        if (response.ok) {
+            let result = await response.json();
+            currencyViewElement.innerHTML = "";
+            showCurrencyElement.dataset.visible = "1";
+            return result;
         }
     }
 
-    listItem = document.createElement("li");
-    listItem.className = "list-group-item title_size_small";
-    const dateItem = document.createElement("strong");
-    dateItem.innerHTML = dateToday;
-    listItem.appendChild(dateItem);
-    currencyViewElement.appendChild(listItem);
-    return true;
+    getResponse(myUrl)
+        .then(result => buildCurrencyList(result));
+
+    function buildCurrencyList(data) {
+        function getListItem() {
+            const listItem = document.createElement("li");
+            listItem.className = "list-group-item title_size_small";
+            return listItem;
+        }
+
+        function createBoldListItem(fieldString, currViewElem) {
+            const listItem = getListItem();
+            const boldItem = document.createElement("strong");
+            boldItem.innerHTML = fieldString;
+            listItem.appendChild(boldItem);
+            currViewElem.appendChild(listItem);
+        }
+
+        function getAnchorItem() {
+            const linkItem = document.createElement("a");
+            linkItem.setAttribute("href", "javascript:void(0)");
+            linkItem.setAttribute("id", key);
+            linkItem.innerText = key;
+            return linkItem;
+        }
+
+        function getRatesItem(fieldString) {
+            let ratesItem = document.createElement("div");
+            ratesItem.setAttribute("style", "display: inline;")
+            ratesItem.innerHTML = fieldString;
+            return ratesItem;
+        }
+
+        function createAllElements(dataRates, baseCurr, currViewElem) {
+            for (key in dataRates) {
+                if (key !== baseCurr) {
+                    let listItem = getListItem();
+                    let linkItem = getAnchorItem();
+                    let ratesItem = getRatesItem(": " + dataRates[key]);
+                    listItem.appendChild(linkItem);
+                    listItem.appendChild(ratesItem);
+                    currViewElem.appendChild(listItem);
+                }
+            }
+        }
+
+        // Create Base currency list item
+        createBoldListItem("BASE: " + baseCurrency, currencyViewElement);
+        // Create all rates list items
+        createAllElements(data.rates, baseCurrency, currencyViewElement);
+        // Create date list item
+        createBoldListItem(dateToday, currencyViewElement);
+
+        return true;
+    }
 }
