f23a081d899a13f9ea4b4f2b6be1045413e2615a
Sagi Zaid Or <sagi@eretz.org.il>
2021-02-02 18:11:40 +0200

    קומיט עצוב ברמות

--- app/routers/dayview.py
+++ app/routers/dayview.py
@@ -16,7 +16,6 @@ templates = Jinja2Templates(directory=TEMPLATES_PATH)
 router = APIRouter()
 
 
-<<<<<<< Updated upstream
 class DivAttributes:
     GRID_BAR_QUARTER = 1
     FULL_GRID_BAR = 4
@@ -70,34 +69,6 @@ class DivAttributes:
         else:
             end = self._get_position(self.end_time)
         return f'{start} / {end}'
-=======
-#inner class of the router, for the jinja page to process the json
-class Event:
-    GRID_BAR_QUARTER = 1
-    FULL_GRID_BAR = 4
-    MIN_MINUTS = 0
-    MAX_MINUTS = 15
-    BASE_GRID_BAR = 5
-
-    def __init__(self, id: int, color: str, content: str, start_datetime: str, end_datetime: str) -> None:
-        self.id = id
-        self.color = color
-        self.content = content
-        self.start_time = datetime.strptime(start_datetime, "%d/%m/%Y %H:%M")
-        self.end_time = datetime.strptime(end_datetime, "%d/%m/%Y %H:%M")
-        self._set_total_time()
-        self._set_grid_position()
-
-    def _minutes_position(self, minutes: int) -> int:
-        for i in range(self.GRID_BAR_QUARTER, self.FULL_GRID_BAR + 1):
-            if self.MIN_MINUTS <= minutes < self.MAX_MINUTS:
-                return i
-            self.MIN_MINUTS = self.MAX_MINUTS
-            self.MAX_MINUTS += 15
-
-    def _get_position(self, time: datetime) -> int:
-        return time.hour * self.FULL_GRID_BAR + self._minutes_position(time.minute) + self.BASE_GRID_BAR
->>>>>>> Stashed changes
 
     def _get_time_format(self) -> str:
         for multiday in [self.start_multiday, self.end_multiday]:
@@ -106,7 +77,6 @@ class Event:
     def _set_total_time(self) -> None:
         length = self.end_time - self.start_time
         self.length = length.seconds / 60
-<<<<<<< Updated upstream
         format_gen = self._get_time_format()
         start_time_str = self.start_time.strftime(next(format_gen))
         end_time_str = self.end_time.strftime(next(format_gen))
@@ -144,15 +114,6 @@ async def dayview(request: Request, date: str, view='day'):
     events = [Event(title='test2', content='test',
                  start=start, end=end, owner_id=1, color='pink')]
     events_n_attrs = [(event, DivAttributes(event, day)) for event in events]
-=======
-        self.total_time = self.start_time.strftime("%H:%M") + ' - ' + self.end_time.strftime("%H:%M")
-
-
-@router.post("/dayview")
-async def dayview(request: Request):
-    form = await request.json()
-    events = [Event(**event) for event in form['events']]
->>>>>>> Stashed changes
     return templates.TemplateResponse("dayview.html", {
         "request": request,
         "events": events_n_attrs,
