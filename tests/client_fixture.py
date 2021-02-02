f77e20c220b022fd527164190792516758e17b86
subscorp <ori81510@gmail.com>
2021-02-02 19:11:09 +0200

    fix: fixed flake8 issue

--- tests/client_fixture.py
+++ tests/client_fixture.py
@@ -87,15 +87,3 @@ def audio_test_client():
     with TestClient(app) as client:
         yield client
     Base.metadata.drop_all(bind=test_engine)
-
-
-@pytest.fixture(scope="session")
-def event_test_client():
-    Base.metadata.create_all(bind=test_engine)
-    app.dependency_overrides[event.get_db] = get_test_db
-
-    with TestClient(app) as client:
-        yield client
-
-    app.dependency_overrides = {}
-    Base.metadata.drop_all(bind=test_engine)
