905eb0d564e58df7bfa5046f6d8466b56210d931
fandomario <fandomario616@gmail.com>
2021-02-05 04:38:18 +0200

    fix: add db tests

--- tests/test_joke.py
+++ tests/test_joke.py
@@ -1,4 +1,10 @@
+from app.internal import jokes
+
 def test_joke(client):
     resp = client.get('/joke')
     assert resp.ok
     assert resp.json
+
+
+def test_get_a_joke(session, joke):
+    assert jokes.have_a_joke(session).text == joke.text
