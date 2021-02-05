f4ea2179f55a1db6853a71618d5e37a75ff0b5b0
fandomario <fandomario616@gmail.com>
2021-02-05 13:30:19 +0200

    fix: add a test

--- tests/test_joke.py
+++ tests/test_joke.py
@@ -7,10 +7,10 @@ def get_jokes_amount(session):
     return session.query(Joke).count()
 
 
-# def test_joke(client):
-#    resp = client.get('/joke')
-#    assert resp.ok
-#    assert resp.json
+def test_joke(client):
+   resp = client.get('/joke')
+   assert resp.ok
+   assert resp.json
 
 
 # def test_get_a_joke(session, joke):
