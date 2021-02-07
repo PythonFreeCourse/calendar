09c2a17d24de9ad37554799f36436eaa07c7f4d0
liaarbel <lia.arbel1@gmail.com>
2021-02-07 22:41:00 +0200

    reformatted json

--- app/internal/international_days.py
+++ app/internal/international_days.py
@@ -8,8 +8,6 @@ from app.database.models import InternationalDays
 
 def create_international_day_object(
         international_day: Dict[str, Union[str, int]]) -> InternationalDays:
-    """This function create a quote object from given fields dictionary.
-    It is used for adding the data from the json into the db"""
     return InternationalDays(
         day=international_day["day"],
         month=international_day["month"],
