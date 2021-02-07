adba494ed58c8e05669cc6c22ccfa782fe589641
Adva Alkalay <adva.alkalay@gmail.com>
2021-02-07 10:54:23 +0200

    Fixes for Yam

--- app/static/celebrity.js
+++ app/static/celebrity.js
@@ -1,5 +1,6 @@
 function getCelebs(dateToday) {
     const celebCards = document.getElementById("celebCards");
+    const numberOfCelebs = 12;
 
     fetch('https://www.imdb.com/search/name/?birth_monthday=' + dateToday)
         .then(function (response) {
@@ -8,110 +9,93 @@ function getCelebs(dateToday) {
             celebCards.innerHTML = "";
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
-
-            function getNames() {
-                let namesArray = new Array();
-                const names = doc.querySelectorAll('h3.lister-item-header');
-                for (let i = 0; i < 12; i++) {
-                    namesArray.push(names[i].innerText.trim().substr(5).trim());
-                }
-                return namesArray;
-            }
-
-            function getImages() {
-                let imagesArray = new Array();
-                const images = doc.querySelectorAll('div.lister-item-image a img');
-                for (let i = 0; i < 12; i++) {
-                    imagesArray.push(images[i].getAttribute("src"));
-                }
-                return imagesArray;
-            }
-
-            function getLinks() {
-                let linksArray = new Array();
-                const links = doc.querySelectorAll('div.lister-item-image a');
-                for (let i = 0; i < 12; i++) {
-                    linksArray.push(links[i].getAttribute("href"));
-                }
-                return linksArray;
-            }
-
-            function getJobs() {
-                let jobsArray = new Array();
-                const jobs = doc.querySelectorAll("p.text-muted");
-                for (let i = 0; i < 12; i++) {
-                    jobsArray.push(jobs[i].innerText.trim().split("|")[0].trim());
-                }
-                return jobsArray;
-            }
-
-            function getTopDiv() {
-                let card = document.createElement("div");
-                card.className = "card top-div";
-                return card;
-            }
-
-            function getImgElement(img) {
-                let imgElement = document.createElement("img");
-                imgElement.className = "card-img-top";
-                imgElement.setAttribute("src", img);
-                return imgElement
-            }
-
-            function getDivFooter() {
-                let divFooter = document.createElement("div");
-                divFooter.className = "card-footer div-footer";
-                return divFooter
-            }
-
-            function getSmallElement(anchorElement, jobElement) {
-                let smallElement = document.createElement("small");
-                smallElement.className = "text-muted";
-                smallElement.appendChild(anchorElement);
-                smallElement.appendChild(jobElement);
-                return smallElement;
-            }
-
-            function getAnchorElement(link, name) {
-                let anchorElement = document.createElement("a");
-                anchorElement.setAttribute("href", "https://www.imdb.com" + link);
-                anchorElement.setAttribute("title", name);
-                anchorElement.setAttribute("target", "_blank");
-                anchorElement.innerText = name;
-                return anchorElement;
-            }
-
-            function getJobElement(job) {
-                let jobElement = document.createElement("div");
-                jobElement.className = "div-job";
-                jobElement.innerText = " | " + job;
-                return jobElement;
-            }
-
-            function buildCards(celebCards) {
-                for (let i = 0; i < 12; i++) {
-                    const card = getTopDiv();
-                    const imgElement = getImgElement(imagesArray[i]);
-                    card.appendChild(imgElement);
-
-                    const divFooter = getDivFooter();
-                    const anchorElement = getAnchorElement(linksArray[i], namesArray[i]);
-                    const jobElement = getJobElement(jobsArray[i]);
-                    const smallElement = getSmallElement(anchorElement, jobElement);
-                    divFooter.appendChild(smallElement);
-
-                    card.appendChild(divFooter);
-                    celebCards.appendChild(card);
-                }
-            }
-
-            const namesArray = getNames();
-            const imagesArray = getImages();
-            const linksArray = getLinks();
-            const jobsArray = getJobs();
-
-            buildCards(celebCards);
+            getCelebsData(doc);
         }).catch(() => {
             celebCards.innerHTML = "Failed to load Celebrity birthdays."
         });
+
+    function getCelebsData(doc) {
+        function getArray(selector, dataType) {
+            let myArray = new Array();
+            const selectorData = doc.querySelectorAll(selector);
+            for (let i = 0; i < numberOfCelebs; i++) {
+                if (dataType === "names") {
+                    myArray.push(selectorData[i].innerText.trim().substr(5).trim());
+                } else if (dataType === "images") {
+                    myArray.push(selectorData[i].getAttribute("src"));
+                } else if (dataType === "links") {
+                    myArray.push(selectorData[i].getAttribute("href"));
+                } else if (dataType === "jobs") {
+                    myArray.push(selectorData[i].innerText.trim().split("|")[0].trim());
+                }
+            }
+            return myArray;
+        }
+
+        function getTopDiv() {
+            let card = document.createElement("div");
+            card.className = "card top-div";
+            return card;
+        }
+
+        function getImgElement(img) {
+            let imgElement = document.createElement("img");
+            imgElement.className = "card-img-top";
+            imgElement.setAttribute("src", img);
+            return imgElement
+        }
+
+        function getDivFooter() {
+            let divFooter = document.createElement("div");
+            divFooter.className = "card-footer div-footer";
+            return divFooter
+        }
+
+        function getSmallElement(anchorElement, jobElement) {
+            let smallElement = document.createElement("small");
+            smallElement.className = "text-muted";
+            smallElement.appendChild(anchorElement);
+            smallElement.appendChild(jobElement);
+            return smallElement;
+        }
+
+        function getAnchorElement(link, name) {
+            let anchorElement = document.createElement("a");
+            anchorElement.setAttribute("href", "https://www.imdb.com" + link);
+            anchorElement.setAttribute("title", name);
+            anchorElement.setAttribute("target", "_blank");
+            anchorElement.innerText = name;
+            return anchorElement;
+        }
+
+        function getJobElement(job) {
+            let jobElement = document.createElement("div");
+            jobElement.className = "div-job";
+            jobElement.innerText = " | " + job;
+            return jobElement;
+        }
+
+        function buildCards(celebCards) {
+            for (let i = 0; i < numberOfCelebs; i++) {
+                const card = getTopDiv();
+                const imgElement = getImgElement(imagesArray[i]);
+                card.appendChild(imgElement);
+
+                const divFooter = getDivFooter();
+                const anchorElement = getAnchorElement(linksArray[i], namesArray[i]);
+                const jobElement = getJobElement(jobsArray[i]);
+                const smallElement = getSmallElement(anchorElement, jobElement);
+                divFooter.appendChild(smallElement);
+
+                card.appendChild(divFooter);
+                celebCards.appendChild(card);
+            }
+        }
+
+        const namesArray = getArray('h3.lister-item-header', "names");
+        const imagesArray = getArray('div.lister-item-image a img', "images");
+        const linksArray = getArray('div.lister-item-image a', "links");
+        const jobsArray = getArray("p.text-muted", "jobs");
+        buildCards(celebCards);
+    }
 }
--- tests/test_celebrity.py
+++ tests/test_celebrity.py
@@ -2,24 +2,19 @@ import datetime
 
 import pytest
 from app.internal.celebrity import get_today_month_and_day
+from hypothesis import given
+from hypothesis import strategies as st
 
 
 CELEBRITY_PAGE = "/celebrity"
-FAKE_TIME = datetime.date(2018, 9, 18)
 
 
-@pytest.fixture
-def datetime_mock(monkeypatch):
-    class MyDateTime:
-        @classmethod
-        def today(cls):
-            return FAKE_TIME
-
-    monkeypatch.setattr(datetime, 'date', MyDateTime)
-
-
-def test_get_today_month_and_day(datetime_mock):
-    assert get_today_month_and_day() == FAKE_TIME.strftime("%m-%d")
+@given(fake_date=st.dates(datetime.date(2019, 1, 1), datetime.date(2022, 1, 1)))
+def test_get_today_month_and_day(fake_date):
+    if datetime.date.today().strftime("%m-%d") == fake_date.strftime("%m-%d"):
+        assert get_today_month_and_day() == fake_date.strftime("%m-%d")
+    else:
+        assert get_today_month_and_day() != fake_date.strftime("%m-%d")
 
 
 def test_celebrity_page_exists(client):
