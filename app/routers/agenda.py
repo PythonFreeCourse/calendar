fe478cfcdb12937de3aa5399b98666bfb14e9656
nadav pesach <nadav-pesach@users.noreply.github.com>
2021-02-08 20:16:38 +0200

    test fix

--- app/routers/agenda.py
+++ app/routers/agenda.py
@@ -40,6 +40,18 @@ def agenda(
     """Route for the agenda page, using dates range or exact amount of days."""
 
     user_id = 1  # there is no user session yet, so I use user id- 1.
+
+    start_date_graph, end_date_graph = calc_dates_range_for_agenda(0, 0, 7)
+
+    events_objects_for_graph = agenda_events.get_events_per_dates(
+        db, user_id, start_date_graph, end_date_graph
+    )
+    events_for_graph = {
+        str(start_date_graph + timedelta(i)): 0 for i in range(8)}
+
+    for event_obj in events_objects_for_graph:
+        events_for_graph[str(event_obj.start.date())] += 1
+
     start_date, end_date = calc_dates_range_for_agenda(
         start_date, end_date, days
     )
@@ -55,23 +67,10 @@ def agenda(
         )
         events[event_obj.start.date()].append((event_obj, event_duration))
 
-    start_date_graph, end_date_graph = calc_dates_range_for_agenda(
-        start_date, end_date, 7
-    )
-
-    events_objects_for_graph = agenda_events.get_events_per_dates(
-        db, user_id, start_date_graph, end_date_graph
-    )
-    events_for_graph = {
-        str(start_date_graph + timedelta(i)): 0 for i in range(8)}
-
-    for event_obj in events_objects_for_graph:
-        events_for_graph[str(event_obj.start.date())] += 1
-
     return templates.TemplateResponse("agenda.html", {
         "request": request,
         "events": events,
-        "events_for_graph": json.dumps(events_for_graph),
         "start_date": start_date,
         "end_date": end_date,
+        "events_for_graph": json.dumps(events_for_graph),
     })
