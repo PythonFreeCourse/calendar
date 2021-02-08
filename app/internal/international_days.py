227117dc2a497928faef24d5db865e2e7edf1b6f
liaarbel <lia.arbel1@gmail.com>
2021-02-08 10:26:16 +0200

    changed typing convert

--- app/internal/international_days.py
+++ app/internal/international_days.py
@@ -8,6 +8,7 @@ from app.database.models import InternationalDays
 
 def create_international_day_object(
         international_day: Dict[str, Union[str, int]]) -> InternationalDays:
+    print(type(international_day))
     return InternationalDays(
         day=international_day["day"],
         month=international_day["month"],
@@ -17,10 +18,10 @@ def create_international_day_object(
 
 def get_international_day_per_day(session: Session, date) -> \
         Optional[InternationalDays]:
-    day_num = int(date.day)
-    month = int(date.strftime("%B"))
+    day_num = date.day
+    month = date.strftime("%B")
     international_day = session.query(InternationalDays).filter(
-        InternationalDays.day == str(day_num)) \
+        InternationalDays.day == day_num) \
         .filter(InternationalDays.month == month).order_by(
         func.random()).first()
     return international_day
