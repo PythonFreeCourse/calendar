b85bc90265c20ea2f743288a89dce4e471763a86
orontamir <orontamir@gmail.com>
2021-02-09 12:39:45 +0200

    Fix event tests and user_exercise test

--- tests/test_user_exercise.py
+++ tests/test_user_exercise.py
@@ -12,6 +12,7 @@ class TestUserExercise:
             password='new_test_password',
             email='new_test.email@gmail.com',
             language='english',
+            language_id=1,
         )
         user_exercise = create_user_exercise(
             session=session,
