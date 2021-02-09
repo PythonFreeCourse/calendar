638506bf80f4f47a1dd4fb1ef7b735479eed073d
Nir Ki-tov <nir9696@gmail.com>
2021-02-09 23:14:49 +0200

    fix: correct minor typos in code and update requirements.txt

--- app/static/voice.js
+++ app/static/voice.js
@@ -1,67 +1,68 @@
-window.addEventListener("load",(event) => {
- 
-  const synth = window.speechSynthesis;
-  let voices = [];
-  const voiceSelect = document.getElementById(voice);
+function listVoices(synth, voiceSelect) {
+	voices = synth.getVoices();
+	const selectedIndex = Math.max(0, voiceSelect.selectedIndex);
 
-  
-  function listVoices(){
-    voices = synth.getVoices();
-    const selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-    
-    voiceSelect.innerHTML = '';
-    for(const voice of voices) {
-      const option = document.createElement('option');
-      option.textContent = `${voice.name} (${voice.lang})`;
-      if(voice.default) {
-        option.textContent += ' -- DEFAULT';
-        option.selected = true;
-      }
-      option.setAttribute('data-lang', voice.lang);
-      option.setAttribute('data-name', voice.name);
-      voiceSelect.appendChild(option);
-    }
-    voiceSelect.selectedIndex = selectedIndex;
-  }
+	voiceSelect.innerText = '';
+	for (const voice of voices) {
+		const option = document.createElement('option');
+		option.textContent = `${voice.name} (${voice.lang})`;
+		if (voice.default) {
+			option.textContent += ' -- DEFAULT';
+			option.selected = true;
+		}
+		option.setAttribute('data-lang', voice.lang);
+		option.setAttribute('data-name', voice.name);
+		voiceSelect.appendChild(option);
+	}
+	voiceSelect.selectedIndex = selectedIndex;
+}
 
-  listVoices();
-  if(synth.onvoiceschanged !== undefined){
-    synth.onvoiceschanged = listVoices;
-  }
-  
-  const controls = ["volume","pitch","rate"];
-  const actions = ["load","input"];
-  function setOutput(ctrl){
-    const range = document.getElementById(ctrl);
-    range.nextElementSibling.textContent = range.value;
-  }
-  for(const control of controls){
-    for(const action of actions){
-      setOutput(`#${control}`);
-      document.getElementById(control).addEventListener(action, (event) => {
-        setOutput(`#${control}`);
-      });  
-    }
-  }
-  
-  
-  document.getElementById(speak).addEventListener("click", (event) => {
-    event.preventDefault();
-    const say = document.getElementById(say).value;
-    const sayThis = new SpeechSynthesisUtterance(say);
-    
-    const vox = voiceSelect.selectedOptions[0].dataset.name;
-    for(voice of voices){
-      if(voice.name === vox) {
-        sayThis.voice = voice;
-        break;
-      }
-    }
-    
-    sayThis.pitch = document.getElementById(pitch).value;
-    sayThis.rate = document.getElementById(rate).value;
-    synth.speak(sayThis);
-  });
-  
+
+function setOutput(ctrl) {
+	const range = document.getElementById(ctrl);
+	range.nextElementSibling.textContent = range.value;
+}
+
+
+window.addEventListener("load", (event) => {
+
+	const synth = window.speechSynthesis;
+	let voices = [];
+	const voiceSelect = document.getElementById('voice');
+
+	listVoices(synth, voiceSelect);
+	if (synth.onvoiceschanged !== undefined) {
+		synth.onvoiceschanged = listVoices;
+	}
+
+	const controls = ["volume", "pitch", "rate"];
+	const actions = ["load", "input"];
+
+	for (const control of controls) {
+		for (const action of actions) {
+			setOutput(`${control}`);
+			document.getElementById(`${control}`).addEventListener(action, (event) => {
+				setOutput(`${control}`);
+			});
+		}
+	}
+
+	document.getElementById('speak').addEventListener("click", (event) => {
+		event.preventDefault();
+		const say = document.getElementById('say').value;
+		const sayThis = new SpeechSynthesisUtterance(say);
+
+		const vox = voiceSelect.selectedOptions[0].dataset.name;
+		for (voice of voices) {
+			if (voice.name === vox) {
+				sayThis.voice = voice;
+				break;
+			}
+		}
+
+		sayThis.pitch = document.getElementById('pitch').value;
+		sayThis.rate = document.getElementById('rate').value;
+		synth.speak(sayThis);
+	});
+
 });
-
