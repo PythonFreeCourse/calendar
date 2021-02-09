e5c5db0c8ed48c773469edc09e69d1561d999842
nadav pesach <nadav-pesach@users.noreply.github.com>
2021-02-09 14:14:37 +0200

    gix

--- app/routers/event.py
+++ app/routers/event.py
@@ -1,6 +1,6 @@
-from datetime import datetime as dt
+from datetime import datetime
 from operator import attrgetter
-from typing import Any, Dict, datetime, List, Optional
+from typing import Any, Dict, List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.exc import SQLAlchemyError
