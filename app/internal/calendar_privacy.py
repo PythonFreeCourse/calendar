48704b01f9c5a1703b1ec221dfe3ceb65b7ffe54
imimouni <imimouni@gmail.com>
2021-02-10 19:00:27 +0200

    fix: comments, typing, user fixture, readability fix

--- app/internal/calendar_privacy.py
+++ app/internal/calendar_privacy.py
@@ -1,5 +1,6 @@
 from app.database.database import get_db
 from app.database.models import User
+# TODO switch to using this when the user system is merged
 # from app.internal.security.dependancies import (
 #    current_user, CurrentUser)
 
@@ -8,24 +9,24 @@ from fastapi import Depends
 
 # TODO add privacy as an attribute in current user
 # in app.internal.security.dependancies
+# when user system is merged
 def can_show_calendar(
     requested_user_username: str,
     db: Depends(get_db),
     current_user: User
-):
-    # to be added after user system is merged:
-    # CurrentUser = Depends(current_user)):
+    # TODO to be added after user system is merged:
+    # CurrentUser = Depends(current_user)
+) -> bool:
     """Check whether current user can show the requested calendar"""
     requested_user = db.query(User).filter(
         User.username == requested_user_username
     ).first()
     privacy = current_user.privacy
-    if privacy == 'Private' and (
-        current_user.username == requested_user.username
-    ):
+    is_current_user = current_user.username == requested_user.username
+    if privacy == 'Private' and is_current_user:
         return True
 
     elif privacy == 'Public':
         return True
 
-    return
+    return False
--- tests/test_calendar_privacy.py
+++ tests/test_calendar_privacy.py
@@ -1,41 +1,23 @@
 from app.internal.calendar_privacy import can_show_calendar
-# after user system is merged:
+# TODO after user system is merged:
 # from app.internal.security.dependancies import CurrentUser
 from app.routers.user import create_user
 
 
-USER_DATA = {
-    'username': 'new_test_username',
-    'email': 'new_test.email@gmail.com',
-    'password': 'new_test_password',
-    'language': 'english',
-    'language_id': 1
-}
-
-
-def test_can_show_calendar_public(session):
-    user = create_user(
-            session=session,
-            **USER_DATA
-    )
+def test_can_show_calendar_public(session, user):
     user.privacy = "Public"
-    #  to be replaced after user system is merged:
+    # TODO to be replaced after user system is merged:
     # current_user = CurrentUser(**user.__dict__)
     current_user = user
     result = can_show_calendar(
-        requested_user_username='new_test_username',
+        requested_user_username='test_username',
         db=session, current_user=current_user
     )
-    assert result
-    session.delete(user)
+    assert result is True
     session.commit()
 
 
-def test_can_show_calendar_private(session):
-    user = create_user(
-        session=session,
-        **USER_DATA
-    )
+def test_can_show_calendar_private(session, user):
     another_user = create_user(
         session=session,
         username='new_test_username2',
@@ -45,7 +27,7 @@ def test_can_show_calendar_private(session):
         language_id=1
     )
     current_user = user
-    # to be replaced after user system is merged:
+    # TODO to be replaced after user system is merged:
     # current_user = CurrentUser(**user.__dict__)
 
     result_a = can_show_calendar(
@@ -56,8 +38,7 @@ def test_can_show_calendar_private(session):
         requested_user_username='new_test_username',
         db=session, current_user=current_user
     )
-    assert not result_a
-    assert result_b
-    session.delete(user)
+    assert result_a is False
+    assert result_b is True
     session.delete(another_user)
     session.commit()
