71fc461d21b8b1d85cc9fb94c97ac14c0f2709ea
Noam <63012023+noam-y@users.noreply.github.com>
2021-02-12 00:54:50 +0200

    more formatting!!

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from datetime import datetime 
+from datetime import datetime
 
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -77,7 +77,8 @@ def disable_user(session: Session, user_id: int):
     """if user doesn't own any event, we will disable him and remove the user from all of its future events."""
     user_disabled = session.query(User).get(user_id)
     user_disabled.disabled = True
-    future_events_for_user = session.query(UserEvent.id).join(Event, Event.id == UserEvent.event_id).filter(
+    future_events_for_user = session.query(UserEvent.id)
+    .join(Event, Event.id == UserEvent.event_id).filter(
         UserEvent.user_id == user_id, Event.start > datetime.now()).all()
     for event_connection in future_events_for_user:
         session.delete(event_connection)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import pytest
 
-from app.routers.user import create_user, disable_user, does_user_exist, get_users
-from app.database.models import User,UserEvent,Event
+from app.routers.user import (
+    create_user, disable_user, does_user_exist, get_users
+)
+from app.database.models import User, UserEvent, Event
+
 
 # -----------------------------------------------------
 # Fixtures
 # -----------------------------------------------------
+
 
 @pytest.fixture
 def new_user(session):
@@ -21,23 +25,25 @@ def new_user(session):
     return user
 
 
-
 # -----------------------------------------------------
 # Tests
 # -----------------------------------------------------
 
+
 def test_disabling_user(new_user, session):
-    """makes sure user is disabled and doesn't have any future events when disabled."""
+    """makes sure user is disabled
+    and doesn't have any future events when disabled."""
     if disable_user(session, new_user.id):
         testing1 = session.query(User).get(new_user.id)
-        assert testing1.disabled == True
-        future_events = list(session.query(Event.id).join(UserEvent).filter(UserEvent.user_id == new_user.id, Event.start > datetime.now()))
+        assert testing1.disabled
+        future_events = list(session.query(Event.id).join(UserEvent)
+        .filter(UserEvent.user_id == new_user.id, Event.start > datetime.now()))
         assert len(future_events) == 0
     else:
-        user_owned_events = session.query(Event).join().filter(Event.start > datetime.now(),Event.owner_id == new_user.id)
+        user_owned_events = session.query(Event).join()
+        .filter(Event.start > datetime.now(),Event.owner_id == new_user.id)
         assert len(user_owned_events) > 0
 
-    
 
 class TestUser:
 
