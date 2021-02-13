819f2b25eb8763be6d8d360b003ac478e535c5d6
Elor Shoshan <elor550@gmail.com>
2021-02-13 16:49:15 +0200

    edit

--- app/internal/load_parasha.py
+++ app/internal/load_parasha.py
@@ -8,18 +8,19 @@ from typing import Dict, List, Optional
 
 def get_parashot_from_json() -> List[Dict[str, Optional[str]]]:
     """Reading all parashot from the a JSON file that have been copied from:
-    'https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on'
-                     '&mf=on&c=on&geo=geoname&geonameid=293397&m=50&s=on'
+    'https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on
+    &mf=on&c=on&geo=geoname&geonameid=293397&m=50&s=on'
     Each year the Json file will need to be re-updated according to this API in this way:
 
     def relevent_details(p):
-    parashot_dict = {'name': p['title'], 'hebrew': p['hebrew'], 'link': p['link'], 'date': p['date']}
+    parashot_dict = {'name': p['title'], 'hebrew': p['hebrew'],
+     'link': p['link'], 'date': p['date']}
     return parashot_dict
 
 
     def get_all_parashot():
-    r = requests.get('https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on'
-                     '&mf=on&c=on&geo=geoname&geonameid=293397&m=50&s=on')
+    r = requests.get('https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on
+    &nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=293397&m=50&s=on')
     items = r.json()['items']
     all_para = list(filter(lambda i: 'Parashat' in i['title'], items))
     return [relevent_details(p) for p in all_para]
@@ -37,12 +38,13 @@ def add_parashot_to_db(session: Session) -> None:
     """This function reads the parashot and inserts them into the db"""
     parashot = get_parashot_from_json()
     parashot_objects = [
-    Parasha(
-        name=parasha['name'],
-        hebrew_name=parasha['hebrew'],
-        link=parasha['link'],
-        date=datetime.strptime(parasha['date'], '%Y-%m-%d').date())
-    for parasha in parashot
+        Parasha(
+            name=parasha['name'],
+            hebrew_name=parasha['hebrew'],
+            link=parasha['link'],
+            date=datetime.strptime(parasha['date'], '%Y-%m-%d').date()
+        )
+        for parasha in parashot
     ]
     session.add_all(parashot_objects)
     session.commit()
