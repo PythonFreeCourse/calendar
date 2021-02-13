fdc91bd8538be9a8b05a4466f3beb2577197257c
fandomario <fandomario616@gmail.com>
2021-02-13 13:21:17 +0200

    fix: flake8 2

--- tests/test_weight.py
+++ tests/test_weight.py
@@ -1,10 +1,4 @@
 
-def test_ok(client):
-    resp = client.get('/weight')
-    assert resp.ok
-    assert b"Weight" in resp.content
-
-
 def test_post_ok(client, user):
     new_data = {'target': user.target_weight, 'current_weight': 60}
     resp = client.post('/weight/', data=new_data)
