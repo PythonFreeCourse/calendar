22802d106868aed0e20779a5447162c06ed1040c
Idan Pelled <pelled.idan@gmail.com>
2021-02-13 10:45:30 +0200

    add: peer review changes

--- app/internal/export.py
+++ app/internal/export.py
@@ -8,12 +8,12 @@ from sqlalchemy.orm import Session
 
 from app.config import DOMAIN, ICAL_VERSION, PRODUCT_ID
 from app.database.models import Event
+from app.internal.email import verify_email_pattern
 from app.routers.event import get_attendees_email
 
 
 def generate_id(event: Event) -> bytes:
     """Creates an unique id."""
-
     return (
             str(event.id)
             + event.start.strftime('%Y%m%d')
@@ -23,9 +23,7 @@ def generate_id(event: Event) -> bytes:
 
 
 def create_ical_calendar():
-    """Creates an ical calendar,
-    and adds the required information"""
-
+    """Creates an ical calendar, and adds the required information"""
     cal = Calendar()
     cal.add('version', ICAL_VERSION)
     cal.add('prodid', PRODUCT_ID)
@@ -35,7 +33,6 @@ def create_ical_calendar():
 
 def add_optional(user_event, data):
     """Adds an optional field if it exists."""
-
     if user_event.location:
         data.append(('location', user_event.location))
 
@@ -46,9 +43,7 @@ def add_optional(user_event, data):
 
 
 def create_ical_event(user_event):
-    """Creates an ical event,
-    and adds the event information"""
-
+    """Creates an ical event, and adds the event information"""
     ievent = IcalEvent()
     data = [
         ('organizer', add_attendee(user_event.owner.email, organizer=True)),
@@ -69,7 +64,6 @@ def create_ical_event(user_event):
 
 def add_attendee(email, organizer=False):
     """Adds an attendee to the event."""
-
     attendee = vCalAddress(f'MAILTO:{email}')
     if organizer:
         attendee.params['partstat'] = vText('ACCEPTED')
@@ -81,15 +75,18 @@ def add_attendee(email, organizer=False):
     return attendee
 
 
-def add_attendees(ievent, attendees: list):
+def add_attendees(
+        ievent: IcalEvent,
+        attendees: List[str],
+):
     """Adds attendees for the event."""
-
     for email in attendees:
-        ievent.add(
-            'attendee',
-            add_attendee(email),
-            encode=0
-        )
+        if verify_email_pattern(email):
+            ievent.add(
+                'attendee',
+                add_attendee(email),
+                encode=0
+            )
 
     return ievent
 
@@ -97,7 +94,6 @@ def add_attendees(ievent, attendees: list):
 def event_to_ical(user_event: Event, attendees: List[str]) -> bytes:
     """Returns an ical event, given an
     "Event" instance and a list of email."""
-
     ical = create_ical_calendar()
     ievent = create_ical_event(user_event)
     ievent = add_attendees(ievent, attendees)
@@ -109,7 +105,6 @@ def event_to_ical(user_event: Event, attendees: List[str]) -> bytes:
 def export_calendar(session: Session, events: List[Event]) -> bytes:
     """Returns an icalendar, given an list of
     "Event" instances and a list of email."""
-
     ical = create_ical_calendar()
     for event in events:
         ievent = create_ical_event(event)
