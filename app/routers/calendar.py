91b9fe3a611667926780c1947d9881cd215533b6
Elor Shoshan <elor550@gmail.com>
2021-02-14 13:58:59 +0200

    edit by yam

--- app/routers/calendar.py
+++ app/routers/calendar.py
@@ -2,8 +2,8 @@ from http import HTTPStatus
 
 
 from app.dependencies import get_db, templates
-from app.internal import load_parasha as lp
-from app.routers import calendar_grid as cg
+from app.internal import load_parasha
+from app.routers import calendar_grid
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from starlette.responses import Response
@@ -21,26 +21,26 @@ ADD_DAYS_ON_SCROLL: int = 42
 
 @router.get("/")
 async def calendar(request: Request, db_session=Depends(get_db)) -> Response:
-    user_local_time = cg.Day.get_user_local_time()
-    day = cg.create_day(user_local_time)
-    parasha_obj = lp.get_weekly_parasha(db_session)
+    user_local_time = calendar_grid.Day.get_user_local_time()
+    day = calendar_grid.create_day(user_local_time)
+    parasha_obj = load_parasha.get_weekly_parasha(db_session)
 
     return templates.TemplateResponse(
         "calendar_monthly_view.html",
         {
             "request": request,
             "day": day,
-            "week_days": cg.Week.DAYS_OF_THE_WEEK,
-            "weeks_block": cg.get_month_block(day),
-            "parasha": parasha_obj
+            "week_days": calendar_grid.Week.DAYS_OF_THE_WEEK,
+            "weeks_block": calendar_grid.get_month_block(day),
+            "parashot": parasha_obj
         }
     )
 
 
 @router.get("/{date}")
 async def update_calendar(request: Request, date: str) -> HTMLResponse:
-    last_day = cg.Day.convert_str_to_date(date)
-    next_weeks = cg.create_weeks(cg.get_n_days(last_day, ADD_DAYS_ON_SCROLL))
+    last_day = calendar_grid.Day.convert_str_to_date(date)
+    next_weeks = calendar_grid.create_weeks(calendar_grid.get_n_days(last_day, ADD_DAYS_ON_SCROLL))
     template = templates.get_template(
         'partials/calendar/monthly_view/add_week.html')
     content = template.render(weeks_block=next_weeks)
