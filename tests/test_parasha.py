2f7b60abcec0b17d2de146c78435f8a896092ec5
Elor Shoshan <elor550@gmail.com>
2021-02-14 14:27:16 +0200

    edit flake8

--- tests/test_parasha.py
+++ tests/test_parasha.py
@@ -1,11 +1,11 @@
-from app.internal import load_parasha as lp
+from app.internal import load_parasha
 
 
 def test_if_db_correct():
     example = [{'name': 'Parashat Vayechi', 'hebrew': 'פרשת ויחי', " \
                 "'link': 'https://www.hebcal.com/sedrot/vayechi-20210102?i=on&utm_source=js&utm_medium=api', " \
                 "'date': '2021-01-02'}]
-    result = lp.get_weekly_parasha(example)
+    result = load_parasha.get_weekly_parasha(example)
     assert result.name == 'Parashat Vayechi'
     assert result.date == '2021-01-02'
     assert result.count() == 1
