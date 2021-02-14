63fddba6378553a8f1a951bfee56b7ac2692c44b
nadav pesach <nadav-pesach@users.noreply.github.com>
2021-02-14 23:16:01 +0200

    minor fix

--- tests/test_event.py
+++ tests/test_event.py
@@ -378,7 +378,8 @@ def test_changeowner(client, event_test_client, user, session, event):
     event_id = event.id
     event_details = [event.title, event.content, event.location, event.start,
                      event.end, event.color, event.category_id]
-    response = event_test_client.post(f"/event/{event_id}/owner_changed", data=None)
+    response = event_test_client.post(f"/event/{event_id}/owner_changed",
+                                      data=None)
     assert response.status_code == status.HTTP_302_FOUND
     assert response.ok
     assert b"View Event" not in response.content
@@ -386,11 +387,13 @@ def test_changeowner(client, event_test_client, user, session, event):
         assert str(event_detail).encode('utf-8') not in response.content, \
             f'{event_detail} not in view event page'
     data = {'username': "worng_username"}
-    response = event_test_client.post(f"/event/{event_id}/owner_changed", data=data)
+    response = event_test_client.post(f"/event/{event_id}/owner_changed",
+                                      data=data)
     assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
     assert b'Username does not exist.' in response.content
     data = {'username': user.username}
-    response = event_test_client.post(f"/event/{event_id}/owner_changed", data=data)
+    response = event_test_client.post(f"/event/{event_id}/owner_changed",
+                                      data=data)
     assert response.ok
     assert response.status_code == status.HTTP_302_FOUND
 
