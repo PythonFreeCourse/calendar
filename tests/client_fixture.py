dc3c0c925e5e8968406892a8f94d3cf7084218b5
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2021-02-15 13:13:31 +0200

    fix: revert pre-commit hook changes

--- tests/client_fixture.py
+++ tests/client_fixture.py
@@ -15,15 +15,17 @@ main.app.include_router(security_testing_routes.router)
 
 
 def get_test_placeholder_user() -> Iterator[User]:
-    return User(username='fake_user',
-                email='fake@mail.fake',
-                password='123456fake',
-                full_name='FakeName',
-                language_id=1,
-                telegram_id='666666')
+    return User(
+        username='fake_user',
+        email='fake@mail.fake',
+        password='123456fake',
+        full_name='FakeName',
+        language_id=1,
+        telegram_id='666666'
+    )
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def client() -> Iterator[TestClient]:
     return TestClient(main.app)
 
@@ -39,27 +41,27 @@ def create_test_client(get_db_function) -> Iterator[TestClient]:
     Base.metadata.drop_all(bind=test_engine)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def agenda_test_client() -> Iterator[TestClient]:
     yield from create_test_client(agenda.get_db)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def event_test_client() -> Iterator[TestClient]:
     yield from create_test_client(event.get_db)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def home_test_client() -> Iterator[TestClient]:
     yield from create_test_client(main.get_db)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def invitation_test_client() -> Iterator[TestClient]:
     yield from create_test_client(invitation.get_db)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def profile_test_client() -> Iterator[TestClient]:
     Base.metadata.create_all(bind=test_engine)
     main.app.dependency_overrides[profile.get_db] = get_test_db
@@ -73,12 +75,12 @@ def profile_test_client() -> Iterator[TestClient]:
     Base.metadata.drop_all(bind=test_engine)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def security_test_client():
     yield from create_test_client(event.get_db)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def salary_test_client() -> Iterator[TestClient]:
     yield from create_test_client(salary.get_db)
 
@@ -95,6 +97,6 @@ def google_connect_test_client():
     Base.metadata.drop_all(bind=test_engine)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def notes_test_client() -> Iterator[TestClient]:
     yield from create_test_client(notes.get_db)
