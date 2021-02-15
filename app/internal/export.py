767c5cc8f33d0d96918c742e390f451d23b30788
Gonny <gonnym@gmail.com>
2021-02-15 14:48:12 +0200

    fix: export.py
    Added and updated doc, changed function names, updated tests, and other format fixes.

--- app/internal/export.py
+++ app/internal/export.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from typing import List
 
+from icalendar import Calendar, Event as IEvent, vCalAddress, vText
 import pytz
-from icalendar import Calendar, vCalAddress, vText
-from icalendar import Event as IcalEvent
 from sqlalchemy.orm import Session
 
 from app.config import DOMAIN, ICAL_VERSION, PRODUCT_ID
@@ -12,8 +11,19 @@ from app.internal.email import verify_email_pattern
 from app.routers.event import get_attendees_email
 
 
-def generate_id(event: Event) -> bytes:
-    """Creates an unique id."""
+def _generate_id(event: Event) -> bytes:
+    """Generates a unique encoded ID.
+
+    The ID is generated from the Event ID, start and end times
+    and the domain name.
+
+    Args:
+        event: The Event.
+
+    Returns:
+        A unique encoded ID in bytes.
+
+    """
     return (
             str(event.id)
             + event.start.strftime('%Y%m%d')
@@ -22,48 +32,57 @@ def generate_id(event: Event) -> bytes:
     ).encode()
 
 
-def create_ical_calendar():
-    """Creates an ical calendar, and adds the required information"""
-    cal = Calendar()
-    cal.add('version', ICAL_VERSION)
-    cal.add('prodid', PRODUCT_ID)
+def _create_icalendar() -> Calendar:
+    """Returns an iCalendar."""
+    calendar = Calendar()
+    calendar.add('version', ICAL_VERSION)
+    calendar.add('prodid', PRODUCT_ID)
 
-    return cal
-
-
-def add_optional(user_event, data):
-    """Adds an optional field if it exists."""
-    if user_event.location:
-        data.append(('location', user_event.location))
-
-    if user_event.content:
-        data.append(('description', user_event.content))
-
-    return data
+    return calendar
 
 
-def create_ical_event(user_event):
-    """Creates an ical event, and adds the event information"""
-    ievent = IcalEvent()
+def _create_icalendar_event(event: Event) -> IEvent:
+    """Returns an iCalendar event with event data.
+
+    Args:
+        event: The Event to transform into an iCalendar event.
+
+    Returns:
+        An iCalendar event.
+
+    """
     data = [
-        ('organizer', add_attendee(user_event.owner.email, organizer=True)),
-        ('uid', generate_id(user_event)),
-        ('dtstart', user_event.start),
+        ('organizer', _get_v_cal_address(event.owner.email, organizer=True)),
+        ('uid', _generate_id(event)),
+        ('dtstart', event.start),
         ('dtstamp', datetime.now(tz=pytz.utc)),
-        ('dtend', user_event.end),
-        ('summary', user_event.title),
+        ('dtend', event.end),
+        ('summary', event.title),
     ]
 
-    data = add_optional(user_event, data)
+    if event.location:
+        data.append(('location', event.location))
 
+    if event.content:
+        data.append(('description', event.content))
+
+    ievent = IEvent()
     for param in data:
         ievent.add(*param)
 
     return ievent
 
 
-def add_attendee(email, organizer=False):
-    """Adds an attendee to the event."""
+def _get_v_cal_address(email: str, organizer: bool = False) -> vCalAddress:
+    """Returns a vCalAddress for an attendee.
+
+    Args:
+        email: The email of the attendee.
+        organizer: A flag whether or not the user is the event organizer.
+
+    Returns:
+
+    """
     attendee = vCalAddress(f'MAILTO:{email}')
     if organizer:
         attendee.params['partstat'] = vText('ACCEPTED')
@@ -75,44 +94,78 @@ def add_attendee(email, organizer=False):
     return attendee
 
 
-def add_attendees(
-        ievent: IcalEvent,
-        attendees: List[str],
-):
-    """Adds attendees for the event."""
-    for email in attendees:
+def _add_attendees(ievent: IEvent, emails: List[str]):
+    """Adds attendees to the event.
+
+    Args:
+        ievent: The iCalendar event.
+        emails: A list of attendees emails.
+
+    """
+    for email in emails:
         if verify_email_pattern(email):
-            ievent.add(
-                'attendee',
-                add_attendee(email),
-                encode=0
-            )
-
-    return ievent
+            ievent.add('attendee', _get_v_cal_address(email), encode=0)
 
 
-def event_to_ical(user_event: Event, attendees: List[str]) -> bytes:
-    """Returns an ical event, given an
-    "Event" instance and a list of email."""
-    ical = create_ical_calendar()
-    ievent = create_ical_event(user_event)
-    ievent = add_attendees(ievent, attendees)
-    ical.add_component(ievent)
+def get_icalendar(event: Event, emails: List[str]) -> bytes:
+    """Returns an iCalendar event in bytes.
 
-    return ical.to_ical()
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
+
+    Args:
+        event: The Event.
+        emails: A list of emails.
+
+    Returns:
+        An iCalendar that can be used as a string for a file.
+
+    """
+    icalendar = _create_icalendar()
+    ievent = _get_icalendar_event(event, emails)
+    icalendar.add_component(ievent)
+    return icalendar.to_ical()
 
 
-def export_calendar(session: Session, events: List[Event]) -> bytes:
-    """Returns an icalendar, given an list of
-    "Event" instances and a list of email."""
-    ical = create_ical_calendar()
+def get_icalendar_with_multiple_events(session: Session, events: List[Event]
+                                       ) -> bytes:
+    """Returns an iCalendar event in bytes.
+
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
+
+    Args:
+        session: The database connection.
+        events: A list of Events.
+
+    Returns:
+        An iCalendar that can be used as a string for a file.
+
+    """
+    icalendar = _create_icalendar()
     for event in events:
-        ievent = create_ical_event(event)
+        emails = get_attendees_email(session, event)
+        emails.remove((event.owner.email,))
+        ievent = _get_icalendar_event(event, emails)
+        icalendar.add_component(ievent)
 
-        attendees = get_attendees_email(session, event)
-        attendees.remove((event.owner.email,))
+    return icalendar.to_ical()
 
-        ievent = add_attendees(ievent, attendees)
-        ical.add_component(ievent)
 
-    return ical.to_ical()
+def _get_icalendar_event(event: Event, emails: List[str]) -> IEvent:
+    """Returns an iCalendar event in bytes.
+
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
+
+    Args:
+        event: The Event.
+        emails: A list of emails.
+
+    Returns:
+        A iCalendar that can be used as a string for a file.
+
+    """
+    ievent = _create_icalendar_event(event)
+    _add_attendees(ievent, emails)
+    return ievent
--- app/routers/export.py
+++ app/routers/export.py
@@ -2,36 +2,47 @@ from datetime import date
 from io import BytesIO
 from typing import Union
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
-from starlette.responses import StreamingResponse
 
 from app.dependencies import get_db
 from app.internal.agenda_events import get_events_in_time_frame
-from app.internal.export import export_calendar
+from app.internal.export import get_icalendar_with_multiple_events
+from app.internal.utils import get_current_user
 
 router = APIRouter(
     prefix="/export",
     tags=["export"],
-    responses={404: {"description": "Not found"}},
+    responses={status.HTTP_404_NOT_FOUND: {"description": _("Not found")}},
 )
 
 
 @router.get("/")
 def export(
-        start_date: Union[date, str],  # date or an empty string
+        start_date: Union[date, str],
         end_date: Union[date, str],
         db: Session = Depends(get_db),
 ) -> StreamingResponse:
+    """Returns the Export page route.
 
-    user_id = 1
-    events = get_events_in_time_frame(start_date, end_date, user_id, db)
-    file = BytesIO(export_calendar(db, list(events)))
+    Args:
+        start_date: A date or an empty string.
+        end_date: A date or an empty string.
+        db: Optional; The database connection.
+
+    Returns:
+
+    """
+    # TODO: connect to real user
+    user = get_current_user(db)
+    events = get_events_in_time_frame(start_date, end_date, user.id, db)
+    file = BytesIO(get_icalendar_with_multiple_events(db, list(events)))
     return StreamingResponse(
         content=file,
         media_type="text/calendar",
         headers={
-            # change filename to "pylendar.ics"
-            "Content-Disposition": "attachment;filename=pylendar.ics"
+            # Change filename to "pylandar.ics".
+            "Content-Disposition": "attachment;filename=pylandar.ics",
         },
     )
