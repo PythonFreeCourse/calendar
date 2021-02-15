0736e3435b78295a4d5f0d8f34a290f5353a8a43
Gonny <gonnym@gmail.com>
2021-02-15 09:37:40 +0200

    fix: json_data_loader.py
    Add and updated doc, corrected type annotations, changed function names, updated tests, updated usage in main.py, and other format fixes.

--- app/internal/json_data_loader.py
+++ app/internal/json_data_loader.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Callable, Dict, List, Union
+from typing import Any, Callable, Dict, List
 
 from loguru import logger
 from sqlalchemy.orm import Session
@@ -9,48 +9,95 @@ from app.database.models import Base, Quote, Zodiac
 from app.internal import daily_quotes, zodiac
 
 
-def get_data_from_json(path: str) -> List[Dict[str, Union[str, int, None]]]:
-    """This function reads all of the data from a specific JSON file.
-    The json file consists of list of dictionaries"""
+def load_to_database(session: Session) -> None:
+    """Loads data from JSON data files into the database.
+
+    On startup, data from the JSON files should be added to the database and
+    not be accessed from a network call for each request as it is costly.
+
+    The quotes JSON file content is copied from the free API:
+    'https://type.fit/api/quotes'.
+
+    Args:
+        session: The database connection.
+
+    """
+    _insert_into_database(
+        session,
+        'app/resources/zodiac.json',
+        Zodiac,
+        zodiac.create_zodiac_object,
+    )
+
+    _insert_into_database(
+        session,
+        'app/resources/quotes.json',
+        Quote,
+        daily_quotes.create_quote_object,
+    )
+
+
+def _insert_into_database(
+        session: Session,
+        path: str,
+        table: Base,
+        model_creator: Callable
+) -> bool:
+    """Inserts the extracted JSON data into the database.
+
+    Args:
+        session: The database connection.
+        path: The file path.
+        table: A model entity table.
+        model_creator: A model creation function.
+
+    Returns:
+        True if the save was successful, otherwise returns False.
+
+    """
+    if not _is_table_empty(session, table):
+        return False
+
+    json_objects = _get_data_from_json(path)
+    model_objects = [model_creator(json_object)
+                     for json_object in json_objects]
+    session.add_all(model_objects)
+    session.commit()
+    return True
+
+
+def _is_table_empty(session: Session, table: Base) -> bool:
+    """Returns True if the table is empty.
+
+    Args:
+        session: The database connection.
+        table: A model entity table.
+
+    Returns:
+        True if the table is empty, otherwise returns False.
+    """
+    return session.query(table).count() == 0
+
+
+def _get_data_from_json(path: str) -> List[Dict[str, Any]]:
+    """Returns a list of dictionary objects.
+
+    Reads the data from a specific JSON file and converts the data into
+    a list of dictionary items.
+
+    Args:
+        path: The file path.
+
+    Returns:
+        A list of dictionary objects.
+
+    """
     try:
-        with open(path, 'r') as f:
-            json_content_list = json.load(f)
+        with open(path, 'r') as json_file:
+            json_content = json.load(json_file)
     except (IOError, ValueError):
         file_name = os.path.basename(path)
         logger.exception(
             f"An error occurred during reading of json file: {file_name}")
         return []
-    return json_content_list
-
-
-def is_table_empty(session: Session, table: Base) -> bool:
-    return session.query(table).count() == 0
-
-
-def load_data(
-        session: Session, path: str,
-        table: Base, object_creator_function: Callable) -> None:
-    """This function loads the specific data to the db,
-    if it wasn't already loaded"""
-    if not is_table_empty(session, table):
-        return None
-    json_objects_list = get_data_from_json(path)
-    objects = [
-        object_creator_function(json_object)
-        for json_object in json_objects_list]
-    session.add_all(objects)
-    session.commit()
-
-
-def load_to_db(session) -> None:
-    """This function loads all the data for features
-    based on pre-defind json data"""
-    load_data(
-        session, 'app/resources/zodiac.json',
-        Zodiac, zodiac.create_zodiac_object)
-    load_data(
-        session, 'app/resources/quotes.json',
-        Quote, daily_quotes.create_quote_object)
-    """The quotes JSON file content is copied from the free API:
-    'https://type.fit/api/quotes'. I saved the content so the API won't be
-    called every time the app is initialized."""
+    return json_content
--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
+from fastapi import Depends, FastAPI, Request, status
+from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
+
 from app import config
 from app.database import engine, models
 from app.dependencies import get_db, logger, MEDIA_PATH, STATIC_PATH, templates
 from app.internal import daily_quotes, json_data_loader
-
 from app.internal.languages import set_ui_language
 from app.internal.security.ouath2 import auth_exception_handler
 from app.routers.salary import routes as salary
-from fastapi import Depends, FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from starlette.status import HTTP_401_UNAUTHORIZED
-from sqlalchemy.orm import Session
 
 
 def create_tables(engine, psql_environment):
@@ -30,12 +29,10 @@ app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 app.mount("/media", StaticFiles(directory=MEDIA_PATH), name="media")
 app.logger = logger
 
-app.add_exception_handler(HTTP_401_UNAUTHORIZED, auth_exception_handler)
+app.add_exception_handler(status.HTTP_401_UNAUTHORIZED, auth_exception_handler)
 
-json_data_loader.load_to_db(next(get_db()))
 # This MUST come before the app.routers imports.
 set_ui_language()
-
 
 from app.routers import (  # noqa: E402
 
@@ -44,7 +41,7 @@ from app.routers import (  # noqa: E402
     register, search, telegram, weekview, whatsapp,
 )
 
-json_data_loader.load_to_db(next(get_db()))
+json_data_loader.load_to_database(next(get_db()))
 
 routers_to_include = [
     agenda.router,
