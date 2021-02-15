647551b11294730ae7412a12dff6fc585f095ac1
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2021-02-16 00:48:51 +0200

    fix: remove unready test

--- tests/notes/test_notes_routes.py
+++ tests/notes/test_notes_routes.py
@@ -6,8 +6,3 @@ def test_notes_page(client):
 def test_add_note_page(client):
     response = client.get("/notes/add")
     assert response.ok
-
-
-def test_edit_note_page(client):
-    response = client.get("/notes/1")
-    assert response.ok
