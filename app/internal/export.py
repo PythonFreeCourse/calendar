fcf103bb6b8b2ae876f5520a4f7fa437b1411edb
Gonny <gonnym@gmail.com>
2021-02-15 15:01:36 +0200

    fix: export.py
    Reorder function list.

--- app/internal/export.py
+++ app/internal/export.py
@@ -11,25 +11,49 @@ from app.internal.email import verify_email_pattern
 from app.routers.event import get_attendees_email
 
 
-def _generate_id(event: Event) -> bytes:
-    """Generates a unique encoded ID.
+def get_icalendar(event: Event, emails: List[str]) -> bytes:
+    """Returns an iCalendar event in bytes.
 
-    The ID is generated from the Event ID, start and end times
-    and the domain name.
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
 
     Args:
         event: The Event.
+        emails: A list of emails.
 
     Returns:
-        A unique encoded ID in bytes.
+        An iCalendar that can be used as a string for a file.
 
     """
-    return (
-            str(event.id)
-            + event.start.strftime('%Y%m%d')
-            + event.end.strftime('%Y%m%d')
-            + f'@{DOMAIN}'
-    ).encode()
+    icalendar = _create_icalendar()
+    ievent = _get_icalendar_event(event, emails)
+    icalendar.add_component(ievent)
+    return icalendar.to_ical()
+
+
+def get_icalendar_with_multiple_events(session: Session, events: List[Event]
+                                       ) -> bytes:
+    """Returns an iCalendar event in bytes.
+
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
+
+    Args:
+        session: The database connection.
+        events: A list of Events.
+
+    Returns:
+        An iCalendar that can be used as a string for a file.
+
+    """
+    icalendar = _create_icalendar()
+    for event in events:
+        emails = get_attendees_email(session, event)
+        emails.remove((event.owner.email,))
+        ievent = _get_icalendar_event(event, emails)
+        icalendar.add_component(ievent)
+
+    return icalendar.to_ical()
 
 
 def _create_icalendar() -> Calendar:
@@ -39,6 +63,25 @@ def _create_icalendar() -> Calendar:
     calendar.add('prodid', PRODUCT_ID)
 
     return calendar
+
+
+def _get_icalendar_event(event: Event, emails: List[str]) -> IEvent:
+    """Returns an iCalendar event in bytes.
+
+    Builds an iCalendar event with information from the Event object.
+    and a list of emails.
+
+    Args:
+        event: The Event.
+        emails: A list of emails.
+
+    Returns:
+        A iCalendar that can be used as a string for a file.
+
+    """
+    ievent = _create_icalendar_event(event)
+    _add_attendees(ievent, emails)
+    return ievent
 
 
 def _create_icalendar_event(event: Event) -> IEvent:
@@ -94,6 +137,27 @@ def _get_v_cal_address(email: str, organizer: bool = False) -> vCalAddress:
     return attendee
 
 
+def _generate_id(event: Event) -> bytes:
+    """Generates a unique encoded ID.
+
+    The ID is generated from the Event ID, start and end times
+    and the domain name.
+
+    Args:
+        event: The Event.
+
+    Returns:
+        A unique encoded ID in bytes.
+
+    """
+    return (
+            str(event.id)
+            + event.start.strftime('%Y%m%d')
+            + event.end.strftime('%Y%m%d')
+            + f'@{DOMAIN}'
+    ).encode()
+
+
 def _add_attendees(ievent: IEvent, emails: List[str]):
     """Adds attendees to the event.
 
@@ -105,67 +169,3 @@ def _add_attendees(ievent: IEvent, emails: List[str]):
     for email in emails:
         if verify_email_pattern(email):
             ievent.add('attendee', _get_v_cal_address(email), encode=0)
-
-
-def get_icalendar(event: Event, emails: List[str]) -> bytes:
-    """Returns an iCalendar event in bytes.
-
-    Builds an iCalendar event with information from the Event object.
-    and a list of emails.
-
-    Args:
-        event: The Event.
-        emails: A list of emails.
-
-    Returns:
-        An iCalendar that can be used as a string for a file.
-
-    """
-    icalendar = _create_icalendar()
-    ievent = _get_icalendar_event(event, emails)
-    icalendar.add_component(ievent)
-    return icalendar.to_ical()
-
-
-def get_icalendar_with_multiple_events(session: Session, events: List[Event]
-                                       ) -> bytes:
-    """Returns an iCalendar event in bytes.
-
-    Builds an iCalendar event with information from the Event object.
-    and a list of emails.
-
-    Args:
-        session: The database connection.
-        events: A list of Events.
-
-    Returns:
-        An iCalendar that can be used as a string for a file.
-
-    """
-    icalendar = _create_icalendar()
-    for event in events:
-        emails = get_attendees_email(session, event)
-        emails.remove((event.owner.email,))
-        ievent = _get_icalendar_event(event, emails)
-        icalendar.add_component(ievent)
-
-    return icalendar.to_ical()
-
-
-def _get_icalendar_event(event: Event, emails: List[str]) -> IEvent:
-    """Returns an iCalendar event in bytes.
-
-    Builds an iCalendar event with information from the Event object.
-    and a list of emails.
-
-    Args:
-        event: The Event.
-        emails: A list of emails.
-
-    Returns:
-        A iCalendar that can be used as a string for a file.
-
-    """
-    ievent = _create_icalendar_event(event)
-    _add_attendees(ievent, emails)
-    return ievent
