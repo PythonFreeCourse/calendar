2c166248022c08197902763fcf5b5c79ed0286c6
Elor Shoshan <elor550@gmail.com>
2021-02-17 00:41:08 +0200

    edit after flake8

--- app/internal/weekly_parasha.py
+++ app/internal/weekly_parasha.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Dict, Optional, List
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter
 from sqlalchemy.orm import Session
 
 from app.database.models import Parasha
@@ -10,8 +10,9 @@ from app.database.models import Parasha
 router = APIRouter()
 
 
-def create_parasha_object(parashot_fields: Dict[str, Optional[str]]) -> Parasha:
-    """This function create a quote object from given fields dictionary.
+def create_parasha_object(parashot_fields: Dict[str, Optional[str]])\
+        -> Parasha:
+    """This function create a parasha object from given fields dictionary.
     It is used for adding the data from the json into the db"""
     return Parasha(
         name=parashot_fields['name'],
@@ -21,6 +22,7 @@ def create_parasha_object(parashot_fields: Dict[str, Optional[str]]) -> Parasha:
     )
 
 
-def get_all_parahot_list(db_session: Session) -> List[Dict[str, Optional[str]]]:
+def get_all_parahot_list(db_session: Session) \
+        -> List[Dict[str, Optional[str]]]:
     """This function return all parashot object in list"""
     return db_session.query(Parasha).all()
