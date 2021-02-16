e08ee634b402a38d001d3fb310d9d78103c44e43
Elor Shoshan <elor550@gmail.com>
2021-02-17 00:48:01 +0200

    edit after pytest

--- tests/test_weekly_parasha.py
+++ tests/test_weekly_parasha.py
@@ -4,7 +4,7 @@ from app.internal import weekly_parasha
 def test_create_parasha_object():
     parashot_fields = {
         'name': 'Parashat Vayechi',
-        'hebrew_name': 'פרשת ויחי',
+        'hebrew': 'פרשת ויחי',
         'link': 'https://www.hebcal.com/sedrot/vayechi-20210102?'
         'i=on&utm_source=js&utm_medium=api',
         'date': '2021-01-02'
