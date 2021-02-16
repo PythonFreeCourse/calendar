c93322fd29a9299535de39b68fd9e6fdec324827
Adi Faibish <adidas2712@gmail.com>
2021-02-16 17:51:00 +0200

    fixfixfixfix

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ json_data_loader.load_to_db(next(get_db()))
 set_ui_language()
 
 from app.routers import (  # noqa: E402
-    about_us, agenda, calendar, categories, celebrity, credits, 
+    about_us, agenda, calendar, categories, celebrity, credits,
     currency, dayview, email, event, export, four_o_four,
     google_connect, invitation, login, logout, profile,
     register, search, telegram, user, weekview, whatsapp,
