b072699a2ce137877d9cf48cf085cf77bbec4d60
Elor Shoshan <elor550@gmail.com>
2021-02-16 18:37:56 +0200

    edit after pytest

--- tests/test_parasha.py
+++ tests/test_parasha.py
@@ -2,12 +2,12 @@ from app.internal import load_parasha
 from sqlalchemy.orm import Session
 
 
-def test_if_db_correct():
+def test_if_db_correct(session: Session):
     first_parasha = [0, 'Parashat Vayechi', 'פרשת ויחי',
                      'https://www.hebcal.com/sedrot/vayechi-20210102?',
                      'i=on&utm_source=js&utm_medium=api',
                      '2021-01-02']
-    result = load_parasha.get_weekly_parasha(Session)[0]
+    result = load_parasha.get_weekly_parasha(session)[0]
     assert result.name is first_parasha[1]
     assert result.date is first_parasha[5]
     assert result.count() == 1
