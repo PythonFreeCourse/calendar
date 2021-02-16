76472c127ba7cdb717fa3c53269436552a6d6594
Elor Shoshan <elor550@gmail.com>
2021-02-16 20:12:29 +0200

    edit after pytest

--- tests/test_parasha.py
+++ tests/test_parasha.py
@@ -1,12 +1,13 @@
+from app.dependencies import get_db
 from app.internal import load_parasha
-from sqlalchemy.orm import Session
+from fastapi import Depends
 
 
-def test_if_db_correct(session: Session):
+def test_if_db_correct():
     first_parasha = [0, 'Parashat Vayechi', 'פרשת ויחי',
                      'https://www.hebcal.com/sedrot/vayechi-20210102?',
                      'i=on&utm_source=js&utm_medium=api',
                      '2021-01-02']
-    result = load_parasha.get_weekly_parasha(session).first()
+    result = load_parasha.get_weekly_parasha(Depends(get_db))
     assert result.name is first_parasha[1]
     assert result.date is first_parasha[5]
