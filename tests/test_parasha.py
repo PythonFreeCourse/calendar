18ed51d3f6f4a9968dc3c0c7ebbfede798b6d0d2
Elor Shoshan <elor550@gmail.com>
2021-02-16 17:59:13 +0200

    edit after pytest

--- tests/test_parasha.py
+++ tests/test_parasha.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 
 
 def test_if_db_correct():
-    first_parasha =[0, 'Parashat Vayechi', 'פרשת ויחי',
-       'https://www.hebcal.com/sedrot/vayechi-20210102?'
-       'i=on&utm_source=js&utm_medium=api',
-       '2021-01-02']
+    first_parasha = [0, 'Parashat Vayechi', 'פרשת ויחי',
+                     'https://www.hebcal.com/sedrot/vayechi-20210102?',
+                     'i=on&utm_source=js&utm_medium=api',
+                     '2021-01-02']
     result = load_parasha.get_weekly_parasha(Session)[0]
     assert result.name == first_parasha[1]
     assert result.date == first_parasha[5]
