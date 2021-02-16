992841ab2baee644141d8eced079ed4ab54626d0
Elor Shoshan <elor550@gmail.com>
2021-02-16 19:58:29 +0200

    edit after pytest

--- tests/test_parasha.py
+++ tests/test_parasha.py
@@ -7,8 +7,6 @@ def test_if_db_correct(session: Session):
                      'https://www.hebcal.com/sedrot/vayechi-20210102?',
                      'i=on&utm_source=js&utm_medium=api',
                      '2021-01-02']
-
     result = load_parasha.get_weekly_parasha(session)
     assert result[0].name is first_parasha[1]
     assert result[0].date is first_parasha[5]
-    
