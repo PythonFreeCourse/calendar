d52f52f043cd1a7bde53ef5025c05d02aef71065
Yair Engel <yair1engel@gmail.com>
2021-02-16 20:57:53 +0200

    Fix Js Out of office

--- app/static/out_of_office/out_of_office.js
+++ app/static/out_of_office/out_of_office.js
@@ -4,9 +4,13 @@ function findselected() {
     if (result == deactivate) {
         document.getElementById("start_date").setAttribute('disabled', true);
         document.getElementById("end_date").setAttribute('disabled', true);
+        document.getElementById("start_time").setAttribute('disabled', true);
+        document.getElementById("end_time").setAttribute('disabled', true);
     }
     else {
         document.getElementById("start_date").removeAttribute('disabled');
         document.getElementById("end_date").removeAttribute('disabled');
+        document.getElementById("start_time").removeAttribute('disabled');
+        document.getElementById("end_time").removeAttribute('disabled');
     }
 }
