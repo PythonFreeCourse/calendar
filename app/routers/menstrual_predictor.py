64c889b50d06a3b77a6da0d3810b73404d6b238e
Liad Noam <LiadN@Taldor.corp>
2021-02-16 22:23:48 +0200

    Menstrual predictor before tests

--- app/routers/menstrual_predictor.py
+++ app/routers/menstrual_predictor.py
@@ -1,24 +1,19 @@
 import datetime
 from typing import List, Union
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_302_FOUND
-from starlette.templating import Jinja2Templates
 
 from app.database.models import Event, UserMenstrualPeriodLength
 from app.dependencies import get_db, templates
-from app.routers.share import accept
 from app.routers.event import create_event
-from app.routers.user import create_user
 from app.internal.utils import create_model, get_current_user
 
 from loguru import logger
 
-
-# templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter(
     prefix="/menstrual_predictor",
@@ -28,14 +23,39 @@ router = APIRouter(
 
 MENSTRUAL_PERIOD_CATEGORY_ID = 111
 
+
 @router.get("/")
 def join(request: Request, db: Session = Depends(get_db)):
-    logger.info('getting menstrual predictor')
-    return templates.TemplateResponse("join_menstrual_predictor.html", {
-        "request": request,
-        # TODO:
-        # "invitations": get_all_invitations(session=db),
-    })
+    current_user_id = get_current_user(db).id
+
+    if not is_user_signed_up_to_menstrual_predictor(db, current_user_id):
+        logger.info('getting menstrual predictor')
+        return templates.TemplateResponse("join_menstrual_predictor.html", {
+            "request": request,
+        })
+    else:
+        return RedirectResponse(url='/', status_code=HTTP_302_FOUND)
+
+
+@router.get("/add-period-start/{start_date}")
+def add_period_start(request: Request, start_date: str, db: Session = Depends(get_db)):
+    try:
+        period_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    except ValueError as err:
+        logger.exception(err)
+        raise HTTPException(
+            status_code=400, detail="The given date doesn't match a date format YYYY-MM-DD")
+    else:
+        current_user_id = get_current_user(db).id
+        user_period_length = is_user_signed_up_to_menstrual_predictor(
+            db, current_user_id)
+
+        remove_existing_period_dates(db, current_user_id)
+        if user_period_length:
+            add_3_month_predictions(
+                db, user_period_length, period_start_date, current_user_id)
+    logger.info('adding menstrual start date')
+    return RedirectResponse('/', status_code=303)
 
 
 @router.post("/")
@@ -43,69 +63,108 @@ async def submit_join_form(
     request: Request,
     db: Session = Depends(get_db)
 ):
-    # user = create_user(username='shimi', password='lalala', email='shimi@shimi.com', language_id=0, session=db, language='hebrew')
-    # event = create_event(db, 'period day', datetime.datetime.now(), datetime.datetime.now(), 1, category_id=MENSTRUAL_PERIOD_CATEGORY_ID)
-    # db.add(event)
-    # db.commit()
+
     data = await request.form()
-    # url = router.url_path_for("view_invitations")
     current_user = get_current_user(session=db)
-    
-    logger.error(f'current user, {current_user.id}')
-    logger.error(data['avg_period_length'])
 
     user_menstrual_period_length = {
         "user_id": current_user.id,
         "period_length": data['avg_period_length'],
     }
-    last_period_date = datetime.datetime.strptime(data['last_period_date'], '%Y-%m-%d')
+    last_period_date = datetime.datetime.strptime(
+        data['last_period_date'], '%Y-%m-%d')
     try:
-        new_signed_up_to_period = create_model(session=db, model_class=UserMenstrualPeriodLength, **user_menstrual_period_length)
+        new_signed_up_to_period = create_model(
+            session=db, model_class=UserMenstrualPeriodLength, **user_menstrual_period_length)
     except SQLAlchemyError as err:
-        logger.warning('User already signed up to the service, hurray')
+        logger.info(
+            f'User {new_signed_up_to_period.user_id} already signed up to the service, hurray')
         db.rollback()
     url = '/'
-    # if not is_date_before_today(last_period_date):
-    #     return RedirectResponse(url=url, status_code=HTTP_302_FOUND)
-    generate_predicted_period_dates(db, data['avg_period_length'], last_period_date, current_user.id)
-    period_days = get_all_period_days(db, user_id=current_user.id)
-    logger.error(period_days)
-    # logger.warning(data)
-    # remove_existing_period_dates(db, user_id=current_user.id)
-    period_days = get_all_period_days(db, user_id=current_user.id)
-    for day in period_days:
-        logger.error(day.start)
+    generate_predicted_period_dates(
+        db, data['avg_period_length'], last_period_date, current_user.id)
 
     return RedirectResponse(url=url, status_code=HTTP_302_FOUND)
 
 
-def remove_existing_period_dates(db, user_id):
-    period_days = (db.query(Event).
-        filter(Event.owner_id == user_id).
-        filter(Event.category_id == MENSTRUAL_PERIOD_CATEGORY_ID).
-        filter(Event.start > datetime.datetime.now()).
-        delete())
-    # db.delete(period_days)
+@router.get("/get-period-dates/")
+def get_period_days(request: Request, db: Session = Depends(get_db)):
+    period_days = get_all_period_days(db, 1)
+    gaps_list = []
+    for i in range(len(period_days) - 1):
+        gap = get_date_diff(period_days[i].start, period_days[i + 1].start)
+        gaps_list.append(gap.days)
+        # logger.debug(period_days[i].start)
+        # logger.debug(period_days[i+1].start)
+        logger.debug(gaps_list)
+    logger.critical(get_list_avg(gaps_list))
+
+
+def get_avg_period_gap(db: Session, user_id):
+    period_days = get_all_period_days(db, user_id)
+    gaps_list = []
+
+    for i in range(len(period_days) - 1):
+        gap = get_date_diff(period_days[i].start, period_days[i + 1].start)
+        gaps_list.append(gap.days)
+    return get_list_avg(gaps_list)
+
+
+def get_date_diff(date_1, date_2):
+    return date_2 - date_1
+
+
+def get_list_avg(received_list: List):
+    return sum(received_list) // len(received_list)
+
+
+def remove_existing_period_dates(db: Session, user_id: int):
+    (db.query(Event).
+     filter(Event.owner_id == user_id).
+     filter(Event.category_id == MENSTRUAL_PERIOD_CATEGORY_ID).
+     filter(Event.start > datetime.datetime.now()).
+     delete())
     db.commit()
+    logger.info('Removed all period predictions to create new ones')
 
 
-def generate_predicted_period_dates(db, period_length, last_period_time, user_id):
-    for i in range(int(period_length)):
-        delta = datetime.timedelta(i + 1)
-        period_date = last_period_time + delta
-        event = create_event(db, 'period day', period_date, period_date, user_id, category_id=MENSTRUAL_PERIOD_CATEGORY_ID)
-        # db.add(event)
-    # db.commit()
+# def add_period_start_date(db: Session, user_id: int, received_date: datetime):
+#     generate_predicted_period_dates(
+#         db, data['avg_period_length'], last_period_date, current_user.id)
+
+
+def generate_predicted_period_dates(db, period_length, period_start_date, user_id):
+    delta = datetime.timedelta(int(period_length))
+    period_end_date = period_start_date + delta
+    event = create_event(db, 'period', period_start_date, period_end_date,
+                         user_id, category_id=MENSTRUAL_PERIOD_CATEGORY_ID)
+    period_days = get_all_period_days(db, user_id=user_id)
+    # remove_existing_period_dates(db, user_id=current_user.id)
+    # period_days = sorted(get_all_period_days(db, user_id=user_id), key=lambda d: d.start)
+    # logger.error(period_days)
+    # for day in period_days:
+    #     logger.error(day.start)
+
+
+def add_3_month_predictions(db, period_length, period_start_date, user_id):
+    avg_gap = get_avg_period_gap(db, user_id)
+    avg_gap_delta = datetime.timedelta(avg_gap)
+    for i in range(4):
+        generate_predicted_period_dates(
+            db, period_length, period_start_date, user_id)
+        period_start_date += avg_gap_delta
+        logger.error(period_start_date)
+    logger.error(avg_gap_delta)
 
 
 def get_all_period_days(session: Session, user_id: int) -> List[Event]:
-    """Returns all period days filter by user id."""
+    """Returns all period days filtered by user id."""
 
     try:
-        period_days = list(session.query(Event).
-        filter(Event.owner_id == user_id).
-        filter(Event.category_id == MENSTRUAL_PERIOD_CATEGORY_ID).
-        all())
+        period_days = sorted((session.query(Event).
+                              filter(Event.owner_id == user_id).
+                              filter(Event.category_id == MENSTRUAL_PERIOD_CATEGORY_ID).
+                              all()), key=lambda d: d.start)
 
     except SQLAlchemyError as err:
         logger.debug(err)
@@ -114,5 +173,10 @@ def get_all_period_days(session: Session, user_id: int) -> List[Event]:
         return period_days
 
 
-def is_date_before_today(received_date: datetime):
-    return received_date < datetime.datetime.now()
+def is_user_signed_up_to_menstrual_predictor(session: Session, user_id: int):
+    user_menstrual_period_length = session.query(
+        UserMenstrualPeriodLength).filter(user_id == user_id).first()
+    if user_menstrual_period_length:
+        return user_menstrual_period_length.period_length
+    else:
+        return False
