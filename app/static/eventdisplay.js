7af514ffc867fba7a4b096a79625257133e46a7b
Adi Faibish <adidas2712@gmail.com>
2021-02-16 11:59:39 +0200

    fix more notes

--- app/static/eventdisplay.js
+++ app/static/eventdisplay.js
@@ -7,11 +7,11 @@ function scrollTo(element) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    let chosen_button = document.getElementsByClassName('event-btn');
+    const chosen_button = document.getElementsByClassName('event-btn');
     for (let i = 0; i < chosen_button.length; i++) {
         chosen_button[i].addEventListener("click", function(e) {
         let clickedElem = e.target.id
-            fetch('/event/' + clickedElem)
+            fetch('/event/edit')
             .then(function(response) {
                 return response.text();
             })
