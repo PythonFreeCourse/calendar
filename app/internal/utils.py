02c2755ed62a1d83869032dd846b81a753ab2c1d
Gonny <gonnym@gmail.com>
2021-02-18 16:20:57 +0200

    fix: post-merge fixes

--- app/internal/utils.py
+++ app/internal/utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 from sqlalchemy.orm import Session
 
@@ -18,7 +18,7 @@ def save(session: Session, instance: Base) -> bool:
 def create_model(
         session: Session,
         model_class: Base,
-        **kwargs: Any
+        **kwargs: Any,
 ) -> Base:
     """Creates and saves a db model."""
     instance = model_class(**kwargs)
