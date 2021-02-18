0dd83f82479b56c67537b3fc692f6325c3d83648
gala <gal.aviv@cellwize.com>
2021-02-19 01:21:41 +0200

    fix: mr comments

--- app/static/js/darkmode.js
+++ app/static/js/darkmode.js
@@ -1,22 +1,20 @@
-const root = document.documentElement;
-const button = document.querySelector('#darkmode');
-let isDarkMode = (localStorage.getItem('isDarkMode') == "true");
-setThemeMode(isDarkMode, button);
-
-button.addEventListener('click', event => {
+const root = document.documentElement
+window.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.querySelector('#darkmode');
+    console.log('loaded')
+    let isDarkMode = (localStorage.getItem('isDarkMode') == "true");
+    setThemeMode(isDarkMode, button);
+    button.addEventListener('click', event => {
     isDarkMode = !isDarkMode;
     localStorage.setItem('isDarkMode', isDarkMode);
     setThemeMode(isDarkMode, button);
+    });
 });
 
-
 function changeIcon(mode) {
-    setTimeout(function() {
-        const modeButton = document.querySelector('#darkmode');
-        modeButton.name = mode;
-        }, 100);
+    const modeButton = document.querySelector('#darkmode');
+    modeButton.name = mode;
 }
-
 
 function setThemeMode(isDarkMode, button) {
     if (isDarkMode) {
