1e2af3ecc07ca5e6473c4407af56687076694d51
gala <gal.aviv@cellwize.com>
2021-02-18 10:56:56 +0200

    fix: adding semicolons to js

--- app/static/js/darkmode.js
+++ app/static/js/darkmode.js
@@ -1,30 +1,30 @@
 const root = document.documentElement;
 const button = document.querySelector('#darkmode');
 let isDarkMode = (localStorage.getItem('isDarkMode') == "true");
-setThemeMode(isDarkMode, button)
+setThemeMode(isDarkMode, button);
 
 button.addEventListener('click', event => {
-    isDarkMode = !isDarkMode
+    isDarkMode = !isDarkMode;
     localStorage.setItem('isDarkMode', isDarkMode);
-    setThemeMode(isDarkMode, button)
+    setThemeMode(isDarkMode, button);
 });
 
 
 function changeIcon(mode) {
     setTimeout(function() {
         const modeButton = document.querySelector('#darkmode');
-        modeButton.name = mode
-        }, 100)
+        modeButton.name = mode;
+        }, 100);
 }
 
 
 function setThemeMode(isDarkMode, button) {
     if (isDarkMode) {
-        root.setAttribute("color-mode", "dark")
+        root.setAttribute("color-mode", "dark");
         button.name = 'moon';
         changeIcon('moon');
     } else {
-        root.setAttribute("color-mode", "regular")
+        root.setAttribute("color-mode", "regular");
         button.name = 'moon-outline';
         changeIcon('moon-outline');
     }
