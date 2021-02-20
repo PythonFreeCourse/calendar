eeeacca749a763fedd624cd2f99ceecae5f362dc
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2021-02-20 03:19:51 +0200

    fix: rearrange imports and black, mypy checkup

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+
 from pydantic import BaseModel, validator, EmailStr, EmailError
 from typing import Optional, Union
 
-EMPTY_FIELD_STRING = 'field is required'
+EMPTY_FIELD_STRING = "field is required"
 MIN_FIELD_LENGTH = 3
 MAX_FIELD_LENGTH = 20
 
@@ -19,6 +20,7 @@ class UserBase(BaseModel):
     Validating fields types
     Returns a User object without sensitive information
     """
+
     username: str
     email: str
     full_name: str
@@ -30,53 +32,60 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     """Validating fields types"""
+
     password: str
     confirm_password: str
     """
     Calling to field_not_empty validaion function,
     for each required field.
     """
-    _fields_not_empty_username = validator('username',
-                                           allow_reuse=True)(fields_not_empty)
-    _fields_not_empty_full_name = validator('full_name',
-                                            allow_reuse=True)(fields_not_empty)
-    _fields_not_empty_password = validator('password',
-                                           allow_reuse=True)(fields_not_empty)
+    _fields_not_empty_username = validator("username", allow_reuse=True)(
+        fields_not_empty
+    )
+    _fields_not_empty_full_name = validator("full_name", allow_reuse=True)(
+        fields_not_empty
+    )
+    _fields_not_empty_password = validator("password", allow_reuse=True)(
+        fields_not_empty
+    )
     _fields_not_empty_confirm_password = validator(
-        'confirm_password', allow_reuse=True)(fields_not_empty)
-    _fields_not_empty_email = validator('email',
-                                        allow_reuse=True)(fields_not_empty)
+        "confirm_password", allow_reuse=True
+    )(fields_not_empty)
+    _fields_not_empty_email = validator("email", allow_reuse=True)(
+        fields_not_empty
+    )
 
-    @validator('confirm_password')
-    def passwords_match(cls, confirm_password: str,
-                        values: UserBase) -> Union[ValueError, str]:
+    @validator("confirm_password")
+    def passwords_match(
+        cls, confirm_password: str, values: UserBase
+    ) -> str:
         """Validating passwords fields identical."""
-        if 'password' in values and confirm_password != values['password']:
-            raise ValueError("doesn't match to password")
+        if "password" in values and confirm_password != values["password"]:
+            raise ValueError("Doesn't match to password")
         return confirm_password
 
-    @validator('username')
+    @validator("username")
     def username_length(cls, username: str) -> Union[ValueError, str]:
         """Validating username length is legal"""
         if not (MIN_FIELD_LENGTH < len(username) < MAX_FIELD_LENGTH):
-            raise ValueError('must contain between 3 to 20 charactars')
+            raise ValueError("Must contain between 3 to 20 charactars")
         return username
 
-    @validator('password')
+    @validator("password")
     def password_length(cls, password: str) -> Union[ValueError, str]:
         """Validating username length is legal"""
         if not (MIN_FIELD_LENGTH < len(password) < MAX_FIELD_LENGTH):
-            raise ValueError('must contain between 3 to 20 charactars')
+            raise ValueError("Must contain between 3 to 20 charactars")
         return password
 
-    @validator('email')
+    @validator("email")
     def confirm_mail(cls, email: str) -> Union[ValueError, str]:
         """Validating email is valid mail address."""
         try:
             EmailStr.validate(email)
             return email
         except EmailError:
-            raise ValueError('address is not valid')
+            raise ValueError("Address is invalid")
 
 
 class User(UserBase):
@@ -84,6 +93,7 @@ class User(UserBase):
     Validating fields types
     Returns a User object without sensitive information
     """
+
     id: int
     is_active: bool
 
@@ -96,9 +106,9 @@ class NoteSchema(BaseModel):
     class Config:
         orm_mode = True
         schema_extra = {
-            'example': {
-                'title': 'Foo',
-                'description': 'Bar',
+            "example": {
+                "title": "Foo",
+                "description": "Bar",
             }
         }
 
