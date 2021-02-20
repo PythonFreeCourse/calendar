cc29fc1275b66b06c6f01f1469fc84987d35adef
fandomario <fandomario616@gmail.com>
2021-02-20 23:32:18 +0200

    fix: fix a bug after QA :)

--- app/internal/timer.py
+++ app/internal/timer.py
@@ -1,4 +1,5 @@
-from typing import Dict, Optional
+from datetime import datetime
+from typing import Dict, List, Optional
 
 from app.routers.event import sort_by_date
 from app.routers.user import get_all_user_events
@@ -7,9 +8,19 @@ from app.database.models import Event
 
 
 def get_next_user_event(session: Session, user_id: int) -> Optional[Event]:
-    events = list(sort_by_date(get_all_user_events(session, user_id)))
+    events = get_only_future_events(
+        sort_by_date(get_all_user_events(session, user_id))
+    )
     if events:
         return events[0]
+
+
+def get_only_future_events(events: List[Event]) -> List[Event]:
+    future_events = []
+    for event in events:
+        if event.start >= datetime.now():
+            future_events.append(event)
+    return future_events
 
 
 def get_next_user_event_start_time(
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -2,17 +2,17 @@ from app.internal.timer import get_next_user_event
 from app.internal.timer import get_next_user_event_start_time
 
 
-def test_get_last_event_success(event, session):
+def test_get_last_event_success(next_week_event, session):
     next_event = get_next_user_event(
         session=session,
-        user_id=event.owner_id,
+        user_id=next_week_event.owner_id,
     )
-    assert next_event == event
+    assert next_event == next_week_event
 
 
-def test_time_left(event, session):
+def test_time_left(next_week_event, session):
     time_left = get_next_user_event_start_time(
         session=session,
-        user_id=event.owner_id,
+        user_id=next_week_event.owner_id,
     )
     assert isinstance(time_left["timer"], str)
