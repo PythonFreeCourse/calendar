7b9627aaa1ae8a99161f3a7a3b508919419afd63
Elior Digmi <Eliory09@gmail.com>
2021-02-22 00:05:17 +0200

    fix: js reformatted

--- app/static/js/shared_list.js
+++ app/static/js/shared_list.js
@@ -1,36 +1,44 @@
 var items_num = 1;
 
-document.addEventListener('click', function(e) {
-    if (e.target && e.target.id.includes('btnRemoveItem')){
-        removeItem(e.target.id);
-    }
-    else if (e.target && e.target.id.includes("btnRemoveLabel")) {
-        let parent = document.getElementById(e.target.id).parentElement.id
-        removeItem(parent);
-    }
-    else if (e.target && e.target.id == "btnAddItem"){
-        addItem();
-    }
+document.addEventListener('click', function (e)
+{
+	if (e.target && e.target.id.includes('btnRemoveItem'))
+	{
+		removeItem(e.target.id);
+	}
+	else if (e.target && e.target.id.includes("btnRemoveLabel"))
+	{
+		let parent = document.getElementById(e.target.id).parentElement.id
+		removeItem(parent);
+	}
+	else if (e.target && e.target.id == "btnAddItem")
+	{
+		addItem();
+	}
 });
 
 
-function addItem() {
-    const remove_button = 7;
-    const remove_label = 1;
-    let item = document.getElementById("shared_list_item").cloneNode(true);
-    item.className = "shared_list_item_on"
-    item.childNodes[remove_button].id += String(items_num);
-    item.childNodes[remove_button].childNodes[remove_label].id += String(items_num);
-    items_num++;
-    for (i = 0; i < item.children.length; i++ ) {
-        if (item.children[i].tagName == 'INPUT') {
-            item.children[i].setAttribute('required', 'required');
-        }
-    }
-    document.getElementById("Items").appendChild(item);
+function addItem()
+{
+	const REMOVE_BUTTON = 7;
+	const REMOVE_LABEL = 1;
+	let item = document.getElementById("shared_list_item").cloneNode(true);
+	item.className = "shared_list_item_on"
+	item.childNodes[REMOVE_BUTTON].id += String(items_num);
+	item.childNodes[REMOVE_BUTTON].childNodes[REMOVE_LABEL].id += String(items_num);
+	items_num++;
+	for (child of item.children)
+	{
+		if (child.tagName == 'INPUT')
+		{
+			child.setAttribute('required', 'required');
+		}
+	}
+	document.getElementById("Items").appendChild(item);
 }
 
-function removeItem(item_id) {
-    document.getElementById(item_id).parentElement.remove();
-    items_num--;
+function removeItem(item_id)
+{
+	document.getElementById(item_id).parentElement.remove();
+	items_num--;
 }
