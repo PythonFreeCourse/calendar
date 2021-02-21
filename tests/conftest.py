16b01128b42c7493ac4d303cb0c3506797a70bab
Liad Noam <LiadN@Taldor.corp>
2021-02-21 23:41:14 +0200

    Added conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,20 +8,20 @@ from app.config import PSQL_ENVIRONMENT
 from app.database.models import Base
 
 pytest_plugins = [
-    'tests.user_fixture',
-    'tests.event_fixture',
-    'tests.dayview_fixture',
-    'tests.invitation_fixture',
-    'tests.association_fixture',
-    'tests.client_fixture',
-    'tests.asyncio_fixture',
-    'tests.logger_fixture',
-    'tests.category_fixture',
-    'smtpdfix',
-    'tests.quotes_fixture',
-    'tests.zodiac_fixture',
-    'tests.jokes_fixture',
-    'tests.comment_fixture',
+    "tests.user_fixture",
+    "tests.event_fixture",
+    "tests.dayview_fixture",
+    "tests.invitation_fixture",
+    "tests.association_fixture",
+    "tests.client_fixture",
+    "tests.asyncio_fixture",
+    "tests.logger_fixture",
+    "tests.category_fixture",
+    "smtpdfix",
+    "tests.quotes_fixture",
+    "tests.zodiac_fixture",
+    "tests.jokes_fixture",
+    "tests.comment_fixture",
 ]
 
 # When testing in a PostgreSQL environment please make sure that:
@@ -30,21 +30,22 @@ pytest_plugins = [
 
 if PSQL_ENVIRONMENT:
     SQLALCHEMY_TEST_DATABASE_URL = (
-        "postgresql://postgres:1234"
-        "@localhost/postgres"
+        "postgresql://postgres:1234" "@localhost/postgres"
     )
-    test_engine = create_engine(
-        SQLALCHEMY_TEST_DATABASE_URL
-    )
+    test_engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL)
 
 else:
     SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///./test.db"
     test_engine = create_engine(
-        SQLALCHEMY_TEST_DATABASE_URL, connect_args={"check_same_thread": False}
+        SQLALCHEMY_TEST_DATABASE_URL,
+        connect_args={"check_same_thread": False},
     )
 
 TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=test_engine)
+    autocommit=False,
+    autoflush=False,
+    bind=test_engine,
+)
 
 
 def get_test_db():
@@ -65,11 +66,15 @@ def session():
 def sqlite_engine():
     SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///./test.db"
     sqlite_test_engine = create_engine(
-        SQLALCHEMY_TEST_DATABASE_URL, connect_args={"check_same_thread": False}
+        SQLALCHEMY_TEST_DATABASE_URL,
+        connect_args={"check_same_thread": False},
     )
 
     TestingSession = sessionmaker(
-        autocommit=False, autoflush=False, bind=sqlite_test_engine)
+        autocommit=False,
+        autoflush=False,
+        bind=sqlite_test_engine,
+    )
 
     yield sqlite_test_engine
     session = TestingSession()
@@ -80,13 +85,3 @@ def sqlite_engine():
 @pytest.fixture
 def Calendar():
     return calendar.Calendar(0)
-
-
-@pytest.fixture(scope="session")
-def module_session():
-    Base.metadata.create_all(bind=test_engine)
-    session = get_test_db()
-    yield session
-    session.rollback()
-    session.close()
-    Base.metadata.drop_all(bind=test_engine)
