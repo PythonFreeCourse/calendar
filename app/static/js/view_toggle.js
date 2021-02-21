0873e47035a48122e6bbe8778bbd2e4ab5ff70b0
Sagi Zaid Or <sagi@eretz.org.il>
2021-02-21 20:47:45 +0200

    changes

--- app/static/js/view_toggle.js
+++ app/static/js/view_toggle.js
@@ -1,15 +1,27 @@
-/*function getPath(view) {
-    if view == "week"
-    '/' + view + '/2021-1-3'
-    return path;
-}*/
+function refreshview() {
+  location.reload();
+}
 
-function changeview(view) {
-    const path = '/' + view + '/2021-1-3';
-    fetch(path).then(function (response) {
-        return response.text();
-      }).then(function (html) {
-        view = document.getElementById("calendarview");
-        view.innerHTML = html
-      });
+function set_views_styles(view, element_id) {
+  if (element_id == "calender-view") {
+    dayviewdiv = document.getElementById("day-view");
+    dayviewdiv.style.display = 'none';
+  }
+  if (element_id == "day-view"){
+    view.classList.add("day-view-class");
+    view.style.overflow = "scroll";
+  }
+}
+
+
+function change_view(view, day, element_id) {
+  const path = '/' + view + '/' + day;
+  fetch(path).then(function (response) {
+      return response.text();
+    }).then(function (html) {
+      view = document.getElementById(element_id);
+      view.innerHTML = html;
+      set_views_styles(view, element_id);
+    }
+  );
 }
