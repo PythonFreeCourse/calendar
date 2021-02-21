9932116e1a52ce17f9e52d50f2b063810a359755
i <inbvar@gmail.com>
2021-02-21 18:47:04 +0200

    PR comments

--- app/static/js/categories_filter.js
+++ app/static/js/categories_filter.js
@@ -8,25 +8,18 @@ function filterByCategory(){
     // TODO(issue#67): Allow filter by category name
     const category = document.getElementById("category").value;
 
-    const css = (!Number.isInteger(+category) || !category || 0 === category.length) ? `
-    .event_line {
-      display: grid;
-      visibility: visible;
-    }
-    ` : `
-    .event_line {
-      display: none;
-      visibility: hidden;
-    }
-    .event_line[data-name*="${category}" i] {
-      display: grid;
-      visibility: visible;
-    }
-    `;
-    window.cssFilter.innerHTML = css;
-
     const allEvents = document.getElementsByClassName("event_line");
     for (event of allEvents) {
+      if (event.dataset.name == category)
+      {
+        event.style.display = "grid";
+      }
+      else {
+        event.style.display = "none";
+      }
+      if (!Number.isInteger(+category) || !category || 0 === category.length) {
+        event.style.display = "grid";
+      }
       event.parentNode.style.display = "none" ;
     }
 
