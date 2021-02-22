4e8d5bf8b35c4236dc537bdfe96a461c28a57e6b
itzik <itzik245@gmail.com>
2021-02-22 10:09:32 +0200

    fix: saves only the username to the global variables

--- app/internal/global_variable.py
+++ app/internal/global_variable.py
@@ -4,14 +4,12 @@ from app.dependencies import templates
 from app.internal.security.ouath2 import (
     Session, get_jwt_token, get_authorization_cookie
 )
-from app.internal.security.schema import CurrentUser
 
 
-async def get_user_for_global_var(db: Session, jwt: str) -> CurrentUser:
+async def get_user_for_global_var(db: Session, jwt: str) -> str:
     jwt_payload = await get_jwt_token(db, jwt)
     username = jwt_payload.get("sub")
-    user_id = jwt_payload.get("user_id")
-    return CurrentUser(user_id=user_id, username=username)
+    return username
 
 
 async def set_global_user_var(request: Request, db: Session, temp: templates):
