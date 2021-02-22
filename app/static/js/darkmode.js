714d1be2c80f0e219e614abb2a30def787167b8d
gala <gal.aviv@cellwize.com>
2021-02-22 11:21:50 +0200

    fix: fixed mr comments

--- app/static/js/darkmode.js
+++ app/static/js/darkmode.js
@@ -1,29 +1,28 @@
-const root = document.documentElement
-window.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.querySelector('#darkmode');
-    console.log('loaded')
-    let isDarkMode = (localStorage.getItem('isDarkMode') == "true");
-    setThemeMode(isDarkMode, button);
-    button.addEventListener('click', event => {
+const ROOT = document.documentElement;
+window.addEventListener("DOMContentLoaded", (event) => {
+  const button = document.querySelector("#darkmode");
+  let isDarkMode = localStorage.getItem("isDarkMode") == "true";
+  setThemeMode(isDarkMode, button, ROOT);
+  button.addEventListener("click", (event) => {
     isDarkMode = !isDarkMode;
-    localStorage.setItem('isDarkMode', isDarkMode);
-    setThemeMode(isDarkMode, button);
-    });
+    localStorage.setItem("isDarkMode", isDarkMode);
+    setThemeMode(isDarkMode, button, ROOT);
+  });
 });
 
 function changeIcon(mode) {
-    const modeButton = document.querySelector('#darkmode');
-    modeButton.name = mode;
+  const modeButton = document.querySelector("#darkmode");
+  modeButton.name = mode;
 }
 
-function setThemeMode(isDarkMode, button) {
-    if (isDarkMode) {
-        root.setAttribute("color-mode", "dark");
-        button.name = 'moon';
-        changeIcon('moon');
-    } else {
-        root.setAttribute("color-mode", "regular");
-        button.name = 'moon-outline';
-        changeIcon('moon-outline');
-    }
+function setThemeMode(isDarkMode, button, root) {
+  if (isDarkMode) {
+    root.setAttribute("color-mode", "dark");
+    button.name = "moon";
+    changeIcon("moon");
+  } else {
+    root.setAttribute("color-mode", "regular");
+    button.name = "moon-outline";
+    changeIcon("moon-outline");
+  }
 }
