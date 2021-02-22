9e0d0ca426f6d338577b9d32c68aaaa6f0f1a2e8
Liad Noam <LiadN@Taldor.corp>
2021-02-22 02:46:29 +0200

    Fixed some issues with dependency injection

--- app/internal/menstrual_predictor_utils.py
+++ app/internal/menstrual_predictor_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from loguru import logger
 
--- app/routers/menstrual_predictor.py
+++ app/routers/menstrual_predictor.py
@@ -1,9 +1,9 @@
 import datetime
 
-# from typing import List
 
 from fastapi import APIRouter, Depends, Request, HTTPException
 from fastapi.responses import RedirectResponse
+from loguru import logger
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_302_FOUND, HTTP_400_BAD_REQUEST
@@ -16,9 +16,9 @@ from app.internal.menstrual_predictor_utils import (
     generate_predicted_period_dates,
     remove_existing_period_dates,
 )
-from app.internal.utils import create_model, get_current_user
-
-from loguru import logger
+from app.internal.security.schema import CurrentUser
+from app.internal.security.dependancies import current_user
+from app.internal.utils import create_model
 
 
 router = APIRouter(
@@ -31,8 +31,12 @@ MENSTRUAL_PERIOD_CATEGORY_ID = 111
 
 
 @router.get("/")
-def join_menstrual_predictor(request: Request, db: Session = Depends(get_db)):
-    current_user_id = get_current_user(db).id
+def join_menstrual_predictor(
+    request: Request,
+    db: Session = Depends(get_db),
+    user: CurrentUser = Depends(current_user),
+):
+    current_user_id = user.user_id
 
     if not is_user_signed_up_to_menstrual_predictor(db, current_user_id):
         return templates.TemplateResponse(
@@ -50,6 +54,7 @@ def add_period_start(
     request: Request,
     start_date: str,
     db: Session = Depends(get_db),
+    user: CurrentUser = Depends(current_user),
 ):
     try:
         period_start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
@@ -60,7 +65,7 @@ def add_period_start(
             detail="The given date doesn't match a date format YYYY-MM-DD",
         )
     else:
-        current_user_id = get_current_user(db).id
+        current_user_id = user.user_id
         user_period_length = is_user_signed_up_to_menstrual_predictor(
             db,
             current_user_id,
@@ -79,13 +84,16 @@ def add_period_start(
 
 
 @router.post("/")
-async def submit_join_form(request: Request, db: Session = Depends(get_db)):
+async def submit_join_form(
+    request: Request,
+    db: Session = Depends(get_db),
+    user: CurrentUser = Depends(current_user),
+):
 
     data = await request.form()
-    current_user = get_current_user(session=db)
 
     user_menstrual_period_length = {
-        "user_id": current_user.id,
+        "user_id": user.user_id,
         "period_length": data["avg_period_length"],
     }
     last_period_date = datetime.datetime.strptime(
@@ -106,7 +114,7 @@ async def submit_join_form(request: Request, db: Session = Depends(get_db)):
         db,
         data["avg_period_length"],
         last_period_date,
-        current_user.id,
+        user.user_id,
     )
 
     return RedirectResponse(url=url, status_code=HTTP_302_FOUND)
