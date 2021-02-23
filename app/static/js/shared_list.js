9e9e930dde3d1f6cb9e3b1bc6e177fb3331bc876
Elior Digmi <Eliory09@gmail.com>
2021-02-24 01:06:50 +0200

    fix: reformat js

--- app/static/js/shared_list.js
+++ app/static/js/shared_list.js
@@ -1,31 +1,30 @@
-window.addEventListener('load', () =>
-{
-	document.getElementById("btn-add-item").addEventListener('click', addItem)
+window.addEventListener('load', () => {
+    document.getElementById("btn-add-item").addEventListener('click', addItem)
 });
 
 
-function addItem()
-{
-	const SHARED_LIST_ITEMS_NUM = document.querySelectorAll("#Items > div").length
-	const shared_list_items = document.getElementById("Items")
-	let shared_list_item = document.getElementById("shared-list-item").cloneNode(true);
+function addItem() {
+    const LIST_ITEMS_NUM = document.querySelectorAll("#Items > div").length
+    const list_items = document.getElementById("Items")
+    let shared_list_item = document.getElementById("shared-list-item").cloneNode(true);
 
-	shared_list_item.className = "shared-list-item-on"
-	shared_list_item.id = shared_list_item.id + SHARED_LIST_ITEMS_NUM
-	for (child of shared_list_item.children)
-	{
-		if (child.tagName == 'INPUT')
-		{
-			child.setAttribute('required', 'required');
-		}
-	}
-	shared_list_items.appendChild(shared_list_item);
-	document.querySelector("#" + shared_list_item.id + " > .remove-btn").addEventListener('click', () =>
-	{
-		shared_list_item.remove()
-		for (i = 3; i < shared_list_items.childNodes.length; i++)
-		{
-			shared_list_items.childNodes[i].id = "shared-list-item" + String(i - 2);
-		}
-	})
+    shared_list_item.className = "shared-list-item-on"
+    shared_list_item.id = shared_list_item.id + LIST_ITEMS_NUM
+    for (child of shared_list_item.children) {
+        if (child.tagName == 'INPUT') {
+            child.setAttribute('required', 'required');
+        }
+    }
+    list_items.appendChild(shared_list_item);
+    document.querySelector("#" + shared_list_item.id + " > .remove-btn").addEventListener('click', () => {
+        removeItem(shared_list_item, list_items)
+    })
+}
+
+
+function removeItem(shared_list_item, list_items) {
+    shared_list_item.remove()
+    for (i = 3; i < list_items.childNodes.length; i++) {
+        list_items.childNodes[i].id = "shared-list-item" + String(i - 2);
+    }
 }
