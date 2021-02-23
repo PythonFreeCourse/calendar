4696df275eb3f163be9137e50027500c8a6b0fcc
Liran C <liran.c.caduri@gmail.com>
2021-02-23 20:40:11 +0200

    pre-commit change some stuff

--- app/routers/features.py
+++ app/routers/features.py
@@ -1,9 +1,9 @@
-from app.internal.utils import get_current_user
 from fastapi import APIRouter, Request, Depends
 from typing import List
 
 from app.dependencies import get_db, SessionLocal
-from app.database.models import UserFeature, Feature
+from app.database.models import User, UserFeature, Feature
+from app.internal.security.dependancies import current_user_from_db
 from app.internal.features import (
     create_user_feature_association,
     is_association_exists_in_db,
@@ -16,24 +16,30 @@ router = APIRouter(
 )
 
 
-@router.get('/')
+@router.get("/")
 async def index(
-    request: Request, session: SessionLocal = Depends(get_db)
-) -> List:
+    request: Request,
+    session: SessionLocal = Depends(get_db),
+) -> List[Feature]:
     features = session.query(Feature).all()
     return features
 
 
-@router.post('/add')
+@router.post("/add")
 async def add_feature_to_user(
-    request: Request, session: SessionLocal = Depends(get_db)
-) -> UserFeature:
+    request: Request,
+    session: SessionLocal = Depends(get_db),
+    user: User = Depends(current_user_from_db),
+) -> UserFeature or bool:
     form = await request.form()
 
-    user = get_current_user(session=session)
-    feat = session.query(Feature).filter_by(id=form['feature_id']).first()
+    feat = session.query(Feature).filter_by(id=form["feature_id"]).first()
 
-    is_exist = is_association_exists_in_db(form=form, session=session)
+    is_exist = is_association_exists_in_db(
+        form=form,
+        session=session,
+        user=user,
+    )
 
     if feat is None or is_exist:
         # in case there is no feature in the database with that same id
@@ -44,30 +50,33 @@ async def add_feature_to_user(
         db=session,
         feature_id=feat.id,
         user_id=user.id,
-        is_enable=True
+        is_enable=True,
     )
 
     return session.query(UserFeature).filter_by(id=association.id).first()
 
 
-@router.post('/delete')
+@router.post("/delete")
 async def delete_user_feature_association(
     request: Request,
-    session: SessionLocal = Depends(get_db)
+    session: SessionLocal = Depends(get_db),
+    user: User = Depends(current_user_from_db),
 ) -> bool:
     form = await request.form()
+    feature_id = form["feature_id"]
 
-    user = get_current_user(session=session)
-    feature_id = form['feature_id']
-
-    is_exist = is_association_exists_in_db(form=form, session=session)
+    is_exist = is_association_exists_in_db(
+        form=form,
+        session=session,
+        user=user,
+    )
 
     if not is_exist:
         return False
 
     session.query(UserFeature).filter_by(
         feature_id=feature_id,
-        user_id=user.id
+        user_id=user.id,
     ).delete()
     session.commit()
 
--- tests/test_feature_panel.py
+++ tests/test_feature_panel.py
@@ -1,5 +1,6 @@
-from app.database.models import Feature, UserFeature
 import pytest
+
+from app.database.models import Feature, UserFeature
 import app.internal.features as internal
 import app.routers.features as route
 
@@ -8,22 +9,22 @@ import app.routers.features as route
 def mock_features():
     return [
         {
-            "name": 'test',
-            "route": '/test',
-            "description": 'testing',
-            "creator": 'test'
-        }
+            "name": "test",
+            "route": "/test",
+            "description": "testing",
+            "creator": "test",
+        },
     ]
 
 
 @pytest.fixture
-@pytest.mark.usefixtures('session')
+@pytest.mark.usefixtures("session")
 def feature(session):
     test = Feature(
-        name='test',
-        route='/test',
-        description='testing',
-        creator='test',
+        name="test",
+        route="/test",
+        description="testing",
+        creator="test",
     )
 
     session.add(test)
@@ -36,10 +37,9 @@ def feature(session):
 
 
 @pytest.fixture
-@pytest.mark.usefixtures('session')
+@pytest.mark.usefixtures("session")
 def association_off(session, user):
-    test = UserFeature(
-        feature_id=1, user_id=user.id, is_enable=False)
+    test = UserFeature(feature_id=1, user_id=user.id, is_enable=False)
 
     session.add(test)
     session.commit()
@@ -51,10 +51,9 @@ def association_off(session, user):
 
 
 @pytest.fixture
-@pytest.mark.usefixtures('session')
+@pytest.mark.usefixtures("session")
 def association_on(session, user):
-    test = UserFeature(
-        feature_id=1, user_id=user.id, is_enable=True)
+    test = UserFeature(feature_id=1, user_id=user.id, is_enable=True)
 
     session.add(test)
     session.commit()
@@ -68,10 +67,10 @@ def association_on(session, user):
 @pytest.fixture
 def update_dict():
     update = {
-        'name': 'test',
-        'route': '/route-test',
-        'description': 'update',
-        'creator': 'test'
+        "name": "test",
+        "route": "/route-test",
+        "description": "update",
+        "creator": "test",
     }
 
     return update
@@ -79,40 +78,40 @@ def update_dict():
 
 @pytest.fixture
 def form_mock():
-    form = {
-        'feature_id': 1,
-        'user_id': 1
-    }
+    form = {"feature_id": 1, "user_id": 1}
 
     return form
 
 
 def test_create_features_at_startup(mocker, session, mock_features):
 
-    mocker.patch(
-        'app.internal.features.features',
-        mock_features
-    )
-    mocker.patch(
-        'app.internal.features.is_feature_exists',
-        return_value=False
-    )
+    mocker.patch("app.internal.features.features", mock_features)
+    mocker.patch("app.internal.features.is_feature_exists", return_value=False)
 
     assert internal.create_features_at_startup(session)
 
 
 def test_create_association(mocker, session, user, feature):
-    assert internal.create_user_feature_association(
-        db=session, feature_id=1, user_id=user.id, is_enable=False
-    ) is not None
+    assert (
+        internal.create_user_feature_association(
+            db=session,
+            feature_id=1,
+            user_id=user.id,
+            is_enable=False,
+        )
+        is not None
+    )
 
 
-def test_get_user_enabled_features(session, feature, association_on):
-    assert internal.get_user_enabled_features(session)[0] is not None
+def test_get_user_enabled_features(session, feature, association_on, user):
+    assert (
+        internal.get_user_enabled_features(session=session, user_id=user.id)[0]
+        is not None
+    )
 
 
-def test_is_association_exist_in_db(session, form_mock, association_off):
-    assert internal.is_association_exists_in_db(form_mock, session)
+def test_is_association_exist_in_db(session, form_mock, association_off, user):
+    assert internal.is_association_exists_in_db(form_mock, session, user)
 
 
 def test_delete_feature(session, feature):
@@ -128,50 +127,75 @@ def test_is_feature_exist_in_db(session, feature, update_dict):
 
 def test_update_feature(session, feature, update_dict):
     feature = internal.update_feature(feature, update_dict, session)
-    assert feature.description == 'update'
+    assert feature.description == "update"
 
 
-def test_is_feature_exist_in_enabled(session, feature, association_on):
+def test_is_feature_exist_in_enabled(
+    mocker,
+    session,
+    feature,
+    association_on,
+    user,
+):
     feat = session.query(Feature).filter_by(name=feature.name).first()
-    assert internal.is_feature_enabled(feat, session)
 
-
-def test_is_feature_enabled(mocker, session, association_on):
     mocker.patch(
-        'app.internal.features.SessionLocal',
-        return_value=session
+        "app.internal.features.get_user_enabled_features",
+        return_value=[feat],
     )
-    assert internal.is_access_allowd(route='/route') is True
+
+    assert internal.is_feature_enabled(user, feat, session)
+
+
+@pytest.mark.asyncio
+async def test_is_feature_enabled(mocker, session, association_on, user):
+    mocker.patch("app.internal.features.SessionLocal", return_value=session)
+    mocker.patch(
+        "app.internal.features.get_authorization_cookie",
+        return_value=None,
+    )
+    mocker.patch(
+        "app.internal.features.current_user_from_db",
+        return_value=user,
+    )
+
+    assert (
+        await internal.is_access_allowd(route="/route", request=None) is True
+    )
 
 
 def test_create_feature(session):
-
     feat = internal.create_feature(
-        name='test1', route='/route', description='testing', creator='test'
+        name="test1",
+        route="/route",
+        description="testing",
+        creator="test",
     )
-
-    assert feat.name == 'test1'
+    assert feat.name == "test1"
 
 
 def test_index(mocker, features_test_client, mock_features):
-    url = route.router.url_path_for('index')
+    url = route.router.url_path_for("index")
 
     resp = features_test_client.get(url)
     assert resp.ok
 
 
 def test_add_feature_to_user(features_test_client, feature, form_mock):
-    url = route.router.url_path_for('add_feature_to_user')
+    url = route.router.url_path_for("add_feature_to_user")
 
     resp = features_test_client.post(url, data=form_mock)
     assert resp.ok
 
 
 def test_delete_user_feature_association(
-    features_test_client, form_mock, association_on, feature
+    mocker,
+    features_test_client,
+    form_mock,
+    association_on,
+    feature,
 ):
-    url = route.router.url_path_for('delete_user_feature_association')
+    url = route.router.url_path_for("delete_user_feature_association")
 
     resp = features_test_client.post(url, data=form_mock)
     assert resp.ok
-    assert resp.content == b'true'
