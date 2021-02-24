e899e87ac5dc11d8e82cc4aa0a288406fdd987c2
Nellie Izhaki <nel_ya@hotmail.com>
2021-02-25 01:18:42 +0200

    fix: add precommit hooks and small cr changes

--- app/internal/showevent.py
+++ app/internal/showevent.py
@@ -8,9 +8,10 @@ from app.database.models import Event, UserEvent
 
 def get_upcoming_events(session: Session, user_id: int) -> List[Event]:
     upcoming_events = (
-        session.query(Event).join(UserEvent)
+        session.query(Event)
+        .join(UserEvent)
         .filter(UserEvent.user_id == user_id)
         .filter(Event.start >= datetime.now())
         .order_by(Event.start)
-        )
+    )
     return upcoming_events
