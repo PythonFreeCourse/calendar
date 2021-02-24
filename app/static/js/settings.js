74798574a60d1ccc3c9fac2f75935ef12f789692
Elior Digmi <Eliory09@gmail.com>
2021-02-24 22:39:54 +0200

    fix: typo

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -3,22 +3,22 @@ const tabBtn =
 
 
 function tabClick(tab_id) {
-    let shown_tab = document.querySelector(".tab-show")
-    let selected_tab_content = document.querySelector("#" + tab_id + "-content")
-    shown_tab.classList.remove("tab-show")
-    shown_tab.classList.add("tab-hide")
+    let shown_tab = document.querySelector(".tab-show");
+    let selected_tab_content = document.querySelector("#" + tab_id + "-content");
+    shown_tab.classList.remove("tab-show");
+    shown_tab.classList.add("tab-hide");
     for (btn of tabBtn) {
-        btn.children[0].classList.remove("active")
+        btn.children[0].classList.remove("active");
     }
-    document.getElementById(tab_id).classList.add("active")
-    selected_tab_content.classList.remove("tab-hide")
-    selected_tab_content.classList.add("tab-show")
+    document.getElementById(tab_id).classList.add("active");
+    selected_tab_content.classList.remove("tab-hide");
+    selected_tab_content.classList.add("tab-show");
 };
 
 
 for (i = 0; i < tabBtn.length; i++) {
-    let btn = document.getElementById("tab" + i)
+    let btn = document.getElementById("tab" + i);
     btn.addEventListener('click', () => {
-        tabClick(btn.id)
+        tabClick(btn.id);
     })
 }
