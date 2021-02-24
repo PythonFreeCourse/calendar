a7613fb49264a88cd9cfcce832bde9682ce7354c
Ori <ori81510@gmail.com>
2021-02-24 07:59:24 +0200

    fix: fixed flake8 error

--- tests/meds/test_routers.py
+++ tests/meds/test_routers.py
@@ -28,4 +28,3 @@ def test_meds_send_form_success(meds_test_client: TestClient, session: Session,
     path = meds.router.url_path_for('medications')
     response = meds_test_client.post(path, data=form, allow_redirects=True)
     assert response.ok
-    
