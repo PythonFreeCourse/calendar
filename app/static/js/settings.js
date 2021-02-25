3aed242c38884274a3ab7e08e3927586b1381bee
Elior Digmi <Eliory09@gmail.com>
2021-02-25 15:43:23 +0200

    fix: typo, js fix

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -1,24 +1,23 @@
-const tabBtn =
-    document.querySelectorAll(".tab");
+document.addEventListener('DOMContentLoaded', () => {
+    const tabBtn = document.querySelectorAll(".tab");
+    for (let i = 0; i < tabBtn.length; i++) {
+        const btn = document.getElementById("tab" + i);
+        btn.addEventListener('click', () => {
+            tabClick(btn.id, tabBtn);
+        });
+    }
+});
 
 
-function tabClick(tab_id) {
-    let shown_tab = document.querySelector(".tab-show");
-    let selected_tab_content = document.querySelector("#" + tab_id + "-content");
-    shown_tab.classList.remove("tab-show");
-    shown_tab.classList.add("tab-hide");
+function tabClick(tab_id, tabBtn) {
+    let shownTab = document.querySelector(".tab-show");
+    let selectedTabContent = document.querySelector(`#${tab_id}-content`);
+    shownTab.classList.remove("tab-show");
+    shownTab.classList.add("tab-hide");
     for (btn of tabBtn) {
         btn.children[0].classList.remove("active");
     }
     document.getElementById(tab_id).classList.add("active");
-    selected_tab_content.classList.remove("tab-hide");
-    selected_tab_content.classList.add("tab-show");
-};
-
-
-for (i = 0; i < tabBtn.length; i++) {
-    let btn = document.getElementById("tab" + i);
-    btn.addEventListener('click', () => {
-        tabClick(btn.id);
-    })
+    selectedTabContent.classList.remove("tab-hide");
+    selectedTabContent.classList.add("tab-show");
 }
