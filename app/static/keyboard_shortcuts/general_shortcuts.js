b0a5938d0f75392730a70f87b9f4333fa07814c6
Yair Engel <yair1engel@gmail.com>
2021-02-25 20:57:15 +0200

    Change keyboard_shortcuts to modal

--- app/static/keyboard_shortcuts/general_shortcuts.js
+++ app/static/keyboard_shortcuts/general_shortcuts.js
@@ -6,8 +6,7 @@ let key_shortcuts = {
     'alt+c+i': function() { window.open('/invitations/', '_self'); },
     'alt+c+p': function() { window.open('/profile/', '_self'); },
     'alt+c+s': function() { window.open('/search/', '_self'); },
-    'alt+c+a': function() { window.open('/agenda/', '_self'); },
-    'ctrl+.': function() { window.open('/keyboard_shortcuts/', '_self'); }
+    'alt+c+a': function() { window.open('/agenda/', '_self'); }
 };
 
 Mousetrap.bind(key_shortcuts);
