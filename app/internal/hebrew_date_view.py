3b1a6be29413c437766e48d1ac06ac9e88b5bf62
Elor Shoshan <elor550@gmail.com>
2021-02-25 20:51:35 +0200

    Details Message

--- app/internal/hebrew_date_view.py
+++ app/internal/hebrew_date_view.py
@@ -1,67 +1,76 @@
-from datetime import datetime
+from datetime import date
 
 from hebrew_numbers import int_to_gematria
 from pyluach import dates, hebrewcal
+from pyluach.dates import HebrewDate
 
 
-EN_TO_HEB_MONTH_NAMES = {
-    'Tishrei': 'תשרי',
-    'Cheshvan': 'חשוון',
-    'Kislev': 'כסלו',
-    'Teves': 'טבת',
-    'Shvat': 'שבט',
-    'Adar': 'אדר',
-    'Adar Rishon': "אדר(א')",
-    'Adar Sheni': "אדר(ב')",
-    'Nissan': 'ניסן',
-    'Iyar': 'אייר',
-    'Sivan': 'סיוון',
-    'Tamuz': 'תמוז',
-    'Av': 'אב',
-    'Elul': 'אלול',
+HEBREW_LANGUAGE_ID = 2
+HEB_MONTH_NAMES = {
+    'Tishrei': _("Tishrei"),
+    'Cheshvan': _("Cheshvan"),
+    'Kislev': _("Kislev"),
+    'Teves': _("Teves"),
+    'Shvat': _("Shvat"),
+    'Adar': _("Adar"),
+    'Adar Rishon': _("Adar Rishon"),
+    'Adar Sheni': _("Adar Sheni"),
+    'Nissan': _("Nissan"),
+    'Iyar': _("Iyar"),
+    'Sivan': _("Sivan"),
+    'Tamuz': _("Tamuz"),
+    'Av': _("Av"),
+    'Elul': _("Elul"),
 }
 
 
-def get_month_name_by_num(date: datetime) -> str:
+def get_hebrew_date_in_words(calendar_date: date, language_id: int) -> str:
+    """Returns the Hebrew date for the specific day.
+
+    Args:
+        calendar_date: The requested date.
+        language_id: The user's language.
+
+    Returns:
+        A Hebrew date string.
+    """
+
+    hebrew_date_object = from_gregorian_to_hebrew_date(calendar_date)
+    day = hebrew_date_object.day
+    month = get_month_name_by_num(hebrew_date_object)
+    year = hebrew_date_object.year
+    if language_id == HEBREW_LANGUAGE_ID:
+        day = int_to_gematria(day)
+        year = int_to_gematria(year % 1000)
+    return ' '.join((str(day), str(month), str(year)))
+
+
+def get_month_name_by_num(calendar_date: HebrewDate) -> str:
     """Returns the Hebrew name date for the specific day.
 
         Args:
-            date: The requested date.
+            calendar_date: The requested date.
 
         Returns:
             A Hebrew name date.
     """
-    for month in hebrewcal.Year(date.year).itermonths():
-        if date.month == month.month:
-            return EN_TO_HEB_MONTH_NAMES[month.name]
+    for month in hebrewcal.Year(calendar_date.year).itermonths():
+        if calendar_date.month == month.month:
+            return HEB_MONTH_NAMES[month.name]
 
 
-def from_greogian_to_hebrew_date(date: datetime) -> str:
-    """Returns the Hebrew date for the specific greogian date.
+def from_gregorian_to_hebrew_date(calendar_date: date) -> HebrewDate:
+    """Returns the Hebrew date for the specific gregorian date.
 
     Args:
-        date: The requested date.
+        calendar_date: The requested date.
 
     Returns:
-        A Hebrew date string.
+        A HebrewDate object.
     """
-    gregorian_date = dates.GregorianDate(date.year, date.month, date.day)
+    gregorian_date = dates.GregorianDate(
+        calendar_date.year,
+        calendar_date.month,
+        calendar_date.day,
+    )
     return gregorian_date.to_heb()
-
-
-def get_hebrew_date_in_words(date: datetime) -> str:
-    """Returns the Hebrew date for the specific day.
-
-    Args:
-        date: The requested date.
-
-    Returns:
-        A Hebrew date string.
-    """
-
-    hebrew_date_object = from_greogian_to_hebrew_date(date)
-    return ' '.join((
-        int_to_gematria(hebrew_date_object.day),
-        get_month_name_by_num(hebrew_date_object),
-        int_to_gematria(hebrew_date_object.year % 1000),
-    ))
--- tests/test_hebrew_date.py
+++ tests/test_hebrew_date.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+
+import pytest
 from pyluach import dates
 
 from app.internal.hebrew_date_view import (
     get_hebrew_date_in_words,
-    from_greogian_to_hebrew_date,
+    from_gregorian_to_hebrew_date,
     get_month_name_by_num,
 )
 
@@ -13,20 +15,26 @@ ADAR = datetime.strptime("2021-02-15", "%Y-%m-%d").date()
 ADAR_A = datetime.strptime("2019-02-15", "%Y-%m-%d").date()
 ADAR_B = datetime.strptime("2019-03-08", "%Y-%m-%d").date()
 
+TRANSLATION_DATES = [
+    (1, "17 Teves 5781"),
+    (2, "י״ז טבת תשפ״א"),
+]
 
-def test_get_hebrew_date():
-    result = get_hebrew_date_in_words(DAY)
-    assert result == "י״ז טבת תשפ״א"
+
+@pytest.mark.parametrize("language_id, hebrew_date", TRANSLATION_DATES)
+def test_get_hebrew_date(language_id, hebrew_date):
+    result = get_hebrew_date_in_words(DAY, language_id)
+    assert result == hebrew_date
 
 
-def test_from_greogian_to_hebrew_date_and_find_month_name():
-    result = from_greogian_to_hebrew_date(ADAR)
+def test_from_gregorian_to_hebrew_date_and_find_month_name():
+    result = from_gregorian_to_hebrew_date(ADAR)
     assert result == dates.HebrewDate(5781, 12, 3)
     assert get_month_name_by_num(result) == 'אדר'
 
 
 def test_if_leap_year():
-    result_a = from_greogian_to_hebrew_date(ADAR_A)
-    result_b = from_greogian_to_hebrew_date(ADAR_B)
+    result_a = from_gregorian_to_hebrew_date(ADAR_A)
+    result_b = from_gregorian_to_hebrew_date(ADAR_B)
     assert get_month_name_by_num(result_a) == "אדר(א')"
     assert get_month_name_by_num(result_b) == "אדר(ב')"
