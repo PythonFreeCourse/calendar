bdafe3f803c2961c8b3b0b76759405b9d8d4105c
Sagi Zaid Or <sagi@eretz.org.il>
2021-02-26 18:07:07 +0200

    added js to close tab view

--- app/static/js/view_toggle.js
+++ app/static/js/view_toggle.js
@@ -9,52 +9,57 @@ function close_the_view() {
 }
 
 function set_views_styles(view_element, element_id) {
-  if (element_id == "day-view"){
-    view_element.classList.add("day-view-class", "day-view-limitions");
+  if (element_id == "day-view") {
+      view_element.classList.add("day-view-class", "day-view-limitions");
+      btn = document.getElementById('close-day-view');
+      btn.addEventListener("click", function() {
+          close_the_view();
+      });
   }
 }
 
 function change_view(view, day, element_id) {
   if (element_id == "calendarview") {
-    close_the_view();
+      close_the_view();
   }
   const path = `/${view}/${day}`;
-  fetch(path).then(function (response) {
+  fetch(path).then(function(response) {
       return response.text();
-    }).then(function (html) {
+  }).then(function(html) {
       view_element = document.getElementById(element_id);
       view_element.innerHTML = html;
       set_views_styles(view_element, element_id);
-    }
-  );
+  });
 }
 
 function set_toggle_view_btns(btn, view) {
   dayview_btn = document.getElementById(btn);
   day = dayview_btn.name;
-  dayview_btn.addEventListener('click', function () {
-    change_view(view, day, "calendarview");
-  }
-  );
+  dayview_btn.addEventListener('click', function() {
+      change_view(view, day, "calendarview");
+  });
 }
 
 function set_days_view_open() {
   const Days = document.getElementsByClassName('day');
   for (let i = 0; i < Days.length; i++) {
-    var day = Days[i].title;
-    Days[i].addEventListener('click', function () {
-      change_view('day', day, 'day-view');
-    });
+      let day = Days[i].title;
+      Days[i].addEventListener('click', function() {
+          change_view('day', day, 'day-view');
+      });
   }
 }
 
 
 document.addEventListener(
-  'DOMContentLoaded', function () {
-    set_toggle_view_btns('week-view-button', 'week');
-    set_toggle_view_btns('day-view-button', 'day');
-    month = document.getElementById('month-view-button');
-    month.addEventListener('click', function () {refreshview();});
-    set_days_view_open();
+  'DOMContentLoaded',
+  function() {
+      set_toggle_view_btns('week-view-button', 'week');
+      set_toggle_view_btns('day-view-button', 'day');
+      month = document.getElementById('month-view-button');
+      month.addEventListener('click', function() {
+          refreshview();
+      });
+      set_days_view_open();
   }
 )
