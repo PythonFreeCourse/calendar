7313c85a7fa31cc6e63e9899c14963cc77d6ac5b
Elor Shoshan <elor550@gmail.com>
2021-03-10 22:57:37 +0200

    fix : bugfix

--- tests/test_shabbat.py
+++ tests/test_shabbat.py
@@ -29,14 +29,7 @@ SHABBAT_TIME = {
             },
         ]
 }
-BAD_DAY = date(2021, 2, 27)
 FRIDAY = date(2021, 2, 26)
-
-
-def test_return_none_if_date_no_friday():
-    location_by_ip = geocoder.ip('me')
-    result = shabbat.get_shabbat_if_date_friday(BAD_DAY, location_by_ip)
-    assert result is None
 
 
 def test_return_if_date_is_friday():
