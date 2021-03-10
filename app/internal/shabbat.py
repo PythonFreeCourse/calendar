421e70b67b7860771f0668c9a49723305ec1d48c
Elor Shoshan <elor550@gmail.com>
2021-03-10 19:16:47 +0200

    fix : bugfix

--- app/internal/shabbat.py
+++ app/internal/shabbat.py
@@ -2,44 +2,44 @@ import json
 from datetime import date, datetime
 from typing import Any, Dict, Optional, Union
 
+import httpx
+
 from app.config import RESOURCES_DIR
 
 
-def return_shabbat_details_by_user_location(location_by_ip) -> Any:
-    """Returns times details which match to ip location.
-    Used the shabbat_time_by_location JSON file, that his content is copied
-    from the free API:
-    'https://www.hebcal.com/shabbat?cfg=json&geonameid=295277'.
-    This Json need to be update once in year.
-
-        Args:
-            location_by_ip: location by ip that create with "geocoder" module.
-
-        Returns:
-            A zip number for the user location.
-    """
-
-    path = RESOURCES_DIR / "shabbat_time_by_location.json"
-    with open(path, 'r', encoding="utf8") as json_file:
-        shabat_details = json.load(json_file)
-    for location in shabat_details:
-        print(location.get('location'))
-        if (location["location"]["city"] == location_by_ip.city
-                and location["location"]["cc"] == location_by_ip.country):
-            return location["items"]
+SHABBAT_API = 'https://www.hebcal.com/shabbat?cfg=json&geonameid='
 
 
-def shabbat_time_by_user_location(location_by_ip) \
-        -> Dict[str, Union[date, Any]]:
-    """Returns the shabbat time of the user location.
+def return_zip_code_of_user_location(location_by_ip) -> str:
+    """Returns zip code from locations JSON file that match user location.
 
         Args:
         location_by_ip: location by ip that create with "geocoder" module.
 
         Returns:
+        A zip code string for the user location.
+    """
+    path = RESOURCES_DIR / "locations.json"
+    with open(path, 'r', encoding="utf8") as json_file:
+        locations = json.load(json_file)
+    for location in locations:
+        if (location["city"] == location_by_ip.city
+                and location["country"] == location_by_ip.country):
+            return location["zip_number"]
+
+
+def return_shabbat_times(shabat_items) -> Dict[str, Union[date, Any]]:
+    """Returns the shabbat time which match to ip(of the user) location.
+    Used the content of this is free API:
+    'https://www.hebcal.com/shabbat?cfg=json&geonameid=295277'.
+
+        Args:
+        shabat_items: dictionary of all the details about shabbat according to
+        specific ip location.
+
+        Returns:
             Shabbat start end ending time and user location by ip.
         """
-    shabat_items = return_shabbat_details_by_user_location(location_by_ip)
     for item in shabat_items:
         if "Candle lighting" in item["title"]:
             shabbat_entry = item["date"]
@@ -59,20 +59,27 @@ def shabbat_time_by_user_location(location_by_ip) \
     return shabbat_limit
 
 
-def get_shabbat_if_date_friday(calendar_date: date,
-                               location_by_ip,
-                               ) -> Optional[Dict[str, date]]:
-    """Returns shabbat start end ending time if specific date
-     is Saturday, else None.
+async def get_shabbat_if_date_friday(calendar_date: date,
+                                     location_by_ip,
+                                     ) -> Optional[Dict[str, date]]:
+    """Returns shabbat start end ending time if specific date is friday,
+     else None.
+     The function used in the free API:
+     'https://www.hebcal.com/shabbat?cfg=json&geonameid=295277'.
 
         Args:
             calendar_date: date.
             location_by_ip: location by ip that create with "geocoder" module.
 
         Returns:
-            Shabbat start end ending time if specific date
-             is Saturday and user location by ip, else None
-        """
-    shabbat_obj = shabbat_time_by_user_location(location_by_ip)
-    if calendar_date == shabbat_obj["start_date"]:
-        return shabbat_obj
+            Shabbat start and ending time if specific date
+            is Saturday and user location by ip, else None
+    """
+    async with httpx.AsyncClient() as client:
+        zip_code = return_zip_code_of_user_location(location_by_ip)
+        shabbat_api = SHABBAT_API + zip_code
+        response = await client.get(shabbat_api)
+        shabat_items = response.json()['items']
+        shabbat_obj = return_shabbat_times(shabat_items)
+        if calendar_date == shabbat_obj["start_date"]:
+            return shabbat_obj
