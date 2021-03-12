659d55f1fccadf8f415ef02729937272c69d7508
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2021-03-12 18:02:23 +0200

    fix: modify 'id' to 'note_id'

--- app/routers/notes/notes.py
+++ app/routers/notes/notes.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Sequence
 
-from fastapi import APIRouter, Request, status, HTTPException
+from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.param_functions import Depends
 from fastapi.responses import Response
 from sqlalchemy.orm import Session
@@ -11,7 +11,6 @@ from app.dependencies import get_db, templates
 from app.internal import utils
 from app.internal.notes import notes
 
-
 router = APIRouter(
     prefix="/notes",
     tags=["notes"],
@@ -20,34 +19,44 @@ router = APIRouter(
 
 
 @router.post(
-    "/edit/{id}", status_code=status.HTTP_202_ACCEPTED, include_in_schema=False
+    "/edit/{note_id}",
+    status_code=status.HTTP_202_ACCEPTED,
+    include_in_schema=False,
 )
 async def redirect_update_note(
-    request: Request, id: int, session: Session = Depends(get_db)
+    request: Request,
+    note_id: int,
+    session: Session = Depends(get_db),
 ) -> RedirectResponse:
     """Update a note using user-interface form."""
     form = await request.form()
     updated_note = NoteSchema(**dict(form))
-    await update_note(updated_note, id, session)
+    await update_note(updated_note, note_id, session)
     return RedirectResponse("/notes", status_code=status.HTTP_302_FOUND)
 
 
 @router.post(
-    "/delete/{id}", status_code=status.HTTP_200_OK, include_in_schema=False
+    "/delete/{note_id}",
+    status_code=status.HTTP_200_OK,
+    include_in_schema=False,
 )
 async def redirect_delete_note(
-    id: int, session: Session = Depends(get_db)
+    note_id: int,
+    session: Session = Depends(get_db),
 ) -> RedirectResponse:
     """Delete a note from the database using user-interface form."""
-    await delete_note(id, session)
+    await delete_note(note_id, session)
     return RedirectResponse("/notes", status_code=status.HTTP_302_FOUND)
 
 
 @router.post(
-    "/add", status_code=status.HTTP_201_CREATED, include_in_schema=False
+    "/add",
+    status_code=status.HTTP_201_CREATED,
+    include_in_schema=False,
 )
 async def create_note_by_form(
-    request: Request, session: Session = Depends(get_db)
+    request: Request,
+    session: Session = Depends(get_db),
 ) -> RedirectResponse:
     """Add a note using user-interface form."""
     form = await request.form()
@@ -59,7 +68,8 @@ async def create_note_by_form(
 
 @router.post("/", response_model=NoteDB, status_code=status.HTTP_201_CREATED)
 async def create_new_note(
-    request: NoteSchema, session: Session = Depends(get_db)
+    request: NoteSchema,
+    session: Session = Depends(get_db),
 ) -> Dict[str, Any]:
     """Create a note in the database."""
     new_note = NoteSchema(**dict(request))
@@ -67,39 +77,47 @@ async def create_new_note(
     return await notes.create_note(note=new_note, session=session)
 
 
-@router.delete("/{id}/", status_code=status.HTTP_200_OK)
-async def delete_note(id: int, session: Session = Depends(get_db)) -> str:
+@router.delete("/{note_id}/", status_code=status.HTTP_200_OK)
+async def delete_note(note_id: int, session: Session = Depends(get_db)) -> str:
     """Delete a note by its identifier."""
-    return await notes.delete(session, id)
+    return await notes.delete(session, note_id)
 
 
-@router.put("/{id}/", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/{note_id}/", status_code=status.HTTP_202_ACCEPTED)
 async def update_note(
-    request: NoteSchema, id: int, session: Session = Depends(get_db)
+    request: NoteSchema,
+    note_id: int,
+    session: Session = Depends(get_db),
 ) -> str:
     """Update a note by providing its identifier and the changed json data."""
-    return await notes.update(request, session, id)
+    return await notes.update(request, session, note_id)
 
 
-@router.get("/view/{id}", include_in_schema=False)
+@router.get("/view/{note_id}", include_in_schema=False)
 async def view_note(
-    request: Request, id: int, session: Session = Depends(get_db)
+    request: Request,
+    note_id: int,
+    session: Session = Depends(get_db),
 ) -> Response:
     """View a note for update using user interface."""
-    note = await notes.view(session, id)
+    note = await notes.view(session, note_id)
     return templates.TemplateResponse(
-        "notes/note_view.html", {"request": request, "data": note}
+        "notes/note_view.html",
+        {"request": request, "data": note},
     )
 
 
-@router.get("/delete/{id}", include_in_schema=False)
+@router.get("/delete/{note_id}", include_in_schema=False)
 async def remove_note(
-    request: Request, id: int, session: Session = Depends(get_db)
+    request: Request,
+    note_id: int,
+    session: Session = Depends(get_db),
 ) -> Response:
     """View a note for delete using user interface."""
-    note = await notes.view(session, id)
+    note = await notes.view(session, note_id)
     return templates.TemplateResponse(
-        "notes/note_delete.html", {"request": request, "data": note}
+        "notes/note_delete.html",
+        {"request": request, "data": note},
     )
 
 
@@ -117,24 +135,33 @@ async def get_all_notes(
     return await notes.get_all(session)
 
 
-@router.get("/{id}/", status_code=status.HTTP_200_OK, response_model=NoteDB)
-async def read_note(id: int, session: Session = Depends(get_db)) -> NoteDB:
+@router.get(
+    "/{note_id}/",
+    status_code=status.HTTP_200_OK,
+    response_model=NoteDB,
+)
+async def read_note(
+    note_id: int,
+    session: Session = Depends(get_db),
+) -> NoteDB:
     """View a note by its identifier."""
-    note = await notes.view(session, id)
+    note = await notes.view(session, note_id)
     if not note:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Note with id {id} not found",
+            detail=f"Note with id {note_id} not found",
         )
     return note
 
 
 @router.get("/", include_in_schema=False)
 async def view_notes(
-    request: Request, session: Session = Depends(get_db)
+    request: Request,
+    session: Session = Depends(get_db),
 ) -> Response:
     """View all notes in the database using user interface."""
     data = await notes.get_all(session)
     return templates.TemplateResponse(
-        "notes/notes.html", {"request": request, "data": data}
+        "notes/notes.html",
+        {"request": request, "data": data},
     )
